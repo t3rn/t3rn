e3f413af4267399f3475f1b21289ea12cf5f26e1
MaciejBaj <macie.baj@gmail.com>
2020-09-17 10:11:24 +0200

    Prepare fixture contracts with small and (too) big transfers
    Signed-off-by: MaciejBaj <macie.baj@gmail.com>

--- gateway/pallet-escrow-gateway/src/fixtures/compile_wat2wasm.sh
+++ gateway/pallet-escrow-gateway/src/fixtures/compile_wat2wasm.sh
@@ -9,3 +9,7 @@ wasm-prune --exports call,deploy $PROJNAME.wasm $PROJNAME-pruned.wasm
 PROJNAME=transfer_return_code
 wat2wasm -o $PROJNAME.wasm $PROJNAME.wat
 wasm-prune --exports call,deploy $PROJNAME.wasm $PROJNAME-pruned.wasm
+
+PROJNAME=transfer_big_amount_return_code
+wat2wasm -o $PROJNAME.wasm $PROJNAME.wat
+wasm-prune --exports call,deploy $PROJNAME.wasm $PROJNAME-pruned.wasm
