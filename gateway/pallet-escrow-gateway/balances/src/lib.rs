fe81c6d4a59517d76acdb08d1179a659aee524ed
MaciejBaj <macie.baj@gmail.com>
2020-09-29 11:00:59 +0200

    Fix gateway balance to satisfy release build requirements - Add missing defaults to cargo deps - Correct misspelling for proof local var - Use vec from sp_std

--- gateway/pallet-escrow-gateway/balances/src/lib.rs
+++ gateway/pallet-escrow-gateway/balances/src/lib.rs
@@ -38,6 +38,7 @@ use sp_runtime::{
     DispatchError,
 };
 use sp_std::convert::TryInto;
+use sp_std::vec;
 use sp_std::vec::Vec;
 use sudo;
 
@@ -323,7 +324,7 @@ decl_module! {
                         Error::<T>::BalanceTransferFailed
                     })?;
 
-                    debug::info("DEBUG multistep_call -- just_transfer total balance of CONTRACT -- vs REQUESTER {:?} vs ESCROW {:?}", <T as EscrowTrait>::Currency::free_balance(&requester), <T as EscrowTrait>::Currency::free_balance(&escrow_account));
+                    debug::info!("DEBUG multistep_call -- just_transfer total balance of CONTRACT -- vs REQUESTER {:?} vs ESCROW {:?}", <T as EscrowTrait>::Currency::free_balance(&requester), <T as EscrowTrait>::Currency::free_balance(&escrow_account));
 
                     let mut transfers = Vec::<TransferEntry>::new();
                     let mut deferred_storage_writes = Vec::<DeferredStorageWrite>::new();
@@ -385,15 +386,15 @@ decl_module! {
                         Some(merged_post_storage) => Some(T::Hashing::hash(&merged_post_storage).encode()),
                     };
 
-                    let execution_poofs = ExecutionProofs {
+                    let execution_proofs = ExecutionProofs {
                         // Present the execution proof by hashing the results.
                         result: result_proof,
                         storage: storage_proof,
                         deferred_transfers: <DeferredTransfers<T>>::get(&requester, &target_dest.clone()),
                     };
-                    debug::info("DEBUG multistepcall -- Execution Proofs : result {:?} ", execution_proofs.result);
-                    debug::info("DEBUG multistepcall -- Execution storage : storage {:?}", execution_proofs.storage);
-                    debug::info("DEBUG multistepcall -- Execution Proofs : deferred_transfers {:?}", execution_proofs.deferred_transfers);
+                    debug::info!("DEBUG multistepcall -- Execution Proofs : result {:?} ", execution_proofs.result);
+                    debug::info!("DEBUG multistepcall -- Execution storage : storage {:?}", execution_proofs.storage);
+                    debug::info!("DEBUG multistepcall -- Execution Proofs : deferred_transfers {:?}", execution_proofs.deferred_transfers);
                     <DeferredStorageWrites<T>>::insert(&requester, &T::Hashing::hash(&code.clone()), deferred_storage_writes);
 
                     <ExecutionStamps<T>>::insert(&requester, &T::Hashing::hash(&code.clone()), ExecutionStamp {
