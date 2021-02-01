f7e09d49612d291ebdc588a7e584eb61498a8a32
MaciejBaj <macie.baj@gmail.com>
2021-02-01 11:15:52 +0100

    Add circuit primitives for compose api and runtime

---
+++ circuit/src/primitives/src/lib.rs
@@ -0,0 +1,68 @@
+// Copyright 2020 Parity Technologies (UK) Ltd.
+// This file is part of Substrate.
+
+// Substrate is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Substrate is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Substrate. If not, see <http://www.gnu.org/licenses/>.
+
+//! A crate that hosts a common definitions that are relevant for the pallet-contracts.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::{Decode, Encode};
+use sp_runtime::RuntimeDebug;
+use sp_std::prelude::*;
+
+/// A struct that encodes RPC parameters required for a call to a smart-contract.
+#[derive(Encode, Decode)]
+pub struct Compose<Account, Balance> {
+    pub name: Vec<u8>,
+    pub code_txt: Vec<u8>,
+    pub gateway_id: Account,
+    pub exec_type: Vec<u8>,
+    pub dest: Account,
+    pub value: Balance,
+    pub bytes: Vec<u8>,
+    pub input_data: Vec<u8>,
+}
+/// A result type of a get storage call.
+pub type FetchContractsResult = Result<Option<Vec<u8>>, ContractAccessError>;
+
+/// A result of execution of a contract.
+#[derive(Eq, PartialEq, Encode, Decode, RuntimeDebug)]
+pub enum ComposableExecResult {
+    /// The contract returned successfully.
+    ///
+    /// There is a status code and, optionally, some data returned by the contract.
+    Success {
+        /// Flags that the contract passed along on returning to alter its exit behaviour.
+        /// Described in `pallet_contracts::exec::ReturnFlags`.
+        flags: u32,
+        /// Output data returned by the contract.
+        ///
+        /// Can be empty.
+        data: Vec<u8>,
+        /// How much gas was consumed by the call.
+        gas_consumed: u64,
+    },
+    /// The contract execution either trapped or returned an error.
+    Error,
+}
+
+/// The possible errors that can happen querying the storage of a contract.
+#[derive(Eq, PartialEq, codec::Encode, codec::Decode, sp_runtime::RuntimeDebug)]
+pub enum ContractAccessError {
+    /// The given address doesn't point to a contract.
+    DoesntExist,
+    /// The specified contract is a tombstone and thus cannot have any storage.
+    IsTombstone,
+}
