36e511be6d6840e4ef0386e6eb0ace1b2d0de36a
MaciejBaj <macie.baj@gmail.com>
2021-03-23 12:48:06 +0100

    Add multi-finality verifier to runtime

--- circuit/build.rs
+++ circuit/build.rs
@@ -1,3 +1,6 @@
+// Copyright 2019-2020 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
 // Parity Bridges Common is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -11,12 +14,10 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
 
-use vergen::{generate_cargo_keys, ConstantsFlags};
-
-const ERROR_MSG: &str = "Failed to generate metadata files";
+use substrate_build_script_utils::{generate_cargo_keys, rerun_if_git_head_changed};
 
 fn main() {
-	generate_cargo_keys(ConstantsFlags::SHA_SHORT).expect(ERROR_MSG);
+    generate_cargo_keys();
 
-	build_script_utils::rerun_if_git_head_changed();
+    rerun_if_git_head_changed();
 }
