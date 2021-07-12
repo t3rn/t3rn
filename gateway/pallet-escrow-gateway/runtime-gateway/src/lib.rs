cfa008b15defbdc6031bf7cd236c27a6c56955af
MaciejBaj <maciej@t3rn.io>
2021-07-12 18:30:58 +0200

    Require bridge-messages as Runtime Gateway deps

--- gateway/pallet-escrow-gateway/runtime-gateway/src/lib.rs
+++ gateway/pallet-escrow-gateway/runtime-gateway/src/lib.rs
@@ -174,7 +174,9 @@ pub mod pallet {
     };
     /// Configure the pallet by specifying the parameters and types on which it depends.
     #[pallet::config]
-    pub trait Config: frame_system::Config + VersatileWasm + EscrowTrait {
+    pub trait Config:
+        frame_system::Config + VersatileWasm + EscrowTrait + pallet_bridge_messages::Config
+    {
         /// Because this pallet emits events, it depends on the runtime's definition of an event.
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
     }
