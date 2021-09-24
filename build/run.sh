97f63041fd6dba2bf96a5b3b468df6fb98724eef
vedhavyas <vedhavyas.singareddi@gmail.com>
2021-09-24 18:26:15 +0200

    complete deply script

--- build/run.sh
+++ build/run.sh
@@ -4,6 +4,13 @@ get_geth_container_id() {
   docker ps -q -f name=geth
 }
 
+circuit_pid_file="/tmp/circuit.pid"
+get_circuit_process_id() {
+  if [[ -s "${circuit_pid_file}" ]]; then
+    cat "${circuit_pid_file}"
+  fi
+}
+
 mode=$1
 case ${mode} in
 start-geth)
@@ -17,7 +24,7 @@ start-geth)
    --dev --dev.period 14 --rpc --rpcport 8545 --rpcaddr 0.0.0.0 --rpcapi db,eth,net,web3,personal,admin,txpool \
   --ws --ws.port 8546 --ws.addr 0.0.0.0 --ws.origins "*" --ws.api db,eth,net,web3,personal,admin,txpool &> /dev/null && \
   id=$(get_geth_container_id) && \
-  echo "Started geth ${id}"
+  echo "Started geth[${id}]"
   ;;
 
 stop-geth)
@@ -41,6 +48,56 @@ deploy-contracts)
   yarn install && \
   cp env.template .env && \
   npx hardhat deploy --network localhost
+  ;;
 
+clean-contracts)
+  echo "cleaning contracts cache..."
+  dir=$(dirname "$0") && \
+  cd "${dir}"/snowbridge/ethereum/ && \
+  rm -rf .deployments
+  echo "done"
+  ;;
+
+start-circuit)
+  c_pid=$(get_circuit_process_id)
+  if [ -n "${c_pid}" ]; then
+      echo "circuit already running[${c_pid}]"
+      exit 0
+  fi
+  dir=$(dirname "${0}")
+  echo "building circuit..."
+  cargo build --release --manifest-path="${dir}/../circuit/Cargo.toml" &> /tmp/circuit.build && \
+  "${dir}"/../circuit/target/release/circuit --alice --log=main,info --tmp &> /tmp/circuit.log &
+  c_pid=$!
+  echo "${c_pid}" > "${circuit_pid_file}"
+  echo "circuit started[${c_pid}]..."
+  ;;
+
+stop-circuit)
+  c_pid=$(get_circuit_process_id)
+  if [ -z "${c_pid}" ]; then
+      echo "circuit not running"
+      exit 0
+  fi
+
+  kill -9 "${c_pid}" &> /dev/null
+  rm -rf "${circuit_pid_file}"
+  echo "circuit stopped"
+  ;;
+
+all)
+  echo "starting environment. may take sometime..."
+  $0 start-geth && \
+  $0 start-circuit && \
+  $0 deploy-contracts && \
+  echo "done"
+  ;;
+
+clean)
+  echo "cleaning up..."
+  $0 stop-geth && \
+  $0 stop-circuit && \
+  $0 clean-contracts && \
+  echo "done"
   ;;
 esac
