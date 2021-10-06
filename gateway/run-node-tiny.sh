212a4dc98a104072573aa346a98dc86f9212186d
t3rn <65117420+t3rn@users.noreply.github.com>
2021-10-06 18:58:15 +0200

    Update node tiny to gateway-runtime

--- gateway/run-node-tiny.sh
+++ gateway/run-node-tiny.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-cd demo-runtime || exit
-
-echo "[run-demo-runtime.sh] Building demo-runtime with 'cargo build'..."
+echo "[demo-gateway-runtime.sh] Building demo-gateway-runtime with 'cargo build'..."
 cargo build || exit
 
-echo "[run-demo-runtime.sh] Cleaning the demo-runtime dev database with './target/debug/demo-runtime purge-chain --dev'..."
-yes | ./target/debug/demo-runtime purge-chain --dev
+echo "[gateway-runtime.sh] Cleaning the gateway-runtime dev database with './target/debug/gateway-runtime purge-chain --dev'..."
+yes | ./target/debug/gateway-runtime purge-chain --dev
 
-echo "[run-demo-runtime.sh] Running demo-runtime with './target/debug/demo-runtime --dev -lruntime=debug'..."
-./target/debug/demo-runtime --dev -lruntime=debug
+echo "[gateway-runtime.sh] Running demo-runtime with './target/debug/gateway-runtime --dev -lruntime=debug'..."
+./target/debug/gateway-runtime --dev -lruntime=debug
