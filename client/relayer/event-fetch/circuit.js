5dd16933fac55e078a54b2bd7d634f051c69e417
devanshu0987 <devanshu0987@gmail.com>
2021-11-10 16:41:33 +0530

    fetch events from rococo and circuit

---
+++ client/relayer/event-fetch/circuit.js
@@ -0,0 +1,27 @@
+// Import
+import { ApiPromise, WsProvider } from '@polkadot/api';
+
+export async function fetchCircuitEvents() {
+    const wsProvider = new WsProvider('ws://localhost:9944');
+    const api = await ApiPromise.create({ provider: wsProvider });
+
+    // Subscribe to system events via storage
+    api.query.system.events((events) => {
+        console.log(`\nReceived ${events.length} events from Circuit:`);
+
+        // Loop through the Vec<EventRecord>
+        events.forEach((record) => {
+            // Extract the phase, event and the event types
+            const { event, phase } = record;
+            const types = event.typeDef;
+
+            // Show what we are busy with
+            console.log(`\t${event.section}:${event.method}`);
+
+            // Loop through each of the parameters, displaying the type and data
+            event.data.forEach((data, index) => {
+                console.log(`\t\t\t${types[index].type}: ${data.toString()}`);
+            });
+        });
+    });
+}
--- client/relayer/index.js
+++ client/relayer/index.js
@@ -1,29 +1,9 @@
-// Import
-import { ApiPromise, WsProvider } from '@polkadot/api';
+import { fetchRococoEvents } from './event-fetch/rococo.js';
+import { fetchCircuitEvents } from './event-fetch/circuit.js';
 
 async function main() {
-    const wsProvider = new WsProvider('wss://rpc.polkadot.io');
-    const api = await ApiPromise.create({ provider: wsProvider });
-
-    // Subscribe to system events via storage
-    api.query.system.events((events) => {
-        console.log(`\nReceived ${events.length} events:`);
-
-        // Loop through the Vec<EventRecord>
-        events.forEach((record) => {
-            // Extract the phase, event and the event types
-            const { event, phase } = record;
-            const types = event.typeDef;
-
-            // Show what we are busy with
-            console.log(`\t${event.section}:${event.method}`);
-
-            // Loop through each of the parameters, displaying the type and data
-            event.data.forEach((data, index) => {
-                console.log(`\t\t\t${types[index].type}: ${data.toString()}`);
-            });
-        });
-    });
+    fetchRococoEvents();
+    fetchCircuitEvents();
 }
 
 main().catch((error) => {
