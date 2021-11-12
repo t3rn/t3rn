8eb3f0bbc13ddea67cc8fb5bbb08e0a72f86adaf
devanshu0987 <devanshu0987@gmail.com>
2021-11-12 22:20:04 +0530

    getProofs implementation done

--- client/relayer/chain_interactions/rococo.js
+++ client/relayer/chain_interactions/rococo.js
@@ -1,5 +1,8 @@
 // Import
 import { ApiPromise, WsProvider, Keyring } from '@polkadot/api';
+import { xxhashAsHex, xxhashAsU8a } from '@polkadot/util-crypto';
+import { u8aToHex } from '@polkadot/util';
+import { TypeRegistry } from '@polkadot/types';
 
 export async function submit_transfer_to_rococo_and_track(api) {
     return new Promise(async function (resolve) {
@@ -20,32 +23,51 @@ export async function submit_transfer_to_rococo_and_track(api) {
 
                 if (result.status.isFinalized) {
                     console.log(`Transaction finalized at blockHash ${result.status.asFinalized}`);
-                    // Loop through Vec<EventRecord> to display all events
-                    // Here we will get to know if we succeeded or failed.
-                    // should we return something from here?
 
                     result.events.forEach(({ phase, event: { data, method, section } }) => {
                         if (method === "ExtrinsicSuccess") {
                             resolve({
-                                "blockHash" : result.status.asFinalized,
-                                "status" : true
+                                "blockHash": result.status.asFinalized,
+                                "status": true
                             });
                         }
-                        else if(method === "ExtrinsicFailed") {
+                        else if (method === "ExtrinsicFailed") {
                             resolve({
-                                "blockHash" : result.status.asFinalized,
-                                "status" : false
+                                "blockHash": result.status.asFinalized,
+                                "status": false
                             });
                         }
                     });
                     unsub();
+
                 }
 
             });
     });
 }
 
-export async function getReadProof(keys, blockHash)
-{
+function generateSystemEventKey() {
+    // lets prepare the storage key for system events.
+    let module_hash = xxhashAsU8a("System", 128)
+    let storage_value_hash = xxhashAsU8a("Events", 128);
 
+    // Special syntax to concatenate Uint8Array
+    let final_key = new Uint8Array([
+        ...module_hash,
+        ...storage_value_hash
+    ]);
+
+    return u8aToHex(final_key);
+
+}
+
+export async function getProofs(api, blockHash) {
+    let key = generateSystemEventKey()
+    console.log(key);
+    let proofs = await api.rpc.state.getReadProof([key], blockHash);
+    console.log("PROOF");
+    console.log(proofs.proof.toJSON());
+
+    const registry = new TypeRegistry();
+    return registry.createType('Vec<Bytes>', proofs.proof.toJSON());
 }
--- client/relayer/index.js
+++ client/relayer/index.js
@@ -1,6 +1,6 @@
 import { fetchRococoEvents } from './event_fetch/rococo.js';
 import { fetchCircuitEvents } from './event_fetch/circuit.js';
-import { submit_transfer_to_rococo_and_track } from './chain_interactions/rococo.js';
+import { submit_transfer_to_rococo_and_track, getProofs } from './chain_interactions/rococo.js';
 import { ApiPromise, WsProvider } from '@polkadot/api';
 
 async function main() {
@@ -18,8 +18,8 @@ async function main() {
     let { blockHash, status } = await submit_transfer_to_rococo_and_track(circuitApi);
     if (status) {
         // the transfer was successful. Hence we now want to get the proofs and stuff
-        console.log(blockHash);
-        console.log(status);
+        let proofs_in_vec_of_bytes = await getProofs(circuitApi, blockHash);
+        // console.log(proofs_in_vec_of_bytes);
     }
     else {
         throw "Transfer was unsuccessful. Cant move forward";
