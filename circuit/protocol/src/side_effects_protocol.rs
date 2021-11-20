9f8d775fb32cfd430f0d4379ed7e8b9d62900a41
MaciejBaj <maciej@t3rn.io>
2021-11-20 19:25:40 +0100

    Test transfer event content

--- circuit/protocol/src/side_effects_protocol.rs
+++ circuit/protocol/src/side_effects_protocol.rs
@@ -182,3 +182,95 @@ pub trait SideEffectDirty<
         Ok(())
     }
 }
+
+#[cfg(test)]
+pub mod tests {
+
+    use codec::Encode;
+    use frame_support::{
+        parameter_types,
+        weights::{constants::WEIGHT_PER_SECOND},
+    };
+    use hex_literal::hex;
+    use sp_runtime::{
+        testing::{Header, H256},
+        traits::{BlakeTwo256, IdentityLookup},
+        AccountId32,
+    };
+
+    type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+    type Block = frame_system::mocking::MockBlock<Test>;
+
+    frame_support::construct_runtime!(
+        pub enum Test where
+            Block = Block,
+            NodeBlock = Block,
+            UncheckedExtrinsic = UncheckedExtrinsic,
+        {
+            System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+            Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+        }
+    );
+
+    parameter_types! {
+        pub const BlockHashCount: u64 = 250;
+        pub BlockWeights: frame_system::limits::BlockWeights =
+            frame_system::limits::BlockWeights::simple_max(2 * WEIGHT_PER_SECOND);
+        pub static ExistentialDeposit: u64 = 0;
+    }
+    impl frame_system::Config for Test {
+        type BaseCallFilter = ();
+        type BlockWeights = BlockWeights;
+        type BlockLength = ();
+        type DbWeight = ();
+        type Origin = Origin;
+        type Index = u64;
+        type BlockNumber = u64;
+        type Hash = H256;
+        type Call = Call;
+        type Hashing = BlakeTwo256;
+        type AccountId = AccountId32;
+        type Lookup = IdentityLookup<Self::AccountId>;
+        type Header = Header;
+        type Event = Event;
+        type BlockHashCount = BlockHashCount;
+        type Version = ();
+        type PalletInfo = PalletInfo;
+        type AccountData = pallet_balances::AccountData<u64>;
+        type OnNewAccount = ();
+        type OnKilledAccount = ();
+        type SystemWeightInfo = ();
+        type SS58Prefix = ();
+        type OnSetCode = ();
+    }
+
+    impl pallet_balances::Config for Test {
+        type MaxLocks = ();
+        type MaxReserves = ();
+        type ReserveIdentifier = [u8; 8];
+        type Balance = u64;
+        type Event = Event;
+        type DustRemoval = ();
+        type ExistentialDeposit = ExistentialDeposit;
+        type AccountStore = System;
+        type WeightInfo = ();
+    }
+
+    #[test]
+    fn successfully_confirms_encoded_balance_transferred_event() {
+        let encoded_balance_transfer_event = pallet_balances::Event::<Test>::Transfer(
+            hex!("0909090909090909090909090909090909090909090909090909090909090909").into(),
+            hex!("0606060606060606060606060606060606060606060606060606060606060606").into(),
+            1,
+        );
+
+        assert_eq!(
+            encoded_balance_transfer_event.encode(),
+            vec![
+                2, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
+                9, 9, 9, 9, 9, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+                6, 6, 6, 6, 6, 6, 6, 6, 6, 1, 0, 0, 0, 0, 0, 0, 0
+            ]
+        );
+    }
+}
