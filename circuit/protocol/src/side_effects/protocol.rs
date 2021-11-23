17f086916f6cd4ddd63dc1e211254b0bcf77d58a
MaciejBaj <maciej@t3rn.io>
2021-11-23 12:18:06 +0100

    Add tests for populate state and volatile

--- circuit/protocol/src/side_effects/protocol.rs
+++ circuit/protocol/src/side_effects/protocol.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use crate::side_effects::volatile::{LocalState, Volatile};
+pub use crate::side_effects::volatile::{LocalState, Volatile};
 use codec::{Decode, Encode};
 use sp_std::vec::*;
 use t3rn_primitives::abi::{GatewayABIConfig, Type};
@@ -118,7 +118,10 @@ pub trait SideEffectProtocol {
         assert!(mapper.len() == encoded_args.len());
         for (i, arg) in encoded_args.iter().enumerate() {
             let arg_name = mapper[i];
-            local_state.insert(arg_name, arg.to_vec())?;
+            match local_state.insert(arg_name, arg.to_vec()) {
+                Ok((_state_key, _state_val)) => continue,
+                Err(err) => return Err(err),
+            }
         }
         Ok(())
     }
@@ -152,69 +155,20 @@ pub trait SideEffectProtocol {
 #[cfg(test)]
 pub mod tests {
 
-    use frame_support::{parameter_types, weights::constants::WEIGHT_PER_SECOND};
+    use super::*;
+    use hex_literal::hex;
 
-    use sp_runtime::{
-        testing::{Header, H256},
-        traits::{BlakeTwo256, IdentityLookup},
-        AccountId32,
-    };
-
-    type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
-    type Block = frame_system::mocking::MockBlock<Test>;
-
-    frame_support::construct_runtime!(
-        pub enum Test where
-            Block = Block,
-            NodeBlock = Block,
-            UncheckedExtrinsic = UncheckedExtrinsic,
-        {
-            System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-            Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
-        }
-    );
-
-    parameter_types! {
-        pub const BlockHashCount: u64 = 250;
-        pub BlockWeights: frame_system::limits::BlockWeights =
-            frame_system::limits::BlockWeights::simple_max(2 * WEIGHT_PER_SECOND);
-        pub static ExistentialDeposit: u64 = 0;
-    }
-    impl frame_system::Config for Test {
-        type BaseCallFilter = ();
-        type BlockWeights = BlockWeights;
-        type BlockLength = ();
-        type DbWeight = ();
-        type Origin = Origin;
-        type Index = u64;
-        type BlockNumber = u64;
-        type Hash = H256;
-        type Call = Call;
-        type Hashing = BlakeTwo256;
-        type AccountId = AccountId32;
-        type Lookup = IdentityLookup<Self::AccountId>;
-        type Header = Header;
-        type Event = Event;
-        type BlockHashCount = BlockHashCount;
-        type Version = ();
-        type PalletInfo = PalletInfo;
-        type AccountData = pallet_balances::AccountData<u64>;
-        type OnNewAccount = ();
-        type OnKilledAccount = ();
-        type SystemWeightInfo = ();
-        type SS58Prefix = ();
-        type OnSetCode = ();
-    }
-
-    impl pallet_balances::Config for Test {
-        type MaxLocks = ();
-        type MaxReserves = ();
-        type ReserveIdentifier = [u8; 8];
-        type Balance = u64;
-        type Event = Event;
-        type DustRemoval = ();
-        type ExistentialDeposit = ExistentialDeposit;
-        type AccountStore = System;
-        type WeightInfo = ();
+    #[test]
+    fn successfully_populates_state_for_transfer_arguments() {
+        let _expected_transfer_arg_names_input = vec!["from", "to", "value"];
+        let encoded_transfer_args_input = vec![
+            hex!("0909090909090909090909090909090909090909090909090909090909090909").into(),
+            hex!("0606060606060606060606060606060606060606060606060606060606060606").into(),
+            1.encode(),
+        ];
+        let mut local_state = LocalState::new();
+        let transfer_protocol = TransferSideEffectProtocol {};
+        let res = transfer_protocol.populate_state(encoded_transfer_args_input, &mut local_state);
+        assert_eq!(res, Ok(()));
     }
 }
