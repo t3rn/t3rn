ffd8d50df356da37ec32c1ec14f0dc0e75901b87
zannis <zannis@t3rn.io>
2021-11-24 19:59:58 +0200

    Add JS integration tests, fix gateway fixtures symlink

--- client/circuit-integration-tests/test/execution_delivery.test.ts
+++ client/circuit-integration-tests/test/execution_delivery.test.ts
@@ -11,8 +11,9 @@ import {
 } from '@t3rn/types/dist';
 import { expect } from 'chai';
 import { BN } from '@polkadot/util';
+import { blake2AsHex } from '@polkadot/util-crypto';
 
-const timeout = new Promise<void>((resolve, reject) => setTimeout(() => reject(new Error('timeout')), 30000))
+const timeoutIn = (seconds: number) => new Promise<void>((resolve, reject) => setTimeout(() => reject(new Error('timeout')), seconds));
 
 describe('Execution Delivery | Extrinsics', function() {
   this.timeout(30000);
@@ -35,7 +36,7 @@ describe('Execution Delivery | Extrinsics', function() {
   };
   let circuitApi: ApiPromise;
 
-  beforeEach(async () => {
+  before(async () => {
     circuitApi = await ApiPromise.create({
       provider: circuitProvider,
       types,
@@ -48,10 +49,12 @@ describe('Execution Delivery | Extrinsics', function() {
       const rococoUrl = 'wss://rococo-rpc.polkadot.io';
       const rococoProvider = new WsProvider(rococoUrl);
       const rococoApi = await ApiPromise.create({ provider: rococoProvider });
-      const rococoCurrentHeader = await rococoApi.rpc.chain.getHeader();
-      const rococoMetadata = await rococoApi.runtimeMetadata;
-      const rococoRuntimeVersion = await rococoApi.runtimeVersion;
-      const rococoGenesisHash = await rococoApi.genesisHash;
+      const [
+        rococoCurrentHeader,
+        rococoMetadata,
+        rococoRuntimeVersion,
+        rococoGenesisHash,
+      ] = await Promise.all([await rococoApi.rpc.chain.getHeader(), await rococoApi.runtimeMetadata, await rococoApi.runtimeVersion, await rococoApi.genesisHash]);
       const rococoAtGenesis = await rococoApi.at(rococoGenesisHash);
       const rococoInitialAuthorityList = await rococoAtGenesis.query.session.validators();
       await rococoApi.disconnect();
@@ -84,24 +87,75 @@ describe('Execution Delivery | Extrinsics', function() {
           expect(new BN('868383000').eq(result.dispatchInfo?.weight!)).to.be.true;
           expect(result.dispatchInfo?.class.isNormal).to.be.true;
           expect(result.dispatchInfo?.paysFee.isYes).to.be.true;
-          expect(result.events).to.be.an('array').with.lengthOf(4);
+          expect(result.events).to.be.an('array');
+          // expect(result.events).to.contain.;
           resolve();
         }
       }));
 
       // The extrinsic should be finalized before the timeout
-      await Promise.race([result, timeout])
+      await Promise.race([result, timeoutIn(30000)]);
 
       // assert the record was added to the xdns storage
       const xdns = await circuitApi.rpc.xdns.fetchRecords();
-      expect(xdns.xdns_records.map((x: XdnsRecord) => x.gateway_id.toHuman())).to.include.deep.members([gatewayId])
+      expect(xdns.xdns_records.map((x: XdnsRecord) => x.gateway_id.toHuman())).to.include.deep.members([gatewayId]);
 
       // assert the gateway exists in multi-finality-verifier storage
-     // await circuitApi.query.bridgePolkadotLikeMultiFinalityVerifier.initialHashMap(gatewayId)
+      // await circuitApi.query.bridgePolkadotLikeMultiFinalityVerifier.initialHashMap(gatewayId)
     });
 
-    it('should not register a gateway if it already exists (Polkadot)');
+    it('should not register a gateway if it already exists (Polkadot)', async () => {
+      const polkadotUrl = 'wss://rpc.polkadot.io';
+      const provider = new WsProvider(polkadotUrl);
+      const polkadotApi = await ApiPromise.create({provider})
+      const [
+        polkadotMetadata,
+        polkadotRuntimeVersion,
+        polkadotGenesisHash,
+      ] = await Promise.all([await polkadotApi.runtimeMetadata, await polkadotApi.runtimeVersion, await polkadotApi.genesisHash]);
 
-    afterEach(async () => await circuitApi.disconnect());
+      // Constuct the keyring after the API (crypto has an async init)
+      const keyring = new Keyring({ type: 'sr25519', ss58Format: 60 });
+      const alice = keyring.addFromUri('//Alice');
+
+      // Create the extrinsic call
+      const registerGateway = circuitApi.tx.execDelivery.registerGateway(
+        polkadotUrl,
+        'pdot',
+        createGatewayABIConfig(circuitApi, 32, 32, 32, 12, 'Sr25519', 'Blake2'), // GatewayABI
+        circuitApi.createType('GatewayVendor', 'Substrate'), // GatewayVendor
+        circuitApi.createType('GatewayType', { ProgrammableExternal: 1 }), // GatewayType
+        createGatewayGenesisConfig(polkadotMetadata, polkadotRuntimeVersion, polkadotGenesisHash, circuitApi), // GatewayGenesisConfig
+        circuitApi.createType('Bytes', []), // first header
+        circuitApi.createType('Option<Vec<AccountId>>', []), // authorities
+        <Vec<AllowedSideEffect>>circuitApi.createType('Vec<AllowedSideEffect>', ['transfer', 'get_storage']), // allowed side effects
+      );
+
+      // Wrap in sudo, submit the extrinsic and make wait until finalized
+      const result = new Promise<void>(resolve => circuitApi.tx.sudo.sudo(registerGateway).signAndSend(alice, (result) => {
+        if (result.status.isFinalized) {
+          expect(result.dispatchError).to.be.undefined;
+          expect(result.internalError).to.be.undefined;
+          expect(result.dispatchInfo).to.be.ok;
+          expect(result.dispatchInfo?.weight).to.be.ok;
+          expect(new BN('868383000').eq(result.dispatchInfo?.weight!)).to.be.true;
+          expect(result.dispatchInfo?.class.isNormal).to.be.true;
+          expect(result.dispatchInfo?.paysFee.isYes).to.be.true;
+          expect(result.events).to.be.an('array');
+          expect(result.events.map(e => e.event.method)).to.not.contain.members(["XdnsRecordStored"])
+          resolve();
+        }
+      }));
+
+      // The extrinsic should be finalized before the timeout
+      await Promise.race([result, timeoutIn(60000)]);
+
+      // assert the record was added to the xdns storage
+      const xdns = await circuitApi.rpc.xdns.fetchRecords();
+      expect(xdns.xdns_records.map((x: XdnsRecord) => x.gateway_id.toHuman())).to.not.include.deep.members(['pdot']);
+      await polkadotApi.disconnect();
+    });
+
+    after(async () => await circuitApi.disconnect());
   });
 });
