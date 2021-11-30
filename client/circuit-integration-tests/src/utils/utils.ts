7e874b8c94937f329abf8f26e4a9f1b85135e3da
MaciejBaj <maciej@t3rn.io>
2021-11-30 11:00:55 +0100

    Merge with t3rn/t3rn 30-11-21

---
+++ client/circuit-integration-tests/src/utils/utils.ts
@@ -0,0 +1,43 @@
+import { Metadata } from '@polkadot/types';
+import { GatewayABIConfig, GatewayGenesisConfig } from '@t3rn/types/dist';
+import { ApiPromise } from '@polkadot/api';
+import { Hash, RuntimeVersion } from '@polkadot/types/interfaces';
+
+export function createGatewayABIConfig(
+  api: ApiPromise,
+  hash_size: number,
+  address_length: number,
+  block_number_type_size: number,
+  decimals: number,
+  crypto: 'Ed25519' | 'Sr25519' | 'Ecdsa',
+  hasher: 'Blake2' | 'Keccak256'
+): GatewayABIConfig {
+  return api.createType('GatewayABIConfig', [
+    api.createType('u16', block_number_type_size),
+    api.createType('u16', hash_size),
+    api.createType('HasherAlgo', hasher),
+    api.createType('CryptoAlgo', crypto),
+    api.createType('u16', address_length),
+    api.createType('u16', 32),
+    api.createType('u16', decimals),
+    api.createType('Vec<StructDecl>', []),
+  ]);
+}
+
+export function createGatewayGenesisConfig(
+  metadata: Metadata,
+  runtimeVersion: RuntimeVersion,
+  genesisHash: Hash,
+  circuitApi: ApiPromise
+): GatewayGenesisConfig {
+  return circuitApi.createType('GatewayGenesisConfig', [
+    circuitApi.createType('Option<Bytes>', metadata.asV14.pallets.toHex()),
+    runtimeVersion,
+    metadata.asV14.extrinsic.version,
+    genesisHash,
+  ]);
+}
+
+export function randomGatewayId() {
+  return String.fromCharCode(...[0, 0, 0, 0].map(() => Math.floor(97 + Math.random() * 26)));
+}
---
+++ client/types/src/interfaces/types.ts
@@ -0,0 +1,9 @@
+// Auto-generated via `yarn polkadot-types-from-defs`, do not edit
+/* eslint-disable */
+
+export * from './primitives/types';
+export * from './xdns/types';
+export * from './contracts_registry/types';
+export * from './execution_delivery/types';
+export * from './volatile_vm/types';
+export * from './snowfork/types';
