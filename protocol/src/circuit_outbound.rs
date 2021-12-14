451bcd404de647d9206b1c66e7e9c9cafaa61df4
beqaabu <beqaabuladze.00@gmail.com>
2021-12-14 14:58:09 +0400

    Update protocol

---
+++ protocol/src/circuit_outbound.rs
@@ -0,0 +1,41 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use bp_messages::LaneId;
+use pallet_bridge_messages::Config;
+use sp_std::vec;
+use sp_std::vec::*;
+
+/// CircuitOutbound covers the path of message assembly and adds it to the queue dispatchable by
+pub enum CircuitOutbound<T: Config> {
+    Programmable {
+        escrow_account: T::AccountId,
+        target_account: T::AccountId,
+        message: Vec<u8>,
+        gateway_id: bp_runtime::ChainId,
+    },
+    TxOnlyExternal {
+        escrow_account: T::AccountId,
+        target_account: T::AccountId,
+        message: Vec<u8>,
+        gateway_id: bp_runtime::ChainId,
+    },
+}
+
+pub trait CircuitOutboundProtocol {}
+
+impl<T: Config> CircuitOutbound<T> {
+    pub fn send_message(&self, message: T::OutboundPayload, submitter: T::AccountId) -> Vec<u8> {
+        let origin = frame_system::RawOrigin::Signed(submitter).into();
+        let lane_id: LaneId = [0, 0, 0, 1];
+        let delivery_and_dispatch_fee: T::OutboundMessageFee = 0.into();
+
+        let _res = <pallet_bridge_messages::Pallet<T>>::send_message(
+            origin,
+            lane_id,
+            message,
+            delivery_and_dispatch_fee,
+        );
+
+        vec![]
+    }
+}
