837f4a99aee0035c7751fec2c53bcffbd7447f67
beqaabu <beqaabuladze.00@gmail.com>
2021-12-15 13:41:07 +0400

    fmt + minor fix

--- protocol/src/circuit_inbound.rs
+++ protocol/src/circuit_inbound.rs
@@ -5,7 +5,7 @@ use codec::{Decode, Encode};
 use scale_info::TypeInfo;
 #[cfg(feature = "std")]
 use serde::{Deserialize, Serialize};
-use sp_core::{U256};
+use sp_core::U256;
 use sp_runtime::RuntimeDebug;
 use sp_std::prelude::*;
 use sp_std::vec::Vec;
