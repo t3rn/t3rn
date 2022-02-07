245f519d373a1ffe7a63a743f7d22f72242334e2
Victor Aremu <victor.olorunbunmi@gmail.com>
2022-02-07 13:44:24 +0100

    fix: update bin path

--- node/run-circuit.sh
+++ node/run-circuit.sh
@@ -7,7 +7,7 @@ echo "[run-circuit.sh] Building circuit with 'cargo build'..."
 cargo build || exit
 
 echo "[run-circuit.sh] Cleaning the circuit dev database with './target/debug/circuit purge-chain --dev'..."
-yes | ./target/debug/circuit purge-chain --dev
+yes | ./target/debug/circuit-collator purge-chain --dev
 
 echo "[run-circuit.sh] Running circuit with './target/debug/circuit --dev -lruntime=debug'..."
-./target/debug/circuit --dev -lruntime=debug
+./target/debug/circuit-collator --dev -lruntime=debug
