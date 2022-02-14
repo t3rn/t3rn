2d664d21df9b33b37a6c09c54279a1cbabed8298
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2022-02-14 12:28:10 +0100

    Build specs using docker

--- docker/xcm-devnet/build-specs.sh
+++ docker/xcm-devnet/build-specs.sh
@@ -1,44 +1,12 @@
 #!/bin/bash -x
 
-git clone \
-    --depth 1 \
-    --single-branch \
-    --branch release-v0.9.13 \
-    https://github.com/paritytech/polkadot.git
-
-git clone \
-    --depth 1 \
-    --single-branch \
-    --branch release-acala-2.3.2 \
-    --recurse-submodules \
-    https://github.com/AcalaNetwork/Acala acala
-
-git clone \
-    --depth 1 \
-    --single-branch \
-    --branch development \
-    --recurse-submodules \
-    https://github.com/t3rn/x-t3rn
-
-cargo build \
-    --manifest-path ./polkadot/Cargo.toml \
-    --release \
-    --locked
-
-cargo build \
-    --manifest-path ./acala/Cargo.toml \
-    --release \
-    --locked \
-    --features with-acala-runtime
-
-cargo build \
-    --manifest-path ./x-t3rn/Cargo.toml \
-    --release \
-    --features with-parachain-runtime
+docker build -t polkadot:release-v0.9.17 -f polkadot.Dockerfile .
+docker build -t acala:release-acala-2.3.2 -f acala.Dockerfile .
+docker build -t circuit-collator:latest -f t3rn.Dockerfile .
 
 mkdir ./specs
 
-./polkadot/target/release/polkadot build-spec \
+docker run polkadot build-spec \
     --chain rococo-local \
     --disable-default-bootnode \
 > ./specs/rococo-local.json
@@ -46,48 +14,48 @@ mkdir ./specs
 sed -i 's/"nextFreeParaId": [[:digit:]]\+/"nextFreeParaId": 4000/g' \
     ./specs/rococo-local.json
 
-./polkadot/target/release/polkadot build-spec \
+docker run polkadot build-spec \
     --chain ./specs/rococo-local.json \
     --disable-default-bootnode \
     --raw \
 > ./specs/rococo-local.raw.json
 
-./acala/target/release/acala build-spec \
+docker run acala build-spec \
     --chain local \
     --disable-default-bootnode \
 > ./specs/acala.json
 
 sed -i 's/"parachainId": [[:digit:]]\+/"parachainId": 2000/g' ./specs/acala.json
 
-./acala/target/release/acala build-spec \
+docker run acala build-spec \
     --chain ./specs/acala.json \
     --disable-default-bootnode \
     --raw \
 > ./specs/acala.raw.json
 
-./x-t3rn/target/release/circuit-collator build-spec \
+docker run circuit-collator build-spec \
     --disable-default-bootnode \
 > ./specs/t3rn.json
 
 sed -i 's/"parachainId": [[:digit:]]\+/"parachainId": 3000/g' ./specs/t3rn.json
 
-./x-t3rn/target/release/circuit-collator build-spec \
+docker run circuit-collator build-spec \
     --chain ./specs/t3rn.json \
     --disable-default-bootnode \
     --raw \
 > ./specs/t3rn.raw.json
 
-./acala/target/release/acala export-genesis-state \
+docker run acala export-genesis-state \
     --chain ./specs/acala.raw.json \
 > ./specs/acala.genesis
 
-./x-t3rn/target/release/circuit-collator export-genesis-state \
+docker run circuit-collator export-genesis-state \
     --chain ./specs/t3rn.raw.json \
 > ./specs/t3rn.genesis
 
-./acala/target/release/acala export-genesis-wasm \
+docker run acala export-genesis-wasm \
     --chain ./specs/acala.raw.json \
 > ./specs/acala.wasm
 
-./x-t3rn/target/release/circuit-collator export-genesis-wasm \
+docker run circuit-collator export-genesis-wasm \
 > ./specs/t3rn.wasm
