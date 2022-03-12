cae7a7383223fba827166707aaa5843fba394d69
MaciejBaj <maciej@t3rn.io>
2022-03-12 12:04:19 -0500

    Add test side effect submission to grandpa ranges demo

--- client/grandpa-range-demo/src/index.ts
+++ client/grandpa-range-demo/src/index.ts
@@ -1,4 +1,6 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
+import Keyring from '@polkadot/keyring';
+import { createTestPairs } from "@polkadot/keyring/testingPairs";
 import { SubstrateListener } from "./listeners/substrate";
 import { types } from '@t3rn/types';
 import { fork } from 'child_process';
@@ -9,18 +11,37 @@ class GrandpaRangeRelayer {
 	targetRpc: string;	
 	circuit: ApiPromise;
 
-		
-		
 	constructor() {
 		
 	}
 
-	
 	async initTargetListener() {
 		const circuitProvider = new WsProvider("ws://127.0.0.1:9944");
 		this.circuit = await ApiPromise.create({ provider: circuitProvider, types });
 
-		console.log(this.circuit)	
+		await this.testSubmitSideEffect()
+	}
+
+	async testSubmitSideEffect() {
+		const keyring = createTestPairs({ type: 'sr25519' });
+
+		let submit_side_effects_request = await this.circuit.tx.circuit
+			.onExtrinsicTrigger(
+				[{
+					target: [0,0,0,0],
+					prize: 0,
+					ordered_at: 0,
+					encoded_action: [176, 203, 123, 178],
+					encoded_args: [keyring.alice.address, keyring.charlie.address, [1]],
+					signature: [],
+					enforce_executioner: false,
+				}],
+				1,
+				true
+			)
+			.signAndSend(keyring.alice);
+
+		console.log(submit_side_effects_request)
 	}
 
 
