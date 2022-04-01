2cfa5db271ff3e3fb5f4743b6c88ae585c853819
petscheit <etscheitp@gmail.com>
2022-04-01 13:35:15 +0200

    adds event data deconstructor

---
+++ client/executor/utils/sideEffectInterfaces.ts
@@ -0,0 +1,41 @@
+export type SideEffect = {
+    sender: any,
+    xtxId: any,
+    target: any,
+    prize: any,
+    encodedAction: any,
+    signature: any,
+    enforceExecutioner: any,
+    encodedArgs: Transfer
+}
+
+export type Transfer = {
+    from: any,
+    to: any,
+    amount: any
+}
+
+export const deconstruct = (args: any): SideEffect => {
+    console.log(args.toHuman())
+    let sideEffect: SideEffect = {
+        sender: args[0],
+        xtxId: args[1],
+        target: args[2][0].target,
+        prize: args[2][0].prize,
+        encodedAction: args[2].encodedAction,
+        signature: args[2][0].signature,
+        enforceExecutioner: args[2][0].enforceExecutioner,
+        encodedArgs: deconstructArgs(args[2][0].encodedArgs)
+    }
+
+    return sideEffect;
+}
+
+const deconstructArgs = (args: any): Transfer => {
+    let transfer: Transfer = {
+        from: args[0],
+        to: args[1],
+        amount: args[2]
+    }
+    return transfer
+} 
