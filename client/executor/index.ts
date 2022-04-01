f5383cab1efcf3409f2176255ceb4b1e5a71c821
petscheit <etscheitp@gmail.com>
2022-04-01 12:36:51 +0200

    adds dynamic relayer init

--- client/executor/index.ts
+++ client/executor/index.ts
@@ -1,20 +1,45 @@
+require('dotenv').config()
 import CircuitListener from "./circuit/listener";
+import SubstrateRelayer from "./gateways/substrate/relayer";
 import config from "./config.json"
 
 class Executor {
     circuitListener: CircuitListener;
+    gatewayInstances: any;
 
     constructor() {
         this.circuitListener = new CircuitListener();
     }
 
-    async init() {
+    async setup() {
         await this.circuitListener.setup(config.circuit.rpc)
         await this.circuitListener.start()
+        await this.initializeGateways()
+    }
+
+    async initializeGateways() {
+        let gatewayInstances = {};
+        for(let i = 0; i < config.gateways.length; i++) {
+            const entry = config.gateways[i]
+            if(entry.type === "substrate") {
+                let instance = new SubstrateRelayer();
+                await instance.setup(entry.rpc)
+                gatewayInstances[entry.id] = instance;
+            }
+        }
+        console.log(gatewayInstances)
+        this.gatewayInstances = gatewayInstances;
+    }
+
+    async start() {
         this.circuitListener.on('NewSideEffect', (data) => {
             console.log(data)
         })
     }
+
+    // async sideEffectRouter(eventData: any) {
+
+    // }
 
     
 
@@ -22,4 +47,4 @@ class Executor {
 
 
 let exec = new Executor();
-exec.init()
+exec.setup()
