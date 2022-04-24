fc13b61c732c8d5b1f106ab7ce9e92a3fc3be647
petscheit <etscheitp@gmail.com>
2022-04-25 01:39:02 +0200

    updates ranger architekture to support dynamic instances and new proof formats

--- client/grandpa-ranger/README.md
+++ client/grandpa-ranger/README.md
@@ -1,6 +1,27 @@
-# GRANDPA Range Demo
+# GRANDPA Ranger
+PoC of the GRANDPA ranger, which collects and submits headers to the circuit.
 
-``` 
-npm run standalone
-npm run start
-```
+## Quick life-cycle Explaination:
+- Collect a GRANDPA justification for the relaychain and submit it via `submit_finality_proof` on circuit
+- Now, we can use `submit_header_range` and pass an aribrary sized header_range, using the GRANDPA justification header, as an anchor and going backwards with the range
+- At the same time, we can use the relaychain header, to verify its parachains header, using `submit_parachain_header`.
+- Once that is complete, we can use the new header, as an anchor header for submitting via `submit_header_range`
+
+## To Run:
+- run `yarn`
+- add gateways in `config.json`
+- run `yarn start`
+
+It is important that the defined gateways are registered and operational on the circuit!!
+
+## Current Issues:
+A couple of things that need to be fixed for a smooth experience.
+
+### Duplicate Headers:
+The headerlistener sometimes receives the same block twice. Currently, the second header with the same number is ignored. This sometimes leads to the header-chains to be invalid, and thereby to rejection on the circuit (`submit_header_range`). We need to ensure that the received headers are valid (aka `headers[n].hash() === headers[n + 1].parentHash`)
+
+### Circuit error handling:
+Errors are sometimes not handled correctly, which can lead to header gaps to develop. Errors can be handled gracefully by the gateway instance never having `finalize()` called. This prevents header from being removed, and results in them being added with the next range. If the errors are not detected correctly, `finalize()` is called, even though the headers where never added. This needs to be addressed in the relayer file (or potentially the circuit)
+
+### AuthoritySetChanges:
+AuthoritySet cahnges are currently not handled. I had some issues finding the correct justification to handle the update, which is why I changed the approach with the justification in general. In realychain.ts we now store all incoming justifications. We should then be able to detect authoritySetUpdates by decoding them. I have left some comments in the place where this should be implemented.
--- client/grandpa-ranger/src/index.ts
+++ client/grandpa-ranger/src/index.ts
@@ -1,117 +1,146 @@
-import Listener from './listeners/relaychain'
-import Relayer from './relayer'
+require('dotenv').config()
+import Relayer from './relayer';
+import config from "../config.json";
+import Relaychain from "./listeners/relaychain";
+import Parachain from './listeners/parachain';
 
-async function main() {
-  const listener = new Listener()
-  const relayer = new Relayer()
+class InstanceManager {
+	// handles circuit communitcation
+  	relayer: Relayer;
+	// stores relay/parachains ranger instances
+	gateways: {
+		[id: string]: any;
+	} = {};
 
-  Listener.debug('🦅 remote endpoint', listener.endpoint)
-  Relayer.debug('⚡ circuit endpoint', relayer.circuitEndpoint)
-  Listener.debug('⛩️  gateway id', listener.gatewayId.toString())
-  Listener.debug('🏔️  range size', listener.rangeSize)
+	// used for mapping parachain instances to its respective relaychain
+	relayLookup: {
+		[id: string]: string[];
+	} = {};
 
-  // Relayer.debug('initializing...')
-  await relayer.init()
-  await listener.init()
+	constructor() {
+		this.relayer = new Relayer();
+	}
 
-  relayer.on("RangeSubmitted", index => {
-    listener.removeAddedHeaders(index)
-  })
+	async setup() {
+		await this.relayer.setup(config.circuit.rpc)
+		await this.initializesRelay()
+		await this.initialieParachains()
+		console.log("Components Initialzed")
+	}
 
-  listener.on('range', relayer.submit.bind(relayer))
+	// initialize relaychain instances as defined in config.json
+	async initializesRelay() {
+		for (let i = 0; i < config.relaychains.length; i++) {
+			const entry = config.relaychains[i]
+		
+			let instance = new Relaychain();
+			await instance.setup(entry.rpc, entry.id)
+
+			// forward SubmitFinalityProof request to relayer
+			instance.on("SubmitFinalityProof", (data: any) => {
+				console.log("Received SubmitFinalityProof")
+				this.relayer.submitFinalityProof(
+					data.gatewayId,
+					data.justification,
+					data.anchorHeader,
+					data.anchorIndex
+				)
+			})
+
+			// forward SubmitHeaderRange request to relayer
+			instance.on("SubmitHeaderRange", (data: any) => {
+				console.log("Received SubmitHeaderRange")
+				this.relayer.submitHeaderRange(
+					data.gatewayId,
+					data.range,
+					data.anchorHeader,
+					data.anchorIndex
+				)
+			})
+
+			instance.start()
+
+			// store relaychain instance in mapping
+			this.gateways[entry.id] = instance;
+			this.relayLookup[entry.id] = [];
+		}
+	}
+
+	// initialize parachain instances as defined in config.json
+	async initialieParachains() {
+		for (let i = 0; i < config.parachains.length; i++) {
+			const entry = config.parachains[i]
+
+			this.relayLookup[entry.relaychainId].push(entry.id);
+
+			let instance = new Parachain();
+			await instance.setup(entry.rpc, entry.id, entry.parachainId)
+
+			// forward SubmitHeaderRange request to relayer
+			instance.on("SubmitHeaderRange", (data: any) => {
+				console.log("Received SubmitHeaderRange")
+				this.relayer.submitHeaderRange(
+					data.gatewayId,
+					data.range,
+					data.anchorHeader,
+					data.anchorIndex
+				)
+			})
+
+			// store instance in mapping
+			this.gateways[entry.id] = instance;
+		}
+	}
+
+	// routes relayer notification to respective function
+	async initializeEventListeners() {
+		// once a relaychains finality proof has been submitted
+		this.relayer.on("FinalityProofSubmitted", (data: any) => {
+			console.log("FinalityProofSubmitted")
+			// Once the relaychain has called submitFinalityProof, we can add relaychain headers
+			this.triggerParachainHeaderVerification(data)
+			// the relaychain can submit a header range immediatly
+			this.gateways[data.gatewayId].submitHeaderRange(data.anchorIndex)
+		})
+
+		// once the headerRange has been submitted, we remove the header from respective instance by using anchorIndex
+		this.relayer.on("SubmittedHeaderRange", (data: any) => {
+			console.log("SubmittedHeaderRange")
+			this.gateways[data.gatewayId].finalize(data.anchorIndex);
+		})
+		
+		// once a parachain has submitted a header, a headerRange can be passed
+		this.relayer.on("ParachainHeaderSubmitted", (data: any) => {
+			console.log("ParachainHeaderSubmitted");
+			this.gateways[data.gatewayId].submitHeaderRange(data.anchorHash)
+		})
+  	}
+
+	// iterates through a relaychains parachain and submittes header 
+	async triggerParachainHeaderVerification(data: any) {
+		// we iterate over a relaychains parachains
+		let promises: Promise<any>[] = this.relayLookup[data.gatewayId].map(entry => {
+			return new Promise(async (res, rej) => {
+				//generate a storage read proof for the header we're looking to verify
+				let [storageProof, headerHash] = await this.gateways[data.gatewayId].getStorageProof(data.blockHash, this.gateways[entry].parachainId);
+				// this.gateways[entry]
+				this.relayer.submitParachainHeader(
+					entry,
+					data.blockHash,
+					storageProof.toJSON().proof,
+					headerHash //this is the encoded parachain header we are proving with this transaction
+				)
+				res;
+			})
+		})
+
+		Promise.all(promises)
+			.then(() => console.log("ran promises"))
+	}
 }
 
-main()
-
-
-// require('dotenv').config()
-// import CircuitRelayer from "./circuit/relayer";
-// import SubstrateRelayer from "./gateways/substrate/relayer";
-// import config from "./config.json"
-// import { colors } from "./utils/helpers";
-// import { SideEffect } from "./utils/types";
-// import { ExecutionManager } from "./utils/executionManager";
-// import chalk from 'chalk';
-
-// class InstanceManager {
-//   circuitListener: CircuitListener;
-//   circuitRelayer: CircuitRelayer;
-//   executionManager: ExecutionManager;
-
-//   instances: {
-//     [id: string]: SubstrateRelayer
-//   } = {};
-//   color: string;
-
-//   constructor() {
-//     this.circuitRelayer = new CircuitRelayer();
-//     this.executionManager = new ExecutionManager();
-
-//     this.color = colors[0];
-//   }
-
-
-//   async setup() {
-//     await this.circuitListener.setup(config.circuit.rpc)
-//     await this.circuitRelayer.setup(config.circuit.rpc, colors[1])
-//     await this.circuitListener.start()
-//     await this.initializeGateways()
-//     this.log("Components Initialzed")
-//   }
-
-//   async initializeGateways() {
-//     for (let i = 0; i < config.gateways.length; i++) {
-//       const entry = config.gateways[i]
-//       if (entry.type === "substrate") {
-//         let instance = new SubstrateRelayer();
-//         await instance.setup(entry.rpc, entry.name, colors[i + 2])
-
-
-//         instance.on("SideEffectExecuted", (id: string) => {
-//           console.log("SideEffectExecuted")
-//           this.executionManager.sideEffectExecuted(id)
-//         })
-
-//         // setup in executionManager
-//         this.executionManager.addGateway(entry.id);
-//         // store relayer instance locally
-//         this.instances[entry.id] = instance;
-//       }
-//     }
-//   }
-
-//   async initializeEventListeners() {
-//     this.circuitListener.on('NewSideEffect', (data: SideEffect) => {
-//       console.log('NewSideEffect')
-//       this.executionManager.addSideEffect(data);
-//     })
-
-//     this.executionManager.on('ExecuteSideEffect', sideEffect => {
-//       console.log('ExecuteSideEffect')
-//       this.instances[sideEffect.getTarget()].executeTx(sideEffect)
-//     })
-
-//     this.executionManager.on("ConfirmSideEffects", (sideEffects: SideEffect[]) => {
-//       console.log("ConfirmSideEffect")
-//       this.circuitRelayer.confirmSideEffects(sideEffects)
-//     })
-
-//     this.circuitRelayer.on("SideEffectConfirmed", (id: string) => {
-//       console.log("SideEffectConfirmed")
-//       this.executionManager.finalize(id);
-//     })
-
-//     this.circuitListener.on('NewHeaderRangeAvailable', (gatewayId: string, blockHeight: number) => {
-//       console.log('NewHeaderRangeAvailable')
-//       this.executionManager.updateGatewayHeight(gatewayId, blockHeight);
-//     })
-
-
-//   }
-// }
-
-// (async () => {
-//   let exec = new InstanceManager();
-//   await exec.setup()
-//   exec.initializeEventListeners()
-// })()
+(async () => {
+  let exec = new InstanceManager();
+  await exec.setup()
+  exec.initializeEventListeners()
+})()
