f647d4797e92a6182122852dc1d26b4004eda03a
Don <donovan@t3rn.io>
2022-05-04 09:55:23 +0100

    feat: lookup an author from the store

--- runtime/standalone/src/lib.rs
+++ runtime/standalone/src/lib.rs
@@ -7,6 +7,7 @@
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
+use codec::Decode;
 use pallet_grandpa::{
     fg_primitives, AuthorityId as GrandpaId, AuthorityList as GrandpaAuthorityList,
 };
@@ -28,7 +29,7 @@ use sp_version::NativeVersion;
 use sp_version::RuntimeVersion;
 
 // A few exports that help ease life for downstream crates.
-use frame_support::traits::{Contains, IsSubType};
+use frame_support::traits::IsSubType;
 pub use frame_support::{
     construct_runtime, parameter_types,
     traits::{
@@ -44,7 +45,7 @@ pub use frame_support::{
 pub use pallet_balances::Call as BalancesCall;
 pub use pallet_timestamp::Call as TimestampCall;
 use pallet_transaction_payment::CurrencyAdapter;
-use sp_core::crypto::UncheckedFrom;
+use sp_runtime::transaction_validity::InvalidTransaction;
 #[cfg(any(feature = "std", test))]
 pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
@@ -301,11 +302,16 @@ where
     ) -> Result<Self::LiquidityInfo, frame_support::pallet_prelude::TransactionValidityError> {
         let call: &<T as frame_system::Config>::Call = call;
 
-        if let Some(inner_call) = call.is_sub_type() {
-            // TODO: EXTRACT AUTHOR
+        if let Some(pallet_3vm_contracts::Call::call { dest, .. }) = call.is_sub_type() {
+            let author = Contracts::get_author(dest)
+                .ok_or(frame_support::pallet_prelude::TransactionValidityError::Invalid(InvalidTransaction::Call))?;
+
             <CurrencyAdapter<C, OU> as pallet_transaction_payment::OnChargeTransaction<T>>::withdraw_fee(
                 who, call, info, fee, tip,
-            ).map(|info| (Some(who.clone()), info))
+            ).map(|info| {
+                let opaque_author = T::AccountId::decode(&mut author.as_ref()).unwrap(); // TODO: remove
+                (Some(opaque_author), info)
+            })
         } else {
             <CurrencyAdapter<C, OU> as pallet_transaction_payment::OnChargeTransaction<T>>::withdraw_fee(
                 who, call, info, fee, tip,
