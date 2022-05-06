9661f3f4ef96a9f970a5007aa96a606fffc195f4
MaciejBaj <maciej@t3rn.io>
2022-05-06 21:30:35 +0200

    Remove confirm_parachain_header from Portal primitive trait

--- primitives/src/circuit_portal.rs
+++ primitives/src/circuit_portal.rs
@@ -1,16 +1,8 @@
-use crate::ProofTriePointer;
 use snowbridge_core::Verifier;
 use sp_std::vec::Vec;
-use sp_trie::StorageProof;
 
 pub trait CircuitPortal<T: frame_system::Config> {
     type EthVerifier: Verifier;
-
-    fn confirm_parachain_header(
-        gateway_id: [u8; 4],
-        block_hash: Vec<u8>,
-        proof: StorageProof,
-    ) -> Result<Vec<u8>, &'static str>;
 
     fn confirm_event_inclusion(
         gateway_id: [u8; 4],
