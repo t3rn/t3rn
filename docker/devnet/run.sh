a40067e95741f61d9e086397d8766916b6a47c4a
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2022-05-24 19:27:49 +0200

    chore: xbi devnet

--- docker/devnet/run.sh
+++ docker/devnet/run.sh
@@ -14,11 +14,6 @@ build_docker_images() {
       -t circuit-collator:update_v0.9.19 \
       -f t3rn.Dockerfile ../..
   fi
-  if ! docker inspect parachain-collator:polkadot-v0.9.19-keyed > /dev/null; then
-    DOCKER_BUILDKIT=1 docker build \
-      -t parachain-collator:polkadot-v0.9.19-keyed \
-      -f pchain.Dockerfile .
-  fi
 }
 
 keygen() {
@@ -80,7 +75,7 @@ build_para_chain_specs() {
       --raw \
   > ./specs/t3rn.raw.json
   ## gen pchain chain spec
-  docker run parachain-collator:polkadot-v0.9.19-keyed build-spec \
+  docker run circuit-collator:update_v0.9.19 build-spec \
       --disable-default-bootnode \
   > ./specs/pchain.json
   # set parachain id(s)
@@ -98,7 +93,7 @@ build_para_chain_specs() {
       -i ./specs/pchain.json
   docker run \
       -v "$(pwd)/specs:/usr/local/etc" \
-      parachain-collator:polkadot-v0.9.19-keyed \
+      circuit-collator:update_v0.9.19 \
       build-spec \
       --chain /usr/local/etc/pchain.json \
       --disable-default-bootnode \
@@ -115,7 +110,7 @@ build_para_genesis_states() {
   > ./specs/t3rn.genesis
   docker run \
       -v "$(pwd)/specs:/usr/local/etc" \
-      parachain-collator:polkadot-v0.9.19-keyed \
+      circuit-collator:update_v0.9.19 \
       export-genesis-state \
       --chain /usr/local/etc/pchain.raw.json \
   > ./specs/pchain.genesis
@@ -124,11 +119,7 @@ build_para_genesis_states() {
 build_para_wasm_runtimes() {
   docker run circuit-collator:update_v0.9.19 export-genesis-wasm \
   > ./specs/t3rn.wasm
-  docker run \
-      -v "$(pwd)/specs:/usr/local/etc" \
-      parachain-collator:polkadot-v0.9.19-keyed \
-      export-genesis-wasm \
-      --chain /usr/local/etc/pchain.raw.json \
+  docker run circuit-collator:update_v0.9.19 export-genesis-wasm \
   > ./specs/pchain.wasm
 }
 
@@ -160,24 +151,24 @@ set_keys() {
     --suri "$t3rn2_phrase" \
     --key-type aura
   docker exec \
-    -u para \
+    -u t3rn \
     pchain1 \
-    parachain-collator \
+    circuit-collator \
     key \
     insert \
-    --base-path /para/data \
-    --chain /para/pchain.raw.json \
+    --base-path /t3rn/data \
+    --chain /t3rn/pchain.raw.json \
     --scheme Sr25519 \
     --suri "$pchain1_phrase" \
     --key-type aura
   docker exec \
-    -u para \
+    -u t3rn \
     pchain2 \
-    parachain-collator \
+    circuit-collator \
     key \
     insert \
-    --base-path /para/data \
-    --chain /para/pchain.raw.json \
+    --base-path /t3rn/data \
+    --chain /t3rn/pchain.raw.json \
     --scheme Sr25519 \
     --suri "$pchain2_phrase" \
     --key-type aura
@@ -238,7 +229,8 @@ devnet|dev|net)
   mkdir -p ./data/{alice,bob,charlie,dave,eve,t3rn1,t3rn2,pchain1,pchain2}
   build_docker_images
   docker-compose up > /dev/null &
-  sleep 13s # allow node startup ~ basepath/datadir/keystore creation
+  # allow node startup ~ basepath/datadir/keystore creation
+  npx --yes wait-port -t 60000 localhost:9933
   echo "⛓️ setting up collator keystores and initializing parachain onboarding..."
   set_keys
   onboard
