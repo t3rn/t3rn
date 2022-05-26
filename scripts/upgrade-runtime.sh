4cd3d8f3f7216238ff655f9d3ecd4187a1fbfbde
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2022-05-26 17:20:59 +0200

    impl: use srtool-cli to compile runtime wasm and its code hash

--- scripts/upgrade-runtime.sh
+++ scripts/upgrade-runtime.sh
@@ -2,52 +2,78 @@
 
 set -xEe
 
-if [[ -z "$1" || -z $2 ]]; then
-  echo "usage: $0 'sudo secret' \$provider \$wasm"
-  # fx: ./upgrade-runtime.sh 'sudo secret' wss://dev.net.t3rn.io /tmp/run-it-up.wasm
+if [[ -z "$1" || -z $2 || -z $3 ]]; then
+  echo "usage: $0 'sudo secret' \$provider \$tag"
+  # fx: ./upgrade-runtime.sh 'sudo secret' wss://dev.net.t3rn.io v3.3.3
   exit 1
 fi
 
 sudo_secret="$1"
 provider=$2
-wasm=$3
+tag=$3
 
-read -n 1 -p "runtime tested and incremented spec version? (y/n) " answer
+git checkout $tag
+
+root_dir=$(git rev-parse --show-toplevel)
+
+if ! cargo install --list | grep -q 'srtool-cli v0.8.0'; then
+  cargo install \
+    --git https://github.com/chevdor/srtool-cli \
+    --tag v0.8.0
+fi
+
+echo "compiling runtime wasm..."
+
+report="$( \
+  srtool build \
+    --profile release \
+    --runtime-dir runtime/parachain \
+    --package circuit-parachain-runtime \
+    --json \
+    $root_dir \
+)"
+
+# left trimming nonjson
+report="{${report#*\{}"
+wasm=$(jq -r .runtimes.compact.wasm <<<"$report")
+hash=$(jq -r .runtimes.compact.blake2_256 <<<"$report")
+hex_wasm_runtime=$(mktemp)
+# xxd from vim
+printf "0x$(cat $wasm | tr -d '\n' | xxd -p | tr -d '\n')" > $hex_wasm
+authorize_upgrade_call_data="0x0102${hash#0x}"
+
+# fetch authoring_version, spec_version, impl_version, and transaction_version from live chain
+# grep authoring_version, spec_version, impl_version, and transaction_version from tagged files
+# mk sure authoring_version, spec_version, impl_version, and transaction_version incremented
+
+# if possible check whether any storage layout has changed
+# if yes assert migrations are included
+
+# assert that benchmarks are up2date
+# ./target/release/node-template benchmark \
+#     --chain dev \               # Configurable Chain Spec
+#     --execution wasm \          # Always test with Wasm
+#     --wasm-execution compiled \ # Always used `wasm-time`
+#     --pallet pallet_example \   # Select the pallet
+#     --extrinsic '*' \          # Select the benchmark case name, using '*' for all
+#     --steps 20 \                # Number of steps across component ranges
+#     --repeat 10 \               # Number of times we repeat a benchmark
+#     --json-file=raw.json \      # Optionally output json benchmark data to a file
+#     --output ./                 # Output results into a Rust file
+
+# https://docs.substrate.io/rustdocs/latest/sp_version/struct.RuntimeVersion.html
+read -n 1 -p "e2e-tested on rococo-local?
+runtime benchmarked?
+storage migrated?
+authoring_version incremented?
+spec_version incremented?
+impl_version incremented?
+transaction_version incremented?
+(y/n) " answer
 
 echo
 
 if [[ "${answer,,}" != "y" ]]; then exit 1; fi
-
-if [[ -z $wasm ]]; then
-  echo "compiling runtime wasm..."
-  wasm=$(mktemp)
-  cargo run \
-    --release \
-    --manifest-path \
-    ../node/parachain/Cargo.toml \
-    -- \
-    export-genesis-wasm \
-  > $wasm
-fi
-
-tmp_dir=$(mktemp -d)
-wd=$(pwd)
-cd $tmp_dir
-npm i blake2b
-call_data=0x0102$( \
-  node --print " \
-    const blake2b = require('blake2b'); \
-    const magicBytes = Buffer.from([1, 3]); \
-    const wasmHex = fs.readFileSync('$wasm', 'utf8');
-    const wasmBuf = Buffer.from(wasmHex.slice(2), 'hex'); \
-    const digest = blake2b(32).update(wasmBuf).digest(); \
-    const prefixed = Buffer.concat([magicBytes, digest]); \
-    const hash = blake2b(32).update(prefixed).digest('hex'); \
-    hash \
-    " \
-)
-echo "call_data: $call_data"
-cd $wd
 
 echo "authorizing runtime upgrade..."
 
@@ -56,12 +82,12 @@ npx --yes @polkadot/api-cli@beta \
   --sudo \
   --seed "$sudo_secret" \
   tx.parachainSystem.authorizeUpgrade \
-  $call_data
+  $authorize_upgrade_call_data
 
 echo "enacting runtime upgrade..."
 
 npx @polkadot/api-cli@beta \
   --ws $provider \
   --seed "$sudo_secret" \
-  --params $wasm \
+  --params $hex_wasm_runtime \
   tx.parachainSystem.enactAuthorizedUpgrade
