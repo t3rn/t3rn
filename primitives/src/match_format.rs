b1f3ed12a23088c9999cddc5387e2eb51ccb023e
beqaabu <beqaabuladze.00@gmail.com>
2022-06-23 14:47:37 +0400

    feat: match side effect to encoded action

--- primitives/src/match_format.rs
+++ primitives/src/match_format.rs
@@ -1,4 +1,6 @@
 use sp_std::{vec, vec::*};
+use codec::Encode;
+use t3rn_types::Bytes;
 
 pub type StrLike = Vec<u8>;
 
@@ -51,6 +53,17 @@ pub fn trim_whitespace(input_string: StrLike) -> StrLike {
         }
     }
     result
+}
+
+pub fn match_side_effect(kind: &StrLike) -> Result<Bytes, &'static str> {
+    match &kind[..] {
+        b"Transfer" => Ok(b"tran".encode()),
+        b"Call" => Ok(b"call".encode()),
+        b"Swap" => Ok(b"swap".encode()),
+        b"MultiTransfer" => Ok(b"mult".encode()),
+        b"AddLiquidity" => Ok(b"aliq".encode()),
+        _ => Err("Unknown side effect kind"),
+    }
 }
 
 pub fn match_signature(signature: StrLike) -> Result<(StrLike, Vec<StrLike>), &'static str> {
@@ -345,5 +358,12 @@ pub mod tests {
                 vec![b"D".to_vec()],
             ])
         )
+    }
+
+    #[test]
+    fn test_match_signature() {
+        let sig = "Transfer(from,to,value)";
+        let decoded_sig = match_signature(sig.as_bytes().to_vec());
+        println!("{:?}", decoded_sig);
     }
 }
