e29af27944e4934da8f528e8a1d73ec9e6a9231b
petscheit <etscheitp@gmail.com>
2022-06-30 11:58:02 +0200

    feature: detects authority updates and triggers submission to keep circuit in sync

--- client/grandpa-ranger/src/util.ts
+++ client/grandpa-ranger/src/util.ts
@@ -4,66 +4,85 @@ import { ApiPromise } from "@polkadot/api"
 import { BN } from "@polkadot/util"
 
 const registry = new TypeRegistry()
-const type = { type: 'GrandpaJustification<Header>' }
-export const decodeJustification = (data: any) => {
-        registry.register(type);
-        const res = createType(registry, type.type as any, data)
+const justification = { type: 'GrandpaJustification<Header>' }
+export const decodeJustificationTarget = (data: any) => {
+    const justification = decodeJustification(data)
 
-        return res.commit.targetNumber.toNumber()
-    }
+    return justification.commit.targetNumber.toNumber()
+}
+
+// gets events of a specific block, and checks if authoritySetUpdate is included.
+// it would be more efficient to listen for the NewAuthorities event directly, but I'm unable to decode the block number currently.
+export const containsAuthoritySetUpdate= async (api: any, blockNumber: number) => {
+    const hash = await api.rpc.chain.getBlockHash(blockNumber);
+    const notifications = await api.query.system.events.at(hash);
+    return notifications.some(noti => noti.event.method === "NewAuthorities")
+}
+
+export const decodeJustification = (data: any) => {
+    registry.register(justification);
+    const res = createType(registry, justification.type as any, data)
+    return res
+}
+
+// not ideal, but I'm unable to use the "NewAuthorites" event, because I'm unable to figure out in which block the event was emitted
+export const decodeAuthoritySet = (data: any) => {
+    const justification = decodeJustification((data))
+    return justification.commit.precommits.map(entry => entry.id.toHex()).sort();
+}
 
 export function formatEvents(
-  events: { event: { section: string; method: string; data: any } }[]
+    events: { event: { section: string; method: string; data: any } }[]
 ): string[] {
-  return events.map(
-    ({ event: { data, method, section } }) =>
-      `${section}.${method} ${data.toString()}`
-  )
+    return events.map(
+        ({ event: { data, method, section } }) =>
+            `${section}.${method} ${data.toString()}`
+    )
 }
 
 export function decodeHeaderNumber(data: string) {
-  // removes the Vec Decoding, bit hacky
-  if (data.slice(0, 6) === "0xe902") {
-    data = "0x" + data.split("e902")[1]
-  }
+    // removes the Vec Decoding, bit hacky
+    if (data.slice(0, 6) === "0xe902") {
+        data = "0x" + data.split("e902")[1]
+    }
 
-  const typeObject = { type: "Block::Header" }
-  registry.register(typeObject)
-  const res: any = createType(registry, typeObject.type, data)
-  return res.number.toNumber()
+    const typeObject = { type: "Block::Header" }
+    registry.register(typeObject)
+    const res: any = createType(registry, typeObject.type, data)
+    return res.number.toNumber()
 }
 
 export async function fetchNonce(
-  api: ApiPromise,
-  address: string
+    api: ApiPromise,
+    address: string
 ): Promise<BN> {
-  return api.rpc.system.accountNextIndex(address)
+    return api.rpc.system.accountNextIndex(address)
 }
 
 export async function fetchMissingHeaders(
-  api: ApiPromise,
-  headers: (Header | number)[],
-  until?: number
+    api: ApiPromise,
+    headers: (Header | number)[],
+    until?: number
 ): Promise<Header[]> {
-  let _headers
-  if (until) {
-    _headers = Array.from(headers)
-    let tail =
-      typeof headers[headers.length - 1] === "number"
-        ? (headers[headers.length - 1] as number)
-        : (headers[headers.length - 1] as Header).number.toNumber()
-    while (++tail <= until) _headers.push(tail)
-  } else {
-    _headers = headers
-  }
-  return Promise.all(
-    _headers.map(async h => {
-      if (typeof h === "number") {
-        const blockHash = await api.rpc.chain.getBlockHash(h)
-        return api.rpc.chain.getHeader(blockHash)
-      } else {
-        return h
-      }
-    })
-  )
+    let _headers
+    if (until) {
+        _headers = Array.from(headers)
+        let tail =
+            typeof headers[headers.length - 1] === "number"
+                ? (headers[headers.length - 1] as number)
+                : (headers[headers.length - 1] as Header).number.toNumber()
+        while (++tail <= until) _headers.push(tail)
+    } else {
+        _headers = headers
+    }
+    return Promise.all(
+        _headers.map(async h => {
+            if (typeof h === "number") {
+                const blockHash = await api.rpc.chain.getBlockHash(h)
+                return api.rpc.chain.getHeader(blockHash)
+            } else {
+                return h
+            }
+        })
+    )
 }
