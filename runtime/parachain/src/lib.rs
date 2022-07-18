b571b27dfa9365f3975eb6efdb520f8a5d1d82f3
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2022-07-18 12:33:50 +0200

    chore: bump runtime version fields for the first t0rn upgrade

--- runtime/parachain/src/lib.rs
+++ runtime/parachain/src/lib.rs
@@ -184,11 +184,11 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // https://docs.rs/sp-version/latest/sp_version/struct.RuntimeVersion.html
     spec_name: create_runtime_str!("t0rn"),
     impl_name: create_runtime_str!("Circuit Collator"),
-    authoring_version: 1,
-    spec_version: 2,
-    impl_version: 2,
+    authoring_version: 2,
+    spec_version: 3,
+    impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 1,
+    transaction_version: 2,
     // https://github.com/paritytech/cumulus/issues/998
     // https://github.com/paritytech/substrate/pull/9732
     // https://github.com/paritytech/substrate/pull/10073
