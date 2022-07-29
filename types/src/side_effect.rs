f44e6c086543144f2c5a0e89b19a44c209929c68
beqaabu <beqaabuladze.00@gmail.com>
2022-07-29 11:30:42 +0400

    feat: decode side effect from encoded bytes

--- types/src/side_effect.rs
+++ types/src/side_effect.rs
@@ -1,6 +1,7 @@
 use codec::{Decode, Encode};
+use num::Zero;
 use scale_info::{
-    prelude::{fmt::Debug, vec::Vec},
+    prelude::{fmt::Debug, vec, vec::Vec},
     TypeInfo,
 };
 
@@ -33,6 +34,91 @@ impl<
 
     pub fn id_as_bytes<Hasher: sp_core::Hasher>(id: <Hasher as sp_core::Hasher>::Out) -> Bytes {
         id.as_ref().to_vec()
+    }
+}
+
+// Decode the side effect from encoded Chain.
+impl<
+        AccountId: Encode,
+        BlockNumber: Ord + Copy + Zero + Encode,
+        BalanceOf: Copy + Zero + Encode + Decode,
+    > From<&Vec<u8>> for SideEffect<AccountId, BlockNumber, BalanceOf>
+{
+    fn from(bytes: &Vec<u8>) -> Self {
+        let (action, args) = match_action(bytes[1], &bytes[2..]).unwrap();
+        SideEffect::<AccountId, BlockNumber, BalanceOf> {
+            target: match_target(bytes[0]).unwrap(),
+            prize: Zero::zero(),
+            ordered_at: Zero::zero(),
+            encoded_action: action.into(),
+            encoded_args: args,
+            signature: vec![],
+            enforce_executioner: None,
+        }
+    }
+}
+
+fn match_target(id: u8) -> Result<[u8; 4], &'static str> {
+    match id {
+        0 => Ok(*b"ksma"),
+        1 => Ok(*b"pdot"),
+        2 => Ok(*b"karu"),
+        3 => Ok(*b"t3rn"),
+        _ => Err("Invalid target Id"),
+    }
+}
+
+fn match_action(id: u8, bytes: &[u8]) -> Result<(Bytes, Vec<Bytes>), &'static str> {
+    match id {
+        0 => {
+            let mut args: Vec<Bytes> = bytes[0..64]
+                .chunks(32)
+                .map(|chunk| chunk.to_vec())
+                .collect(); //from, to
+            args.push(bytes[64..].to_vec()); //amount
+            Ok((b"tran".encode(), args))
+        },
+        1 => {
+            let mut args: Vec<Bytes> = bytes[0..64]
+                .chunks(32)
+                .map(|chunk| chunk.to_vec())
+                .collect(); //from, to
+            args.push(bytes[64..80].to_vec()); //amount
+            args.push(bytes[80..].to_vec()); //asset; not sure maybe 2 bytes, maybe 4
+
+            Ok((b"mult".encode(), args))
+        },
+        2 => {
+            let mut args: Vec<Bytes> = bytes[0..160]
+                .chunks(32)
+                .map(|chunk| chunk.to_vec())
+                .collect(); //from, to, asset_lef, asset_right
+            args.push(bytes[160..176].to_vec()); //amount_left
+            args.push(bytes[176..192].to_vec()); //amount_right
+            args.push(bytes[192..].to_vec()); //amount liquidity token
+
+            Ok((b"aliq".encode(), args))
+        },
+        3 => {
+            let mut args: Vec<Bytes> = bytes[0..64]
+                .chunks(32)
+                .map(|chunk| chunk.to_vec())
+                .collect(); //from, to
+            args.push(bytes[64..80].to_vec()); //amount_from
+            args.push(bytes[80..96].to_vec()); //amount_to
+            args.push(bytes[96..128].to_vec()); //asset_from
+            args.push(bytes[128..].to_vec()); //asset_to
+
+            Ok((b"swap".encode(), args))
+        },
+        4 => {
+            let mut args: Vec<Bytes> = Vec::new();
+            args.push(bytes[0..32].to_vec()); //caller
+            args.push(bytes[32..].to_vec()); //vm
+
+            Ok((b"call".encode(), args))
+        },
+        _ => Err("Invalid action Id"),
     }
 }
 
@@ -186,5 +272,38 @@ mod tests {
         };
 
         assert_eq!(empty_side_effect, SideEffect::default(),);
+    }
+
+    #[test]
+    fn from_encoded_chain_to_side_effect() {
+        let v: Vec<u8> = vec![
+            1, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+            5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+            6, 6, 6, 6, 6, 6, 6, 6, 100, 0, 0, 0,
+        ];
+        let s = SideEffect::<[u8; 2], u32, u32>::from(&v);
+
+        assert_eq!(
+            s,
+            SideEffect {
+                target: [112, 100, 111, 116],
+                prize: 0,
+                ordered_at: 0,
+                encoded_action: vec![116, 114, 97, 110],
+                encoded_args: vec![
+                    vec![
+                        5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+                        5, 5, 5, 5, 5, 5, 5
+                    ],
+                    vec![
+                        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+                        6, 6, 6, 6, 6, 6, 6
+                    ],
+                    vec![100, 0, 0, 0]
+                ],
+                signature: vec![],
+                enforce_executioner: None,
+            }
+        );
     }
 }
