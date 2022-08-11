7a0ddcecf1e31f7687a6d75e69d26ac53ed1f597
MaciejBaj <maciej@t3rn.io>
2022-08-11 15:55:18 +0100

    feat: Scaffold a global clock for Circuit

---
+++ pallets/clock/src/lib.rs
@@ -0,0 +1,189 @@
+#![feature(associated_type_defaults)]
+//! <!-- markdown-link-check-disable -->
+//! # Account Manager pallet
+//! </pre></p></details>
+
+// Ensure we're `no_std` when compiling for Wasm.
+#![cfg_attr(not(feature = "std"), no_std)]
+#![allow(clippy::type_complexity)]
+#![allow(clippy::too_many_arguments)]
+
+pub use crate::pallet::*;
+use frame_support::{
+    pallet_prelude::Weight,
+    traits::{Currency, Get},
+};
+
+pub use t3rn_primitives::{
+    abi::{GatewayABIConfig, Type},
+    common::RoundInfo,
+    protocol::SideEffectProtocol,
+    ChainId, GatewayGenesisConfig, GatewayType, GatewayVendor,
+};
+use t3rn_primitives::{
+    account_manager::AccountManager, executors::Executors, treasury::Treasury, EscrowTrait,
+};
+
+#[cfg(test)]
+mod mock;
+#[cfg(test)]
+mod tests;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+mod weights;
+
+pub type BalanceOf<T> =
+    <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+// Definition of the pallet logic, to be aggregated at runtime definition through
+// `construct_runtime`.
+#[frame_support::pallet]
+pub mod pallet {
+    // Import various types used to declare pallet in scope.
+    use super::*;
+    use frame_support::{pallet_prelude::*, traits::ReservableCurrency};
+    use frame_system::pallet_prelude::*;
+    use sp_runtime::traits::Zero;
+    use t3rn_primitives::{
+        circuit_clock::{CircuitClock, ClaimableArtifacts},
+        transfers::EscrowedBalanceOf,
+    };
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// The overarching event type.
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+        #[pallet::constant]
+        type RoundDuration: Get<Self::BlockNumber>;
+
+        type Treasury: Treasury<Self>;
+
+        type Executors: Executors<
+            Self,
+            <<Self::Escrowed as EscrowTrait<Self>>::Currency as frame_support::traits::Currency<
+                Self::AccountId,
+            >>::Balance,
+        >;
+
+        /// A type that provides access to AccountManager
+        type AccountManager: AccountManager<
+            Self::AccountId,
+            <<Self::Escrowed as EscrowTrait<Self>>::Currency as frame_support::traits::Currency<
+                Self::AccountId,
+            >>::Balance,
+            Self::Hash,
+            Self::BlockNumber,
+        >;
+    }
+
+    // Simple declaration of the `Pallet` type. It is placeholder we use to implement traits and
+    // method.
+    #[pallet::pallet]
+    #[pallet::generate_store(pub (super) trait Store)]
+    #[pallet::without_storage_info]
+    pub struct Pallet<T>(PhantomData<T>);
+
+    #[pallet::storage]
+    pub type LastClaims<T: Config> =
+        StorageMap<_, Identity, T::AccountId, RoundInfo<T::BlockNumber>>;
+
+    #[pallet::storage]
+    pub type ClaimableArtifactsPerRound<T: Config> = StorageMap<
+        _,
+        Identity,
+        RoundInfo<T::BlockNumber>,
+        Vec<ClaimableArtifacts<T::AccountId, EscrowedBalanceOf<T, <T as Config>::Escrowed>>>,
+    >;
+
+    impl<T: Config> Pallet<T> {
+        fn calculate_claimable_for_round(n: T::BlockNumber) -> DispatchResult {
+            // fixme: move current_round from treasury to circuit-clock
+            let r = T::Treasury::current_round();
+            let mut claimable_artifacts = vec![];
+            claimable_artifacts.extend(T::AccountManager::on_collect_claimable(n, r)?);
+            // claimable_artifacts.push(T::Treasury::on_collect_claimable(n, r)?);
+            // claimable_artifacts.push(T::Contracts::on_collect_claimable(n, r)?);
+            // claimable_artifacts.push(T::Ambassadors::on_collect_claimable(n, r)?);
+            // claimable_artifacts.push(T::LiquidityPools::on_collect_claimable(n, r)?);
+
+            ClaimableArtifactsPerRound::<T>::insert(r, claimable_artifacts.clone());
+            // todo: aggregated claimable_artifacts to TotalClaimablePerRound
+            Ok(().into())
+        }
+    }
+
+    // Pallet implements [`Hooks`] trait to define some logic to execute in some context.
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+        // `on_finalize` is executed at the end of block after all extrinsic are dispatched.
+        fn on_finalize(n: T::BlockNumber) {
+            // Perform necessary data/state clean up here.
+
+            if n % T::RoundDuration::get() == T::BlockNumber::zero() {
+                Self::calculate_claimable_for_round(n);
+                // After the rewards has been recalculate it's safe to shuffle the executors orded and stakes
+                T::Executors::recalculate_executors_stakes();
+            }
+        }
+
+        // `on_initialize` is executed at the beginning of the block before any extrinsic are
+        // dispatched.
+        //
+        // This function must return the weight consumed by `on_initialize` and `on_finalize`.
+        fn on_initialize(_n: T::BlockNumber) -> Weight {
+            // TODO: we may want to retry failed transactions here, ensuring a max weight and max retry list
+            // Anything that needs to be done at the start of the block.
+            // We don't do anything here.
+            0
+        }
+
+        // A runtime code run after every block and have access to extended set of APIs.
+        //
+        // For instance you can generate extrinsics for the upcoming produced block.
+        fn offchain_worker(_n: T::BlockNumber) {
+            // We don't do anything here.
+            // but we could dispatch extrinsic (transaction/unsigned/inherent) using
+            // sp_io::submit_extrinsic.
+            // To see example on offchain worker, please refer to example-offchain-worker pallet
+            // accompanied in this repository.
+        }
+    }
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub (super) fn deposit_event)]
+    pub enum Event<T: Config> {}
+
+    #[pallet::error]
+    pub enum Error<T> {}
+
+    #[pallet::genesis_config]
+    pub struct GenesisConfig<T: Config> {
+        phantom: PhantomData<T>,
+    }
+
+    #[cfg(feature = "std")]
+    impl<T: Config> Default for GenesisConfig<T> {
+        fn default() -> Self {
+            Self {
+                phantom: Default::default(),
+            }
+        }
+    }
+
+    /// The build of genesis for the pallet.
+    /// Populates storage with the known XDNS Records
+    #[pallet::genesis_build]
+    impl<T: Config> GenesisBuild<T> for GenesisConfig<T> {
+        fn build(&self) {}
+    }
+
+    impl<T: Config> CircuitClock<T, EscrowedBalanceOf<T, T::Escrowed>> for Pallet<T> {
+        fn current_round() -> RoundInfo<T::BlockNumber> {
+            // todo: move current round from treasury to circui-clock
+            T::Treasury::current_round()
+        }
+    }
+}
---
+++ pallets/clock/src/mock.rs
@@ -0,0 +1,183 @@
+use crate::Config;
+use frame_support::{
+    parameter_types,
+    traits::{ConstU64, Everything},
+};
+use sp_core::H256;
+use sp_runtime::{
+    testing::Header,
+    traits::{BlakeTwo256, IdentityLookup},
+    Perbill,
+};
+
+pub type AccountId = sp_runtime::AccountId32;
+pub type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+pub type Block = frame_system::mocking::MockBlock<Test>;
+
+pub const ALICE: AccountId = AccountId::new([1u8; 32]);
+pub const BOB: AccountId = AccountId::new([2u8; 32]);
+
+// For testing the pallet, we construct a mock runtime.
+frame_support::construct_runtime!(
+    pub enum Test where
+        Block = Block,
+        NodeBlock = Block,
+        UncheckedExtrinsic = UncheckedExtrinsic,
+    {
+        System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+        Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+        AccountManager: pallet_account_manager,
+        Timestamp: pallet_timestamp::{Pallet},
+        Sudo: pallet_sudo::{Pallet, Call, Event<T>},
+        CircuitClock: crate::{Pallet, Storage, Event<T>},
+
+        Treasury: pallet_treasury::{Pallet, Call, Config<T>, Storage, Event<T>},
+    }
+);
+
+parameter_types! {
+    pub const MinimumPeriod: u64 = 1;
+    pub const TransactionByteFee: u64 = 1;
+}
+
+impl pallet_timestamp::Config for Test {
+    type MinimumPeriod = MinimumPeriod;
+    type Moment = u64;
+    type OnTimestampSet = ();
+    type WeightInfo = ();
+}
+
+parameter_types! {
+    pub const BlockHashCount: u64 = 250;
+    pub BlockWeights: frame_system::limits::BlockWeights =
+        frame_system::limits::BlockWeights::simple_max(1024);
+}
+impl frame_system::Config for Test {
+    type AccountData = pallet_balances::AccountData<u64>;
+    type AccountId = AccountId;
+    type BaseCallFilter = Everything;
+    type BlockHashCount = BlockHashCount;
+    type BlockLength = ();
+    type BlockNumber = u64;
+    type BlockWeights = ();
+    type Call = Call;
+    type DbWeight = ();
+    type Event = Event;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type Header = Header;
+    type Index = u64;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type MaxConsumers = frame_support::traits::ConstU32<16>;
+    type OnKilledAccount = ();
+    type OnNewAccount = ();
+    type OnSetCode = ();
+    type Origin = Origin;
+    type PalletInfo = PalletInfo;
+    type SS58Prefix = ();
+    type SystemWeightInfo = ();
+    type Version = ();
+}
+
+impl pallet_sudo::Config for Test {
+    type Call = Call;
+    type Event = Event;
+}
+
+parameter_types! {
+    pub const ExistentialDeposit: u64 = 1;
+    pub const MaxReserves: u32 = 50;
+}
+
+impl pallet_balances::Config for Test {
+    type AccountStore = System;
+    type Balance = u64;
+    type DustRemoval = ();
+    type Event = Event;
+    type ExistentialDeposit = ExistentialDeposit;
+    type MaxLocks = ();
+    type MaxReserves = MaxReserves;
+    type ReserveIdentifier = [u8; 8];
+    type WeightInfo = ();
+}
+
+parameter_types! {
+    pub EscrowAccount: AccountId = AccountId::new([55u8; 32]);
+}
+
+impl pallet_account_manager::Config for Test {
+    type CircuitClock = CircuitClock;
+    type Currency = Balances;
+    type EscrowAccount = EscrowAccount;
+    type Escrowed = Self;
+    type Event = Event;
+    type Executors = t3rn_primitives::executors::ExecutorsMock<Self>;
+    type Time = Timestamp;
+    type WeightInfo = ();
+}
+
+parameter_types! {
+    pub const TreasuryAccount:  AccountId =  AccountId::new([10u8; 32]);
+    pub const ReserveAccount:  AccountId =  AccountId::new([11u8; 32]);
+    pub const AuctionFund:  AccountId =  AccountId::new([22u8; 32]);
+    pub const ContractFund:  AccountId =  AccountId::new([33u8; 32]);
+    pub const MinRoundTerm: u32 = 20; // TODO
+    pub const DefaultRoundTerm: u32 = 6 * t3rn_primitives::common::BLOCKS_PER_HOUR; // TODO
+    pub const GenesisIssuance: u32 = 20_000_000; // TODO
+    pub const IdealPerpetualInflation: Perbill = Perbill::from_percent(1);
+    pub const InflationRegressionMonths: u32 = 72;
+}
+
+impl pallet_treasury::Config for Test {
+    type AuctionFund = AuctionFund;
+    type ContractFund = ContractFund;
+    type Currency = Balances;
+    type DefaultRoundTerm = DefaultRoundTerm;
+    type Event = Event;
+    type GenesisIssuance = GenesisIssuance;
+    type IdealPerpetualInflation = IdealPerpetualInflation;
+    type InflationRegressionMonths = InflationRegressionMonths;
+    type MinRoundTerm = MinRoundTerm;
+    type ReserveAccount = ReserveAccount;
+    type TreasuryAccount = TreasuryAccount;
+    type WeightInfo = ();
+}
+
+impl Config for Test {
+    type AccountManager = AccountManager;
+    type Event = Event;
+    type Executors = t3rn_primitives::executors::ExecutorsMock<Self>;
+    type RoundDuration = ConstU64<500>;
+    type Time = Timestamp;
+    type Treasury = Treasury;
+    type WeightInfo = ();
+}
+
+impl t3rn_primitives::EscrowTrait<Test> for Test {
+    type Currency = Balances;
+    type Time = Timestamp;
+}
+
+pub(crate) struct ExtBuilder {}
+
+impl Default for ExtBuilder {
+    fn default() -> ExtBuilder {
+        ExtBuilder {}
+    }
+}
+
+impl ExtBuilder {
+    pub(crate) fn build(self) -> sp_io::TestExternalities {
+        let mut t = frame_system::GenesisConfig::default()
+            .build_storage::<Test>()
+            .expect("Frame system builds valid default genesis config");
+
+        pallet_balances::GenesisConfig::<Test> { balances: vec![] }
+            .assimilate_storage(&mut t)
+            .expect("Pallet balances storage can be assimilated");
+
+        let mut ext = sp_io::TestExternalities::new(t);
+        ext.execute_with(|| System::set_block_number(1));
+        ext
+    }
+}
