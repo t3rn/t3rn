3d26d8141fef52abc8549eade652378b2e2fdda0
MaciejBaj <maciej@t3rn.io>
2022-09-09 17:32:49 +0100

    fix: Update default GatewayABI value type to u128

--- types/src/abi.rs
+++ types/src/abi.rs
@@ -95,7 +95,7 @@ impl Default for GatewayABIConfig {
             hasher: HasherAlgo::Blake2,
             crypto: CryptoAlgo::Sr25519,
             address_length: 32, // 32 bytes : 32 * 8 = 256 bits
-            value_type_size: 8, // u64 = 8 bytes = 64 bits.
+            value_type_size: 16, // u128 = 16 bytes = 128 bits.
             decimals: 8,
             structs: vec![],
         }
