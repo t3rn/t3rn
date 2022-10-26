0a530a332c69977d2931ec1f3dcb94db050a1e2c
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2022-10-26 13:10:05 +0200

    refactor: move zombienet bins into repo and ignore

--- zombienet.sh
+++ zombienet.sh
@@ -14,21 +14,21 @@ runtime=${RUNTIME:-t3rn-parachain}
 pdot_branch=${PDOT_BRANCH:-release-v0.9.27}
 root_dir=$(git rev-parse --show-toplevel)
 
-mkdir -p /tmp/zombienet/bin
+mkdir -p $root_dir/bin
 
 if [[ ! -x /usr/local/bin/zombienet ]]; then
   curl -fL# -o /usr/local/bin/zombienet https://github.com/paritytech/zombienet/releases/download/$version/zombienet-$machine
   chmod +x /usr/local/bin/zombienet
 fi
 
-if [[ ! -x /tmp/zombienet/bin/polkadot ]]; then
+if [[ ! -x $root_dir/bin/polkadot ]]; then
   tmp_dir=$(mktemp -d)
   git clone --branch $pdot_branch --depth 1 https://github.com/paritytech/polkadot $tmp_dir
   cargo build --manifest-path $tmp_dir/Cargo.toml --features fast-runtime --release --locked
-  mv -f $tmp_dir/target/release/polkadot /tmp/zombienet/bin/polkadot
+  mv -f $tmp_dir/target/release/polkadot $root_dir/bin/polkadot
 fi
 
 cargo build --manifest-path $root_dir/node/$runtime/Cargo.toml --release --locked
-cp -f $root_dir/target/release/circuit-collator /tmp/zombienet/bin/circuit-collator
+cp -f $root_dir/target/release/circuit-collator $root_dir/bin/circuit-collator
 
-PATH=/tmp/zombienet/bin:$PATH /usr/local/bin/zombienet --provider=$provider spawn $root_dir/zombienet.toml
+PATH=$root_dir/bin:$PATH /usr/local/bin/zombienet --provider=$provider spawn $root_dir/zombienet.toml
