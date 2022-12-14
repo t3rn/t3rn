195ee274b9a4525565cf1304975bbffbb9f718b3
Pablo <pablo@t3rn.io>
2022-12-14 13:05:17 +0100

    feat: add documentation to @t3rn/sdk
    Add documentation to @t3rn/sdk using typedoc
    
    refactor: update tsconfig.json
    
    Use recursive pattern matching for src path
    
    chore: update git ignore
    
    Ignore all docs folders in packages directory

--- client/packages/sdk/src/gateways/index.ts
+++ client/packages/sdk/src/gateways/index.ts
@@ -1,24 +1,31 @@
 import { ApiPromise } from "@polkadot/api";
 // @ts-ignore
-import {T3rnPrimitivesXdnsXdnsRecord} from "@polkadot/types/lookup"
-import {Gateway} from "./gateway";
+import { T3rnPrimitivesXdnsXdnsRecord } from "@polkadot/types/lookup";
+import { Gateway } from "./gateway";
 
 export enum GatewayType {
-	Substrate,
-	Evm
+  Substrate,
+  Evm,
 }
+
+/**
+ * Get gateways from the chain xdns records
+ * @param api - The api to use
+ * @returns The gateway records
+ */
 
 export const initGateways = async (api: ApiPromise) => {
-	// @ts-ignore
-	const records = (await api.rpc.xdns.fetchRecords())['xdns_records'];
+  // @ts-ignore
+  const records = (await api.rpc.xdns.fetchRecords())["xdns_records"];
 
-	let res = {}
+  let res: Record<string, Gateway> = {};
 
-	for(let i = 0; i < records.length; i++) {
-		const gateway = new Gateway(records[i])
-		res[gateway.id] = gateway
-	}
-	return res
-}
+  for (let i = 0; i < records.length; i++) {
+    const gateway = new Gateway(records[i]);
+    res[gateway.id] = gateway;
+  }
 
-export{Gateway}
+  return res;
+};
+
+export { Gateway };
