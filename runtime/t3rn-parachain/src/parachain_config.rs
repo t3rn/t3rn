077fad7b71c49ef417a8a7c076d9892cd793a35d
AwesomeIbex <donovan@t3rn.io>
2023-01-23 11:53:40 +0000

    chore: clean up para config

--- runtime/t3rn-parachain/src/parachain_config.rs
+++ runtime/t3rn-parachain/src/parachain_config.rs
@@ -1,18 +1,19 @@
 use crate::*;
+
+#[cfg(any(feature = "std", test))]
+pub use sp_runtime::BuildStorage;
+
 use frame_support::{traits::NeverEnsureOrigin, PalletId};
 use frame_system::EnsureRoot;
 use smallvec::smallvec;
 use sp_runtime::{impl_opaque_keys, Permill};
 use sp_std::prelude::*;
-
-#[cfg(any(feature = "std", test))]
-pub use sp_runtime::BuildStorage;
-
-use xcm_config::{XcmConfig, XcmOriginToTransactDispatchOrigin};
-
-// XCM Imports
 use xcm::latest::prelude::BodyId;
+use xcm_config::{XcmConfig, XcmOriginToTransactDispatchOrigin};
 use xcm_executor::XcmExecutor;
+
+// TODO: remove when we import t3rn_primitives
+pub(crate) const TRN: u64 = 1_000_000_000_000;
 
 /// Handles converting a weight scalar to a fee value, based on the scale and granularity of the
 /// node's balance type.
@@ -48,9 +49,11 @@ impl WeightToFeePolynomial for WeightToFee {
 /// to even the core data structures.
 pub mod opaque {
     use super::*;
-    use sp_runtime::{generic, traits::BlakeTwo256};
 
     pub use sp_runtime::OpaqueExtrinsic as UncheckedExtrinsic;
+
+    use sp_runtime::{generic, traits::BlakeTwo256};
+
     /// Opaque block header type.
     pub type Header = generic::Header<BlockNumber, BlakeTwo256>;
     /// Opaque block type.
@@ -98,6 +101,7 @@ parameter_types! {
         })
         .avg_block_initialization(AVERAGE_ON_INITIALIZE_RATIO)
         .build_or_panic();
+    // Allows for t3 prefix in addresses
     pub const SS58Prefix: u16 = 9935;
 }
 
@@ -106,7 +110,7 @@ parameter_types! {
 }
 
 impl pallet_authorship::Config for Runtime {
-    type EventHandler = (CollatorSelection,);
+    type EventHandler = CollatorSelection;
     type FilterUncle = ();
     type FindAuthor = pallet_session::FindAccountFromAuthorIndex<Self, Aura>;
     type UncleGenerations = UncleGenerations;
@@ -245,7 +249,7 @@ parameter_types! {
     pub const MaxApprovals: u32 = 100;
     pub const ProposalBond: Permill = Permill::from_percent(3);
     pub const SpendPeriod: u32 = (60 * 60 * 24) / 12;
-    pub const ProposalBondMinimum: u128 = 10_u128 * 1_000_000_000_000_u128;
+    pub const ProposalBondMinimum: u128 = 10_u128 * (TRN as u128);
 }
 
 impl pallet_treasury::Config for Runtime {
