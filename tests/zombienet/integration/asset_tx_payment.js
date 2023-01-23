beeda76b0b531982a5d1bc05750c1decc0c3420d
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2023-01-23 12:57:33 +0100

    wip: zombie txs?

--- tests/zombienet/integration/asset_tx_payment.js
+++ tests/zombienet/integration/asset_tx_payment.js
@@ -1,31 +1,29 @@
-let { Keyring } = require("@polkadot/api")
-// let { WsProvider, ApiPromise } = require("@polkadot/api")
-let { cryptoWaitReady } = require("@polkadot/util-crypto")
 let assert = require("assert")
 
 async function run(nodeName, networkInfo, args) {
-  await cryptoWaitReady()
-
-  let keyring = new Keyring({ type: "sr25519" })
-  let alice = keyring.addFromUri("//Alice")
-  let bob = keyring.addFromUri("//Bob")
+  await zombie.util.cryptoWaitReady();
 
   let { wsUri, userDefinedTypes } = networkInfo.nodesByName[nodeName]
   let api = await zombie.connect(wsUri, userDefinedTypes)
-  // let api = await ApiPromise.create({ provider: new WsProvider("ws://localhost:9930") })
+
+  let keyring = new zombie.Keyring({ type: "sr25519" });
+  let alice = keyring.addFromUri("//Alice")
+  let bob = keyring.addFromUri("//Bob")
 
   // force_create and set_metadata an asset
   let name = "Asset"
   let symbol = "ASS"
   let decimals = 12
-  let id = 1
+  let id = 1n
   let owner = alice.address
   let isSufficient = true
-  let minBalance = 1
+  let minBalance = 1n
+  console.log("ALICE & BOB", alice.address, bob.address)
   await api.tx.assets.forceCreate(id, owner, isSufficient, minBalance).signAndSend(alice)
-  // await api.tx.assets.setMetadata(id, name, symbol, decimals).signAndSend(alice)
+  await api.tx.assets.setMetadata(id, name, symbol, decimals).signAndSend(alice)
 
   // mint some asset balance to bob
+  console.log("MADE IT !!!")
 
   // have bob do a tx paying with the non-native asset
 }
