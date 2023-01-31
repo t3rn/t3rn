ec4e085fb8f463ca4d42b7c5be9d3fa2911549c9
MaciejBaj <maciej@t3rn.io>
2023-01-31 10:11:26 +0000

    fix: loosen collators conf MaxCandidate and SessionTime

--- runtime/t3rn-parachain/src/parachain_config.rs
+++ runtime/t3rn-parachain/src/parachain_config.rs
@@ -163,8 +163,8 @@ impl pallet_aura::Config for Runtime {
 parameter_types! {
     pub const PotId: PalletId = PalletId(*b"PotStake");
     pub const MaxCandidates: u32 = 1000;
-    pub const MinCandidates: u32 = 5;
-    pub const SessionLength: BlockNumber = 6 * HOURS;
+    pub const MinCandidates: u32 = 2;
+    pub const SessionLength: BlockNumber = 24 * HOURS;
     pub const MaxInvulnerables: u32 = 100;
 }
 
