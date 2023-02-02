6bc8f494a963afaa17d09f722e50d79f7269889a
MaciejBaj <maciej@t3rn.io>
2023-02-02 17:16:24 +0000

    refactor: implement global on init hooks for t0rn and mock runtimes

--- runtime/mock/src/circuit_config.rs
+++ runtime/mock/src/circuit_config.rs
@@ -1,18 +1,100 @@
 use crate::*;
 
-use frame_support::{parameter_types, traits::ConstU32, PalletId};
+use frame_support::{parameter_types, traits::ConstU32, weights::Weight, PalletId};
 use pallet_grandpa_finality_verifier::bridges::runtime as bp_runtime;
+
 use sp_core::H256;
+use sp_runtime::Perbill;
+
+use sp_runtime::traits::One;
 
 impl t3rn_primitives::EscrowTrait<Runtime> for Runtime {
     type Currency = Balances;
     type Time = Timestamp;
 }
 
+pub struct GlobalOnInitQueues;
+
+impl pallet_clock::traits::OnHookQueues<Runtime> for GlobalOnInitQueues {
+    fn process(n: BlockNumber, on_init_weight_limit: Weight) -> Weight {
+        let mut weights_consumed = vec![];
+        // Iterate over all pre-init hooks implemented by pallets and return aggregated weight
+        weights_consumed.push(Circuit::process_signal_queue(
+            n,
+            BlockNumber::one(),
+            Perbill::from_percent(15) * on_init_weight_limit,
+        ));
+        log::debug!(
+            "Circuit::process_signal_queue consumed: {:?}",
+            weights_consumed
+                .last()
+                .expect("Circuit::process_signal_queue consumed weight")
+        );
+        weights_consumed.push(Circuit::process_xtx_tick_queue(
+            n,
+            BlockNumber::one(),
+            Perbill::from_percent(35) * on_init_weight_limit,
+        ));
+        log::debug!(
+            "Circuit::process_xtx_tick_queue consumed: {:?}",
+            weights_consumed
+                .last()
+                .expect("Circuit::process_xtx_tick_queue consumed weight")
+        );
+        weights_consumed.push(Circuit::process_revert_xtx_queue(
+            n,
+            10u32,
+            Perbill::from_percent(35) * on_init_weight_limit,
+        ));
+        log::debug!(
+            "Circuit::process_revert_xtx_queue consumed: {:?}",
+            weights_consumed
+                .last()
+                .expect("Circuit::process_revert_xtx_queue consumed weight")
+        );
+        weights_consumed.push(Clock::check_bump_round(
+            n,
+            BlockNumber::one(),
+            Perbill::from_percent(5) * on_init_weight_limit,
+        ));
+        log::debug!(
+            "Clock::check_bump_round consumed: {:?}",
+            weights_consumed
+                .last()
+                .expect("Clock::check_bump_round consumed weight")
+        );
+        weights_consumed.push(Clock::calculate_claimable_for_round(
+            n,
+            BlockNumber::one(),
+            Perbill::from_percent(10) * on_init_weight_limit,
+        ));
+        log::debug!(
+            "Clock::calculate_claimable_for_round consumed: {:?}",
+            weights_consumed
+                .last()
+                .expect("Clock::calculate_claimable_for_round consumed weight")
+        );
+        let total_consumed: Weight = weights_consumed
+            .iter()
+            .fold(0, |acc: Weight, weight: &Weight| {
+                acc.saturating_add(*weight)
+            });
+
+        log::debug!(
+            "Total weight consumed by on init hook: {:?}",
+            total_consumed
+        );
+
+        total_consumed
+    }
+}
+
 impl pallet_clock::Config for Runtime {
     type AccountManager = AccountManager;
     type Event = Event;
     type Executors = t3rn_primitives::executors::ExecutorsMock<Self>;
+    type OnFinalizeQueues = pallet_clock::traits::EmptyOnHookQueues<Self>;
+    type OnInitializeQueues = GlobalOnInitQueues;
     type RoundDuration = ConstU32<500u32>;
 }
 
