660b2aad01e5d5132a0842211639a289bd1f67ee
marcin <6998565+3h4x@users.noreply.github.com>
2023-02-13 14:57:28 +0100

    ci: fix zombienet test scripts

--- tests/zombienet/download_previous_version.sh
+++ tests/zombienet/download_previous_version.sh
@@ -1,28 +1,20 @@
-#!/bin/bash
+#!/bin/bash -x
 new_version=$(($2+1))
 
 case "$1" in
   t0rn*)
       echo =========== T0RN OLD COLLATOR ===========; \
-      export tag_version=$(git tag --list --sort=-version:refname "v[0-9]*.[0-9]*.[0-9]*-rc.[0-9]*" | head -n 1)
+      export tag_version=$(git tag --list --sort=-version:refname "v[0-9]*.[0-9]*.[0-9]*-rc.[0-9]*" | head -n 2 | tail -n 1)
       echo Version: "$tag_version"
 
-      export url=$(curl -s https://api.github.com/repos/t3rn/t3rn/releases/tags/"$tag_version" | jq -r '.assets[0].browser_download_url')
-      echo Url: $url
-      
-      [ "$url" ] || exit 1
-      curl -L -o - "$url" | gunzip -c > "$bin_dir"/collator-old && chmod +x "$bin_dir"/collator-old
+      ./download_collator.sh "$tag_version" 
     ;;
   t3rn*)
     echo =========== T3RN OLD COLLATOR ===========; \
-      export tag_version=$(version=git tag --list --sort=-version:refname "v[0-9]*.[0-9].[0-9]" | head -n 1)
+      export tag_version=$(version=git tag --list --sort=-version:refname "v[0-9]*.[0-9].[0-9]" | head -n 2 | tail -n 1)
       echo Version: "$tag_version"
 
-      export url=$(curl -s https://api.github.com/repos/t3rn/t3rn/releases/tags/"$tag_version" | jq -r '.assets[0].browser_download_url')
-      echo Url: $url
-      
-      [ "$url" ] || exit 1
-      curl -L -o - "$url" | gunzip -c > "$bin_dir"/collator-old && chmod +x "$bin_dir"/collator-old
+      ./download_collator.sh "$tag_version" 
     ;;
   *)        exit 1;;
 esac
