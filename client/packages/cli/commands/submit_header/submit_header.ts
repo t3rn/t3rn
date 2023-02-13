bfe0322f4dfa80d597f07ba3f45600aaff13d314
Paul Etscheit <etscheitp@gmail.com>
2023-02-13 15:44:06 +0100

    feat: add logging for ranger monitoring
    Signed-off-by: petscheit <etscheitp@gmail.com>

--- client/packages/cli/commands/submit_header/submit_header.ts
+++ client/packages/cli/commands/submit_header/submit_header.ts
@@ -1,22 +1,25 @@
 import { submitRelaychainHeaders, submitParachainHeaders } from "./substrate";
 import{ ApiPromise, WsProvider } from'@polkadot/api';
 
-export const submitHeader = async (circuitApi: any, gatewayData: any, gatewayId: string) => {
+export const submitHeader = async (circuitApi: any, gatewayData: any, gatewayId: string, logger: any) => {
     switch(gatewayData.registrationData.gatewayVendor) {
         case "Rococo": {
             const targetApi = await ApiPromise.create({
                 provider: new WsProvider(gatewayData.rpc),
             });
             if(gatewayData.registrationData.parachain == null) { // null or undefined
-                return submitRelaychainHeaders(circuitApi, targetApi, gatewayId)
+                return submitRelaychainHeaders(circuitApi, targetApi, gatewayId, logger)
             }
             else {
-                return submitParachainHeaders(circuitApi, targetApi, gatewayData)
+                return submitParachainHeaders(circuitApi, targetApi, gatewayData, logger)
             }
             break;
         }
         default: {
-            console.log("Vendor not configured!");
+            logger.debug({
+                success: false,
+                msg: "Vendor not configured"
+            });
             process.exit(1);
         }
     }
