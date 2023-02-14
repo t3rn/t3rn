607737282d8a5a2c0a08d903d8d7783c99e1d2f0
Pablo Lozano <pablo@t3rn.io>
2023-02-14 10:21:27 +0100

    chore: add cleaning events script
    Thanks to @chiefbiiko.

--- tests/integration/index.ts
+++ tests/integration/index.ts
@@ -1,5 +1,4 @@
 import { ApiPromise, WsProvider } from "@polkadot/api"
-// import { BlockHash } from "@polkadot/types/interfaces"
 import { Keyring } from '@polkadot/keyring'
 import { SubmittableExtrinsic } from "@polkadot/api/types"
 import { KeyringPair } from "@polkadot/keyring/types"
@@ -93,11 +92,6 @@ function formatEvent({
   }
 }
 
-
-// Noah's
-
-const BOB = '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty';
-
 async function main() {
   // New provider instance for local zombienet
   const wsProvider = new WsProvider('ws://127.0.0.1:9900');
@@ -110,27 +104,21 @@ async function main() {
 
   // Add Alice to our keyring with a hard-derivation path (empty phrase, so uses dev)
   const alice = keyring.addFromUri('//Alice');
+  const BOB = '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty';
 
   // Create a extrinsic, transferring 12345 units to Bob
   const transfer = api.tx.balances.transfer(BOB, 12345);
 
   // Sign and send the transaction using our account
-  const hash = await transfer.signAndSend(alice);
-  console.log('Transfer sent with hash', hash.toHex());
+  // const hash = await transfer.signAndSend(alice);
+  // console.log('Transfer sent with hash', hash.toHex());
 
-  // From @chiefbiiko <3
+  // thanks @chiefbiiko
   const event = await signAndSendSafe(api, alice, transfer);
-  console.log('event ->',
-    // event['events']
-    // JSON.stringify(event)
-    // JSON.stringify(event['events'])
-    JSON.parse(
-      JSON.stringify(
-        event['events'],
-        (key, value) => typeof value === 'bigint' ? value.toString() : value
-      )
-    )
-  );
+  event['events'].forEach((e) => {
+    // @ts-ignore
+    console.log('event ->', e);
+  })
 
 }
 
