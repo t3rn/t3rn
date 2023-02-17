0361f24905a1ed880cb2913ae002c71db61d8c98
marcin <6998565+3h4x@users.noreply.github.com>
2023-02-17 12:57:32 +0100

    ci: include chainspec regeneration check

--- scripts/regenerate_parachain_build_artifacts.sh
+++ scripts/regenerate_parachain_build_artifacts.sh
@@ -9,14 +9,6 @@ echo "building artifacts for: $PARACHAIN_NAME at $RELAYCHAIN_NAME"
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
-echo "removing current binaries for $PARACHAIN_NAME"
-
-rm "$ROOT_DIR"/target/release/"${PARACHAIN_NAME}"-collator
-
-cd "${ROOT_DIR}"/node/"${PARACHAIN_NAME}"-parachain && cargo build --release --locked || exit
-
-cd "${ROOT_DIR}" || exit
-
 mkdir -p "${ROOT_DIR}"/target/release/specs
 
 exec
@@ -28,9 +20,13 @@ cp "${ROOT_DIR}"/target/release/specs/"${PARACHAIN_NAME}".json "${ROOT_DIR}"/spe
 cp "${ROOT_DIR}"/target/release/specs/"${PARACHAIN_NAME}".raw.json "${ROOT_DIR}"/specs/"${PARACHAIN_NAME}".raw.json
 cp "${ROOT_DIR}"/target/release/specs/"${PARACHAIN_NAME}".wasm "${ROOT_DIR}"/specs/"${PARACHAIN_NAME}".wasm
 
-rm -rf "${ROOT_DIR}"/target/release/specs
-
 echo "successfully generated artifacts (.json, raw.json, .wasm) for: ${PARACHAIN_NAME} to ${ROOT_DIR}/target/release/specs"
 echo "$PARACHAIN_NAME.wasm: $(head -c 64 "${ROOT_DIR}"/specs/"${PARACHAIN_NAME}".wasm)"
 echo "$PARACHAIN_NAME.json: $(head -c 21 "${ROOT_DIR}"/specs/"${PARACHAIN_NAME}".json)"
 echo "$PARACHAIN_NAME.raw.json: $(head -c 21 "${ROOT_DIR}"/specs/"${PARACHAIN_NAME}".raw.json)"
+
+if [[ $(git diff --stat) != '' ]]; then
+  echo 'Chainspecs are not up to date. Please run `./scripts/regenerate_parachain_build_artifacts.sh` and commit the changes.'
+  exit 1
+fi
+
