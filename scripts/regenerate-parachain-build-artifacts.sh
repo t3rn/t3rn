f0f93cd90ff94e96712eec4728443850a5b917cf
marcin <6998565+3h4x@users.noreply.github.com>
2023-02-20 11:27:48 +0100

    ci: remove genesis.wasm as its already part of release artifact

--- scripts/regenerate-parachain-build-artifacts.sh
+++ scripts/regenerate-parachain-build-artifacts.sh
@@ -22,15 +22,12 @@ mkdir -p "${ROOT_DIR}"/target/release/specs
 exec
   "$ROOT_DIR"/target/release/"${PARACHAIN_NAME}"-collator build-spec --chain "$RELAYCHAIN_NAME" > "${ROOT_DIR}"/target/release/specs/"${PARACHAIN_NAME}".json
   "$ROOT_DIR"/target/release/"${PARACHAIN_NAME}"-collator build-spec --chain "$RELAYCHAIN_NAME" --raw > "${ROOT_DIR}"/target/release/specs/"${PARACHAIN_NAME}".raw.json
-  "$ROOT_DIR"/target/release/"${PARACHAIN_NAME}"-collator export-genesis-wasm --chain "${ROOT_DIR}"/target/release/specs/"${PARACHAIN_NAME}".raw.json > "${ROOT_DIR}"/target/release/specs/"${PARACHAIN_NAME}".wasm
 
 cp "${ROOT_DIR}"/target/release/specs/"${PARACHAIN_NAME}".json "${ROOT_DIR}"/specs/"${PARACHAIN_NAME}".json
 cp "${ROOT_DIR}"/target/release/specs/"${PARACHAIN_NAME}".raw.json "${ROOT_DIR}"/specs/"${PARACHAIN_NAME}".raw.json
-cp "${ROOT_DIR}"/target/release/specs/"${PARACHAIN_NAME}".wasm "${ROOT_DIR}"/specs/"${PARACHAIN_NAME}".wasm
 
 rm -rf "${ROOT_DIR}"/target/release/specs
 
-echo "successfully generated artifacts (.json, raw.json, .wasm) for: ${PARACHAIN_NAME} to ${ROOT_DIR}/target/release/specs"
-echo "$PARACHAIN_NAME.wasm: $(head -c 64 "${ROOT_DIR}"/specs/"${PARACHAIN_NAME}".wasm)"
+echo "successfully generated artifacts (.json, raw.json) for: ${PARACHAIN_NAME} to ${ROOT_DIR}/target/release/specs"
 echo "$PARACHAIN_NAME.json: $(head -c 21 "${ROOT_DIR}"/specs/"${PARACHAIN_NAME}".json)"
 echo "$PARACHAIN_NAME.raw.json: $(head -c 21 "${ROOT_DIR}"/specs/"${PARACHAIN_NAME}".raw.json)"
