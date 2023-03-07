16a02b985ea70472c47b1821b499ed24803d8b88
petscheit <etscheitp@gmail.com>
2023-03-07 23:07:48 +0100

    chore: remove build files
    Signed-off-by: petscheit <etscheitp@gmail.com>

--- client/packages/types/lib/cjs/augment/registry.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Auto-generated via `yarn polkadot-types-from-defs`, do not edit
-/* eslint-disable */
-Object.defineProperty(exports, "__esModule", { value: true });
--- client/packages/types/lib/mjs/augment/augment-api.d.ts
+++
@@ -1,6 +0,0 @@
-import './augment-api-consts';
-import './augment-api-errors';
-import './augment-api-events';
-import './augment-api-query';
-import './augment-api-tx';
-import './augment-api-rpc';
