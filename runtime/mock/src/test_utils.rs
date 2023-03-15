c4a94dac63444b5ea37b1d0effce60f0e8ce3041
MaciejBaj <maciej@t3rn.io>
2023-03-15 12:41:46 +0000

    test: rewrite prodice test sfx args in mock with latest ABI

--- runtime/mock/src/test_utils.rs
+++ runtime/mock/src/test_utils.rs
@@ -1,9 +1,9 @@
+pub use circuit_runtime_types::{AccountId, Balance as BalanceOf, BlockNumber};
 use codec::Encode;
 use hex_literal::hex;
-use sp_std::{vec, vec::Vec};
-use t3rn_types::{abi::Type, interface::SideEffectInterface, sfx::SideEffect, types::Bytes};
-
-pub use circuit_runtime_types::{AccountId, Balance as BalanceOf, BlockNumber};
+use sp_core::U256;
+use sp_std::{convert::TryInto, vec, vec::Vec};
+use t3rn_types::{sfx::SideEffect, types::Bytes};
 
 pub type Arguments = Vec<Bytes>;
 pub type Hashing = sp_runtime::traits::BlakeTwo256;
@@ -41,81 +41,135 @@ pub enum ArgVariant {
     C,
 }
 
-pub fn produce_test_args(types: Vec<(Type, ArgVariant)>) -> Arguments {
-    let mut args: Arguments = vec![];
-    for (t, variant) in types {
-        match t {
-            Type::Bytes(n) => {
-                let mut bytes: Vec<u8> = vec![];
-                for m in 0..n {
-                    bytes.push(m);
-                }
-                args.push(bytes);
-            },
-            Type::Address(n) => {
-                args.push(match n {
-                    32 => match variant {
-                        ArgVariant::A =>
-                            hex!("0909090909090909090909090909090909090909090909090909090909090909")
-                                .into(),
-                        ArgVariant::B =>
-                            hex!("0606060606060606060606060606060606060606060606060606060606060606")
-                                .into(),
-                        ArgVariant::C =>
-                            hex!("0303030303030303030303030303030303030303030303030303030303030303")
-                                .into(),
-                    },
-                    _ => unimplemented!(),
-                });
-            },
-            Type::OptionalInsurance => {
-                args.push(match variant {
-                    ArgVariant::A =>
-                        hex!("0100000000000000000000000000000001000000000000000000000000000000")
-                            .into(),
-                    ArgVariant::B =>
-                        hex!("0200000000000000000000000000000002000000000000000000000000000000")
-                            .into(),
-                    ArgVariant::C =>
-                        hex!("0300000000000000000000000000000003000000000000000000000000000000")
-                            .into(),
-                });
-            },
-            Type::Uint(n) => {
-                args.push(match n {
-                    32 => match variant {
-                        ArgVariant::A => 1u32.encode(),
-                        ArgVariant::B => 2u32.encode(),
-                        ArgVariant::C => 3u32.encode(),
-                    },
-                    64 => match variant {
-                        ArgVariant::A => 1u64.encode(),
-                        ArgVariant::B => 2u64.encode(),
-                        ArgVariant::C => 3u64.encode(),
-                    },
-                    128 => match variant {
-                        ArgVariant::A => 1u128.encode(),
-                        ArgVariant::B => 2u128.encode(),
-                        ArgVariant::C => 3u128.encode(),
-                    },
-                    _ => unimplemented!(),
-                });
-            },
-            _ => unimplemented!(),
-        }
+pub fn recursive_produce_test_args_for_abi(abi: Abi, args_variant: ArgVariant) -> Arguments {
+    match abi {
+        Abi::Struct(_, abi_vec) | Abi::Log(_, abi_vec) | Abi::Enum(_, abi_vec) => {
+            let mut args = vec![];
+            for field in abi_vec {
+                args.append(&mut recursive_produce_test_args_for_abi(
+                    *field,
+                    args_variant.clone(),
+                ));
+            }
+            args
+        },
+        Abi::Tuple(_, (abi1, abi2)) => match args_variant {
+            ArgVariant::A | ArgVariant::B | ArgVariant::C =>
+                recursive_produce_test_args_for_abi(*abi1, args_variant.clone())
+                    .into_iter()
+                    .chain(recursive_produce_test_args_for_abi(*abi2, args_variant).into_iter())
+                    .collect(),
+        },
+        Abi::Vec(_, _abi_vec) => {
+            unimplemented!("Vec is not supported yet")
+        },
+        Abi::Option(_, abi) => match args_variant {
+            ArgVariant::A => vec![0u8.encode()],
+            ArgVariant::B => recursive_produce_test_args_for_abi(*abi, args_variant),
+            ArgVariant::C => recursive_produce_test_args_for_abi(*abi, args_variant),
+        },
+        Abi::Account20(_) => match args_variant {
+            ArgVariant::A => vec![hex!("0909090909090909090909090909090909090909").into()],
+            ArgVariant::B => vec![hex!("0606060606060606060606060606060606060606").into()],
+            ArgVariant::C => vec![hex!("0303030303030303030303030303030303030303").into()],
+        },
+        Abi::Account32(_) => match args_variant {
+            ArgVariant::A =>
+                vec![
+                    hex!("0909090909090909090909090909090909090909090909090909090909090909").into(),
+                ],
+            ArgVariant::B =>
+                vec![
+                    hex!("0606060606060606060606060606060606060606060606060606060606060606").into(),
+                ],
+            ArgVariant::C =>
+                vec![
+                    hex!("0303030303030303030303030303030303030303030303030303030303030303").into(),
+                ],
+        },
+        Abi::H256(_) => match args_variant {
+            ArgVariant::A =>
+                vec![
+                    hex!("0909090909090909090909090909090909090909090909090909090909090909").into(),
+                ],
+            ArgVariant::B =>
+                vec![
+                    hex!("0606060606060606060606060606060606060606060606060606060606060606").into(),
+                ],
+            ArgVariant::C =>
+                vec![
+                    hex!("0303030303030303030303030303030303030303030303030303030303030303").into(),
+                ],
+        },
+        Abi::Bytes(_) => match args_variant {
+            ArgVariant::A => vec![hex!("0909090909090909090909090909090909").into()],
+            ArgVariant::B => vec![hex!("0606060606060606").into()],
+            ArgVariant::C =>
+                vec![
+                    hex!("0303030303030303030303030303030303030303030303030303030303030303").into(),
+                ],
+        },
+        Abi::Value256(_) => match args_variant {
+            ArgVariant::A => vec![U256::from(30000).encode()],
+            ArgVariant::B => vec![U256::from(40000000).encode()],
+            ArgVariant::C => vec![U256::from(0).encode()],
+        },
+        Abi::Value128(_) => match args_variant {
+            ArgVariant::A => vec![30000u128.encode()],
+            ArgVariant::B => vec![40000000u128.encode()],
+            ArgVariant::C => vec![0u128.encode()],
+        },
+        Abi::Value64(_) => match args_variant {
+            ArgVariant::A => vec![30000u64.encode()],
+            ArgVariant::B => vec![40000000u64.encode()],
+            ArgVariant::C => vec![0u64.encode()],
+        },
+        Abi::Value32(_) => match args_variant {
+            ArgVariant::A => vec![30000u32.encode()],
+            ArgVariant::B => vec![40000000u32.encode()],
+            ArgVariant::C => vec![0u32.encode()],
+        },
+        Abi::Byte(_) => match args_variant {
+            ArgVariant::A => vec![1u8.encode()],
+            ArgVariant::B => vec![255u8.encode()],
+            ArgVariant::C => vec![0u8.encode()],
+        },
+        Abi::Bool(_) => match args_variant {
+            ArgVariant::A => vec![true.encode()],
+            ArgVariant::B => vec![false.encode()],
+            ArgVariant::C => vec![0u8.encode()],
+        },
     }
-    args
 }
 
+use t3rn_abi::{Abi, SFXAbi};
+use t3rn_types::sfx::Sfx4bId;
+
 pub fn produce_and_validate_side_effect(
-    sfx_interface: SideEffectInterface,
-    args_variants: Vec<(Type, ArgVariant)>,
+    sfx_4b_id: Sfx4bId,
+    insurance: BalanceOf,
+    max_reward: BalanceOf,
+    codec: t3rn_abi::recode::Codec,
+    args_variant: ArgVariant,
 ) -> SideEffect<AccountId, BalanceOf> {
-    produce_test_side_effect(
-        sfx_interface.get_id(),
-        produce_test_args(args_variants),
-        vec![],
-    )
+    let sfx_interface =
+        SFXAbi::get_standard_interface(sfx_4b_id).expect("SFX should be registered");
+
+    let abi: Abi = sfx_interface
+        .get_expected_egress_descriptor(codec)
+        .try_into()
+        .expect("ABI should be valid for standard registered SFX");
+
+    SideEffect::<AccountId, BalanceOf> {
+        target: [0, 0, 0, 0],
+        max_reward,
+        encoded_action: sfx_4b_id.to_vec(),
+        encoded_args: recursive_produce_test_args_for_abi(abi, args_variant),
+        signature: vec![],
+        insurance,
+        enforce_executor: None,
+        reward_asset_id: None,
+    }
 }
 
 pub fn generate_xtx_id<Hasher: sp_core::Hasher>(
