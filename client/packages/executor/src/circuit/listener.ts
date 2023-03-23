aef524c2a0a13a602378dd6b623dec9da33e71d6
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2023-03-23 11:43:44 +0100

    feat: custom named executor config file

--- client/packages/executor/src/circuit/listener.ts
+++ client/packages/executor/src/circuit/listener.ts
@@ -38,78 +38,69 @@ export type ListenerEventData = {
 
 /** @group t3rn Circuit */
 export class CircuitListener extends EventEmitter {
-  client: ApiPromise;
+    client: ApiPromise
+    stop: any
 
   constructor(client: ApiPromise) {
     super();
     this.client = client;
   }
 
-  async start() {
-    this.client.query.system.events((notifications) => {
-      for (let i = 0; i < notifications.length; i++) {
-        if (notifications[i].event.method === "NewSideEffectsAvailable") {
-          // receives new side effects
-          this.emit("Event", <ListenerEventData>{
-            type: ListenerEvents.NewSideEffectsAvailable,
-            data: notifications[i].event.data,
-          });
-        } else if (notifications[i].event.method === "SFXNewBidReceived") {
-          this.emit("Event", <ListenerEventData>{
-            type: ListenerEvents.SFXNewBidReceived,
-            data: notifications[i].event.data,
-          });
-        } else if (
-          notifications[i].event.method === "XTransactionReadyForExec"
-        ) {
-          this.emit("Event", <ListenerEventData>{
-            type: ListenerEvents.XTransactionReadyForExec,
-            data: notifications[i].event.data,
-          });
-        } else if (notifications[i].event.method === "HeadersAdded") {
-          console.log(notifications[i].toHuman());
-          let vendor;
-          if (notifications[i].event.section === "rococoBridge") {
-            vendor = "Rococo";
-          }
-          const data = {
-            vendor,
-            height: parseInt(notifications[i].event.data[0].toString()),
-          };
-          this.emit("Event", <ListenerEventData>{
-            type: ListenerEvents.HeaderSubmitted,
-            data,
-          });
-        } else if (notifications[i].event.method === "SideEffectConfirmed") {
-          this.emit("Event", <ListenerEventData>{
-            type: ListenerEvents.SideEffectConfirmed,
-            data: notifications[i].event.data,
-          });
-        } else if (
-          notifications[i].event.method ===
-          "XTransactionXtxFinishedExecAllSteps"
-        ) {
-          this.emit("Event", <ListenerEventData>{
-            type: ListenerEvents.XtxCompleted,
-            data: notifications[i].event.data,
-          });
-        } else if (
-          notifications[i].event.method === "XTransactionXtxDroppedAtBidding"
-        ) {
-          this.emit("Event", <ListenerEventData>{
-            type: ListenerEvents.DroppedAtBidding,
-            data: notifications[i].event.data,
-          });
-        } else if (
-          notifications[i].event.method ===
-          "XTransactionXtxRevertedAfterTimeOut"
-        ) {
-          this.emit("Event", <ListenerEventData>{
-            type: ListenerEvents.RevertTimedOut,
-            data: notifications[i].event.data,
-          });
-        }
-      }
-    });
-  }
+    async start() {
+        this.stop = this.client.query.system.events((notifications) => {
+            for (let i = 0; i < notifications.length; i++) {
+                if (notifications[i].event.method === "NewSideEffectsAvailable") {
+                    // receives new side effects
+                    this.emit("Event", <ListenerEventData>{
+                        type: ListenerEvents.NewSideEffectsAvailable,
+                        data: notifications[i].event.data,
+                    })
+                } else if (notifications[i].event.method === "SFXNewBidReceived") {
+                    this.emit("Event", <ListenerEventData>{
+                        type: ListenerEvents.SFXNewBidReceived,
+                        data: notifications[i].event.data,
+                    })
+                } else if (notifications[i].event.method === "XTransactionReadyForExec") {
+                    this.emit("Event", <ListenerEventData>{
+                        type: ListenerEvents.XTransactionReadyForExec,
+                        data: notifications[i].event.data,
+                    })
+                } else if (notifications[i].event.method === "HeadersAdded") {
+                    console.log(notifications[i].toHuman())
+                    let vendor;
+                    if(notifications[i].event.section === "rococoBridge") {
+                        vendor = "Rococo"
+                    }
+                    const data = {
+                        vendor,
+                        height: parseInt(notifications[i].event.data[0]),
+                    }
+                    this.emit("Event", <ListenerEventData>{
+                        type: ListenerEvents.HeaderSubmitted,
+                        data,
+                    })
+                } else if (notifications[i].event.method === "SideEffectConfirmed") {
+                    this.emit("Event", <ListenerEventData>{
+                        type: ListenerEvents.SideEffectConfirmed,
+                        data: notifications[i].event.data,
+                    })
+                } else if (notifications[i].event.method === "XTransactionXtxFinishedExecAllSteps") {
+                    this.emit("Event", <ListenerEventData>{
+                        type: ListenerEvents.XtxCompleted,
+                        data: notifications[i].event.data,
+                    })
+                } else if (notifications[i].event.method === "XTransactionXtxDroppedAtBidding") {
+                    this.emit("Event", <ListenerEventData>{
+                        type: ListenerEvents.DroppedAtBidding,
+                        data: notifications[i].event.data,
+                    })
+                } else if (notifications[i].event.method === "XTransactionXtxRevertedAfterTimeOut") {
+                    this.emit("Event", <ListenerEventData>{
+                        type: ListenerEvents.RevertTimedOut,
+                        data: notifications[i].event.data,
+                    })
+                }
+            }
+        })
+    }
 }
