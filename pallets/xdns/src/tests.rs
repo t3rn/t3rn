9017ca3e11366aabf8ba96bfa7caf8c6d1046dcc
MaciejBaj <maciej@t3rn.io>
2023-03-24 12:42:17 +0000

    test: re-compile XDNS tests after API changes

--- pallets/xdns/src/tests.rs
+++ pallets/xdns/src/tests.rs
@@ -36,33 +36,30 @@ fn genesis_should_seed_circuit_gateway_polkadot_and_kusama_nodes() {
         .build()
         .execute_with(|| {
             assert_eq!(
-                pallet_xdns::XDNSRegistry::<Runtime>::iter().count(),
+                pallet_xdns::Gateways::<Runtime>::iter().count(),
                 DEFAULT_GATEWAYS_IN_STORAGE_COUNT
             );
-            assert!(pallet_xdns::XDNSRegistry::<Runtime>::get([3, 3, 3, 3]).is_some());
-            assert!(pallet_xdns::XDNSRegistry::<Runtime>::get(b"gate").is_some());
-            assert!(pallet_xdns::XDNSRegistry::<Runtime>::get(b"pdot").is_some());
-            assert!(pallet_xdns::XDNSRegistry::<Runtime>::get(b"ksma").is_some());
+            assert!(pallet_xdns::Gateways::<Runtime>::get([3, 3, 3, 3]).is_some());
+            assert!(pallet_xdns::Gateways::<Runtime>::get(b"gate").is_some());
+            assert!(pallet_xdns::Gateways::<Runtime>::get(b"pdot").is_some());
+            assert!(pallet_xdns::Gateways::<Runtime>::get(b"ksma").is_some());
         });
 }
 
 #[test]
 fn should_add_a_new_xdns_record_if_it_doesnt_exist() {
     ExtBuilder::default().build().execute_with(|| {
-        assert_ok!(XDNS::add_new_xdns_record(
-            b"some_url".to_vec(),
+        assert_ok!(XDNS::add_new_gateway(
             *b"test",
-            None,
             Default::default(),
             GatewayVendor::Rococo,
-            GatewayType::TxOnly(0),
-            Default::default(),
-            Default::default(),
-            vec![],
-            vec![],
+            t3rn_abi::Codec::Scale,
+            None,   // registrant
+            None,   // escrow_account
+            vec![], // allowed_side_effects
         ));
-        assert_eq!(pallet_xdns::XDNSRegistry::<Runtime>::iter().count(), 1);
-        assert!(pallet_xdns::XDNSRegistry::<Runtime>::get(b"test").is_some());
+        assert_eq!(pallet_xdns::Gateways::<Runtime>::iter().count(), 1);
+        assert!(pallet_xdns::Gateways::<Runtime>::get(b"test").is_some());
     });
 }
 
@@ -85,22 +82,19 @@ fn should_not_add_a_new_xdns_record_if_it_already_exists() {
         .build()
         .execute_with(|| {
             assert_noop!(
-                XDNS::add_new_xdns_record(
-                    b"some_url".to_vec(),
+                XDNS::add_new_gateway(
                     [3, 3, 3, 3],
-                    None,
                     Default::default(),
                     GatewayVendor::Rococo,
-                    GatewayType::TxOnly(0),
-                    Default::default(),
-                    Default::default(),
-                    vec![],
-                    vec![],
+                    t3rn_abi::Codec::Scale,
+                    None,   // registrant
+                    None,   // escrow_account
+                    vec![], // allowed_side_effects
                 ),
                 pallet_xdns::pallet::Error::<Runtime>::XdnsRecordAlreadyExists
             );
             assert_eq!(
-                pallet_xdns::XDNSRegistry::<Runtime>::iter().count(),
+                pallet_xdns::Gateways::<Runtime>::iter().count(),
                 DEFAULT_GATEWAYS_IN_STORAGE_COUNT
             );
         });
@@ -114,7 +108,7 @@ fn should_purge_a_xdns_record_successfully() {
         .build()
         .execute_with(|| {
             assert_eq!(
-                pallet_xdns::XDNSRegistry::<Runtime>::iter().count(),
+                pallet_xdns::Gateways::<Runtime>::iter().count(),
                 DEFAULT_GATEWAYS_IN_STORAGE_COUNT
             );
             assert_ok!(XDNS::purge_xdns_record(
@@ -123,10 +117,10 @@ fn should_purge_a_xdns_record_successfully() {
                 *b"gate"
             ));
             assert_eq!(
-                pallet_xdns::XDNSRegistry::<Runtime>::iter().count(),
+                pallet_xdns::Gateways::<Runtime>::iter().count(),
                 DEFAULT_GATEWAYS_IN_STORAGE_COUNT - 1
             );
-            assert!(pallet_xdns::XDNSRegistry::<Runtime>::get(b"gate").is_none());
+            assert!(pallet_xdns::Gateways::<Runtime>::get(b"gate").is_none());
         });
 }
 
@@ -156,7 +150,7 @@ fn should_error_trying_to_purge_a_missing_xdns_record() {
                 pallet_xdns::pallet::Error::<Runtime>::UnknownXdnsRecord
             );
             assert_eq!(
-                pallet_xdns::XDNSRegistry::<Runtime>::iter().count(),
+                pallet_xdns::Gateways::<Runtime>::iter().count(),
                 DEFAULT_GATEWAYS_IN_STORAGE_COUNT
             );
         });
@@ -174,35 +168,10 @@ fn should_error_trying_to_purge_an_xdns_record_if_not_root() {
                 DispatchError::BadOrigin
             );
             assert_eq!(
-                pallet_xdns::XDNSRegistry::<Runtime>::iter().count(),
+                pallet_xdns::Gateways::<Runtime>::iter().count(),
                 DEFAULT_GATEWAYS_IN_STORAGE_COUNT
             );
-            assert!(pallet_xdns::XDNSRegistry::<Runtime>::get(b"gate").is_some());
-        });
-}
-
-#[test]
-fn should_update_ttl_for_a_known_xdns_record() {
-    ExtBuilder::default()
-        .with_standard_sfx_abi()
-        .with_default_xdns_records()
-        .build()
-        .execute_with(|| {
-            assert_ok!(XDNS::update_ttl(
-                Origin::<Runtime>::Root.into(),
-                *b"gate",
-                2
-            ));
-            assert_eq!(
-                pallet_xdns::XDNSRegistry::<Runtime>::iter().count(),
-                DEFAULT_GATEWAYS_IN_STORAGE_COUNT
-            );
-            assert_eq!(
-                pallet_xdns::XDNSRegistry::<Runtime>::get(b"gate")
-                    .unwrap()
-                    .last_finalized,
-                Some(2)
-            );
+            assert!(pallet_xdns::Gateways::<Runtime>::get(b"gate").is_some());
         });
 }
 
@@ -224,31 +193,6 @@ fn should_error_when_trying_to_update_ttl_as_non_root() {
             DispatchError::BadOrigin
         );
     });
-}
-
-#[test]
-fn should_contain_gateway_system_properties() {
-    ExtBuilder::default()
-        .with_standard_sfx_abi()
-        .with_default_xdns_records()
-        .build()
-        .execute_with(|| {
-            let polkadot_xdns_record = pallet_xdns::XDNSRegistry::<Runtime>::get(b"pdot").unwrap();
-            let kusama_xdns_record = pallet_xdns::XDNSRegistry::<Runtime>::get(b"ksma").unwrap();
-            let polkadot_symbol: Vec<u8> =
-                Decode::decode(&mut &polkadot_xdns_record.gateway_sys_props.token_symbol[..])
-                    .unwrap();
-            let kusama_symbol: Vec<u8> =
-                Decode::decode(&mut &kusama_xdns_record.gateway_sys_props.token_symbol[..])
-                    .unwrap();
-
-            assert_eq!(polkadot_xdns_record.gateway_sys_props.ss58_format, 0u16);
-            assert_eq!(kusama_xdns_record.gateway_sys_props.ss58_format, 2u16);
-            assert_eq!(&String::from_utf8_lossy(&polkadot_symbol), "DOT");
-            assert_eq!(&String::from_utf8_lossy(&kusama_symbol), "KSM");
-            assert_eq!(polkadot_xdns_record.gateway_sys_props.token_decimals, 10u8);
-            assert_eq!(kusama_xdns_record.gateway_sys_props.token_decimals, 12u8);
-        });
 }
 
 #[test]
@@ -281,7 +225,7 @@ fn gate_gateway_vendor_returns_error_for_unknown_record() {
         .with_default_xdns_records()
         .build()
         .execute_with(|| {
-            let actual = XDNS::get_gateway_vendor(b"rand");
+            let actual = XDNS::get_verification_vendor(b"rand");
             assert_err!(actual, pallet_xdns::Error::<Runtime>::XdnsRecordNotFound);
         });
 }
@@ -292,7 +236,7 @@ fn gate_gateway_vendor_returns_vendor_for_known_record() {
         .with_default_xdns_records()
         .build()
         .execute_with(|| {
-            let actual = XDNS::get_gateway_vendor(b"pdot");
+            let actual = XDNS::get_verification_vendor(b"pdot");
             assert_ok!(actual, GatewayVendor::Rococo);
         });
 }
