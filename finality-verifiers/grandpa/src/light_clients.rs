98dfff389ab898d1c34f00baf4c30f6530300eb9
MaciejBaj <maciej@t3rn.io>
2023-03-29 19:23:18 +0100

    refactor: update LightClient trait not to rely on Option

--- finality-verifiers/grandpa/src/light_clients.rs
+++ finality-verifiers/grandpa/src/light_clients.rs
@@ -8,6 +8,7 @@ pub use t3rn_light_client_commons::traits::{LightClient, LightClientHeartbeat};
 use sp_runtime::{traits::Header, DispatchError};
 use sp_std::marker::PhantomData;
 use t3rn_abi::types::Bytes;
+use t3rn_light_client_commons::traits::{BlockHeightResult, HeaderResult};
 use t3rn_primitives::GatewayVendor;
 
 pub type RococoInstance = ();
@@ -54,7 +55,7 @@ impl<T, I: 'static> LightClient<T> for PalletInstance<T, I>
 where
     T: Config<RococoInstance> + Config<KusamaInstance> + Config<PolkadotInstance> + Config<I>,
 {
-    fn get_latest_finalized_header(&self) -> Result<Option<Bytes>, DispatchError> {
+    fn get_latest_finalized_header(&self) -> Result<HeaderResult, DispatchError> {
         match self {
             PalletInstance::Rococo(pallet) => pallet.get_latest_finalized_header(),
             PalletInstance::Kusama(pallet) => pallet.get_latest_finalized_header(),
@@ -63,7 +64,9 @@ where
         }
     }
 
-    fn get_latest_finalized_height(&self) -> Result<Option<T::BlockNumber>, DispatchError> {
+    fn get_latest_finalized_height(
+        &self,
+    ) -> Result<BlockHeightResult<T::BlockNumber>, DispatchError> {
         match self {
             PalletInstance::Rococo(pallet) => pallet.get_latest_finalized_height(),
             PalletInstance::Kusama(pallet) => pallet.get_latest_finalized_height(),
@@ -72,7 +75,9 @@ where
         }
     }
 
-    fn get_latest_updated_height(&self) -> Result<Option<T::BlockNumber>, DispatchError> {
+    fn get_latest_updated_height(
+        &self,
+    ) -> Result<BlockHeightResult<T::BlockNumber>, DispatchError> {
         match self {
             PalletInstance::Rococo(pallet) => pallet.get_latest_updated_height(),
             PalletInstance::Kusama(pallet) => pallet.get_latest_updated_height(),
@@ -117,7 +122,7 @@ where
         }
     }
 
-    fn get_current_epoch(&self) -> Result<Option<u32>, DispatchError> {
+    fn get_current_epoch(&self) -> Result<BlockHeightResult<T::BlockNumber>, DispatchError> {
         match self {
             PalletInstance::Rococo(pallet) => pallet.get_current_epoch(),
             PalletInstance::Kusama(pallet) => pallet.get_current_epoch(),
@@ -252,18 +257,29 @@ where
 }
 
 impl<T: Config<I>, I: 'static> LightClient<T> for Pallet<T, I> {
-    fn get_latest_finalized_header(&self) -> Result<Option<Bytes>, DispatchError> {
-        Ok(Pallet::<T, I>::get_latest_finalized_header())
+    fn get_latest_finalized_header(&self) -> Result<HeaderResult, DispatchError> {
+        match Pallet::<T, I>::get_latest_finalized_header() {
+            Some(header) => Ok(HeaderResult::Some(header)),
+            None => Ok(HeaderResult::None),
+        }
     }
 
-    fn get_latest_finalized_height(&self) -> Result<Option<T::BlockNumber>, DispatchError> {
+    fn get_latest_finalized_height(
+        &self,
+    ) -> Result<BlockHeightResult<T::BlockNumber>, DispatchError> {
         let header = Pallet::<T, I>::best_finalized_map();
-        Ok(Some(to_local_block_number::<T, I>(*header.number())?))
+        Ok(BlockHeightResult::Some(to_local_block_number::<T, I>(
+            *header.number(),
+        )?))
     }
 
-    fn get_latest_updated_height(&self) -> Result<Option<T::BlockNumber>, DispatchError> {
+    fn get_latest_updated_height(
+        &self,
+    ) -> Result<BlockHeightResult<T::BlockNumber>, DispatchError> {
         let header = Pallet::<T, I>::best_finalized_map();
-        Ok(Some(to_local_block_number::<T, I>(*header.number())?))
+        Ok(BlockHeightResult::Some(to_local_block_number::<T, I>(
+            *header.number(),
+        )?))
     }
 
     fn get_latest_heartbeat(&self) -> Result<LightClientHeartbeat<T>, DispatchError> {
@@ -290,8 +306,8 @@ impl<T: Config<I>, I: 'static> LightClient<T> for Pallet<T, I> {
         Ok(T::FinalizedConfirmationOffset::get())
     }
 
-    fn get_current_epoch(&self) -> Result<Option<u32>, DispatchError> {
-        Ok(None)
+    fn get_current_epoch(&self) -> Result<BlockHeightResult<T::BlockNumber>, DispatchError> {
+        Ok(BlockHeightResult::None)
     }
 
     fn read_epoch_offset(&self) -> Result<T::BlockNumber, DispatchError> {
