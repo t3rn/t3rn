768d8bb9cb7cd102e6810087594eb21512e87e90
marcin <6998565+3h4x@users.noreply.github.com>
2023-03-30 18:50:22 +0100

    build: sed to update versions

--- scripts/upgrade-runtime-cargo-release.sh
+++ scripts/upgrade-runtime-cargo-release.sh
@@ -6,8 +6,8 @@ sed -i.bak -E "s/v[0-9]+\\.[0-9]\\.[0-9]-rc\\.[0-9]/v${NEW_VERSION}/g" $GITHUB_W
 rm $GITHUB_WORKSPACE/docs/main/docs/collator/testnet/testnet-collator.md.bak
 
 echo "Increment versions for ${PARACHAIN_NAME} runtime"
-gsed -i.bak -r 's/(impl_version:)(.*)/echo \1 $(echo "\2 + 1"\|bc)/ge' $GITHUB_WORKSPACE/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs 
-gsed -i.bak -r 's/(transaction_version:)(.*)/echo \1 $(echo "\2 + 1"\|bc)/ge' $GITHUB_WORKSPACE/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs 
-gsed -i.bak -r 's/(authoring_version:)(.*)/echo \1 $(echo "\2 + 1"\|bc)/ge' $GITHUB_WORKSPACE/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs 
-gsed -i.bak -r 's/(specVersion:)(.*)/echo \1 $(echo "\2 + 1"\|bc)/ge' $GITHUB_WORKSPACE/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs 
+sed -i.bak -r 's/([[:blank:]]+impl_version: )([0-9]+)(.*)/echo "\1$(echo \"\2 + 1\"|bc)\3"/ge' $GITHUB_WORKSPACE/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs
+sed -i.bak -r 's/([[:blank:]]+transaction_version: )([0-9]+)(.*)/echo "\1$(echo \"\2 + 1\"|bc)\3"/ge' $GITHUB_WORKSPACE/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs
+sed -i.bak -r 's/([[:blank:]]+authoring_version: )([0-9]+)(.*)/echo "\1$(echo \"\2 + 1\"|bc)\3"/ge' $GITHUB_WORKSPACE/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs
+sed -i.bak -r 's/([[:blank:]]+spec_version: )([0-9]+)(.*)/echo "\1$(echo \"\2 + 1\"|bc)\3"/ge' $GITHUB_WORKSPACE/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs
 rm $GITHUB_WORKSPACE/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs.bak
