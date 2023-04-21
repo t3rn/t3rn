f5a25f85d6f5d724fb163b4e70ce2c3b9b0c2a1a
marcin <6998565+3h4x@users.noreply.github.com>
2023-04-21 13:19:10 +0200

    build: group zombienet and display how long it took

--- tests/zombienet/zombienet.sh
+++ tests/zombienet/zombienet.sh
@@ -125,7 +125,9 @@ smoke() {
     echo "Running smoke tests.."
     # TODO[Optimisation]: loop through directory and test all
 	# TODO[Optimisation, NotImplemented]: when zombienet can run on a pre-existing network, run it
-    zombienet --provider="$provider" test $working_dir/smoke/0001-is_up_and_registered.feature
+    echo "::group::Zombienet tests..."
+    time zombienet --provider="$provider" test $working_dir/smoke/0001-is_up_and_registered.feature
+    echo "::endgroup::"
 }
 
 upgrade() {
