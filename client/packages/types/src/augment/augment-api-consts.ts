5837cc935f22b2624f0bd7fdd811b61825f3de49
marcin <6998565+3h4x@users.noreply.github.com>
2023-04-28 16:58:38 +0200

    build: fix yarn and npm 🤞

--- client/packages/types/src/augment/augment-api-consts.ts
+++ client/packages/types/src/augment/augment-api-consts.ts
@@ -1,13 +1,19 @@
 // Auto-generated via `yarn polkadot-types-from-chain`, do not edit
 /* eslint-disable */
 
-import type { ApiTypes } from '@polkadot/api-base/types';
+// import type lookup before we augment - in some environments
+// this is required to allow for ambient/previous definitions
+import '@polkadot/api-base/types/consts';
+
+import type { ApiTypes, AugmentedConst } from '@polkadot/api-base/types';
 import type { Option, U8aFixed, u128, u16, u32, u64, u8 } from '@polkadot/types-codec';
 import type { AccountId32, Permill } from '@polkadot/types/interfaces/runtime';
 import type { FrameSupportPalletId, FrameSupportWeightsRuntimeDbWeight, FrameSystemLimitsBlockLength, FrameSystemLimitsBlockWeights, PalletContractsSchedule, SpVersionRuntimeVersion } from '@polkadot/types/lookup';
 
+export type __AugmentedConst<ApiType extends ApiTypes> = AugmentedConst<ApiType>;
+
 declare module '@polkadot/api-base/types/consts' {
-  export interface AugmentedConsts<ApiType extends ApiTypes> {
+  interface AugmentedConsts<ApiType extends ApiTypes> {
     accountManager: {
       escrowAccount: AccountId32 & AugmentedConst<ApiType>;
     };
