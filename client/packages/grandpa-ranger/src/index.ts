458a6d3792e4bfca3ef86d821c9cf0e07057081c
petscheit <etscheitp@gmail.com>
2023-04-28 15:47:25 +0200

    feat: imple header collection and circuit submission logic
    Signed-off-by: petscheit <etscheitp@gmail.com>

--- client/packages/grandpa-ranger/src/index.ts
+++ client/packages/grandpa-ranger/src/index.ts
@@ -1,7 +1,8 @@
+import {generateRange} from "./collect";
+
 require('dotenv').config()
 import { Connection } from './connection';
-import { ApiPromise, Keyring } from '@polkadot/api';
-
+import { cryptoWaitReady } from "@t3rn/sdk"
 
 class GrandpaRanger {
 	circuit: Connection;
@@ -12,11 +13,94 @@ class GrandpaRanger {
 		this.config = config;
 	}
 
+	async start() {
+		await this.connectClients();
+		this.scheduleRangeSubmission();
+	}
+
 	async connectClients() {
-		this.circuit = new Connection(this.config.circuit.rpc1, this.config.circuit.rpc2);
-		await this.circuit.connect();
+		await cryptoWaitReady()
+		this.circuit = new Connection(this.config.circuit.rpc1, this.config.circuit.rpc2, true, this.config.circuitSigner);
+		this.circuit.connect();
+		this.target = new Connection(this.config.target.rpc1, this.config.target.rpc2, false);
+		this.target.connect();
+		this.collectAndSubmit(() => {})
+	}
+
+	async collectAndSubmit(resolve: any) {
+		let range = await generateRange(this.config, this.circuit, this.target)
+			.then(range => range)
+			.catch((e) => {
+				console.log(e);
+				// potentially we want to introduce a retry logic here
+				return [];
+			})
+
+		if(range.length > 0) {
+			console.log(`Submitting ${range.length} ranges`)
+			this.submitToCircuit(range)
+				.then(() => resolve())
+				.catch((e) => {
+					console.log(e);
+					resolve() // resolve anyway
+				})
+		}
+	}
+
+	async submitToCircuit(range: any[]) {
+		// limit to 10 batches per tx
+		if(range.length > 10) {
+			range = range.slice(0, 10);
+		}
+		new Promise((resolve, reject) => {
+			if(this.circuit.sdk && this.circuit.isActive) {
+				let tx = this.circuit.sdk.circuit.tx.createBatch(range.map(args => {
+					let submit;
+					if(this.config.targetGatewayId === "roco") {
+						submit = this.circuit.client.tx.rococoBridge.submitHeaders
+					} else if (this.config.targetGatewayId === "ksma") {
+						submit = this.circuit.client.tx.kusamaBridge.submitHeaders
+					} else {
+						throw new Error(`Unknown targetGatewayId ${this.config.targetGatewayId}`)
+					}
+					return submit(
+						args.range,
+						args.signed_header,
+						args.justification
+					)
+				}))
+
+				this.circuit.sdk.circuit.tx.signAndSendSafe(tx)
+					.then((result) => {
+						console.log(`Submitted ${range.length} ranges to circuit`)
+						console.log(`Tx hash: ${result}`)
+					})
+
+				resolve
+			} else {
+				// we should prob have some retry logic here instead
+				reject(new Error(`Circuit client is not active!`))
+			}
+		})
+
+	}
+
+	async scheduleRangeSubmission() {
+		while(true) {
+			await new Promise((resolve, reject) => {
+				setTimeout(
+					() => {
+						this.collectAndSubmit(resolve)
+							.catch(() => reject) // we should never get here with the setup above
+					},
+					this.config.rangeBreak * 1000
+				)
+			})
+		}
 	}
 }
+
+
 
 
 (async () => {
@@ -27,6 +111,6 @@ class GrandpaRanger {
 		config = require('../config/local.ts').default;
 	}
 	const grandpaRanger = new GrandpaRanger(config);
-	await grandpaRanger.connectClients();
+	await grandpaRanger.start();
 
 })()
