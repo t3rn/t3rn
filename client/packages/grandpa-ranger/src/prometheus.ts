e68e2b9327489b6832443e2dac1569ca84c5cc99
petscheit <etscheitp@gmail.com>
2023-04-28 21:44:32 +0200

    feat: impl prometheus metrics
    Signed-off-by: petscheit <etscheitp@gmail.com>

---
+++ client/packages/grandpa-ranger/src/prometheus.ts
@@ -0,0 +1,129 @@
+const client = require('prom-client');
+const http = require('http')
+const url = require('url')
+export class Prometheus {
+	register: any;
+	circuitHeight: any
+	targetHeight: any
+	nextSubmission: any
+	successCount: any
+	errorCount: any
+	circuitDisconnected: any
+	targetDisconnected: any
+	rangeBreak: any
+	errors: any
+	successes: any
+
+	constructor() {
+		const Registry = client.Registry;
+		this.register = new Registry();
+		this.createMetrics()
+	}
+
+	createMetrics() {
+		const collectDefaultMetrics = client.collectDefaultMetrics;
+		collectDefaultMetrics({ register: this.register });
+		this.circuitHeight = new client.Gauge(
+			{
+				name: 'circuit_height',
+				help: 'The header height stored on circuit',
+				registers: [this.register]
+			}
+		);
+
+		this.targetHeight = new client.Gauge(
+			{
+				name: 'target_height',
+				help: 'The current header height on the target',
+				registers: [this.register]
+			}
+		);
+
+		this.nextSubmission = new client.Gauge(
+			{
+				name: 'next_submission',
+				help: 'Unix timestamp of the next scheduled submission',
+				registers: [this.register]
+			}
+		)
+
+		this.successes = new client.Counter(
+			{
+				name: 'successes',
+				help: 'Information on the latest successful submissions',
+				registers: [this.register],
+				labelNames: ['rangeSize', 'timestamp', 'circuitBlock']
+			}
+		);
+
+		this.successCount = new client.Counter(
+			{
+				name: 'success_counter',
+				help: 'Number of successful submissions',
+				registers: [this.register],
+			}
+		)
+
+		this.errors = new client.Counter(
+			{
+				name: 'errors',
+				help: 'Information on the latest errored submissions',
+				registers: [this.register],
+				labelNames: ['rangeSize', 'timestamp']
+			}
+		);
+
+		this.errorCount = new client.Counter(
+			{
+				name: 'error_counter',
+				help: 'Number of errored submissions',
+				registers: [this.register],
+			}
+		)
+
+		this.circuitDisconnected = new client.Counter(
+			{
+				name: 'circuit_disconnect',
+				help: 'Number of times circuit rpc server has disconnected',
+				registers: [this.register],
+				labelNames: ['endpoint', 'timestamp']
+			}
+		)
+
+		this.targetDisconnected = new client.Counter(
+			{
+				name: 'target_disconnect',
+				help: 'Number of times target rpc server has disconnected',
+				registers: [this.register],
+				labelNames: ['endpoint', 'timestamp']
+			}
+		)
+
+		this.rangeBreak = new client.Counter({
+			name: 'range_loop',
+			help: 'The number of seconds between each range submission',
+			registers: [this.register]
+		})
+
+		this.startServer()
+	}
+
+	startServer() {
+		const server = http.createServer(async (req, res) => {
+		  try {
+			res.setHeader('Content-Type', this.register.contentType);
+			const metrics = await this.register.metrics();
+			res.end(metrics);
+		  } catch (error) {
+			res.statusCode = 500;
+			res.end(error.toString());
+		  }
+		});
+
+		const port = 8080;
+		server.listen(port, () => {
+		  console.log(`Metrics server listening on port ${port}`);
+		});
+	}
+
+}
