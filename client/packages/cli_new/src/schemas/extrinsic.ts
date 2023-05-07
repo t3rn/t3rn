ed54de2d796a2e5e96f2943e994c4cfba36a4f97
Victor Aremu <victor.olorunbunmi@gmail.com>
2023-05-07 23:09:45 +0100

    feat: add support for speed mode

--- client/packages/cli_new/src/schemas/extrinsic.ts
+++ client/packages/cli_new/src/schemas/extrinsic.ts
@@ -1,12 +1,16 @@
 import { z } from "zod"
 import { SideEffectSchema } from "./sfx.ts"
 
+const SpeedModeEnum = z.enum(["Fast", "Rational", "Finalized"], {
+  required_error: "Speed mode is required",
+  invalid_type_error: "Speed mode must be one of Fast, Rational, Finalized",
+})
+
+export type SpeedMode = z.infer<typeof SpeedModeEnum>
+
 export const ExtrinsicSchema = z.object({
   sideEffects: z.array(SideEffectSchema),
-  sequential: z.boolean({
-    required_error: "Sequential is required",
-    invalid_type_error: "Sequential must be a boolean",
-  }),
+  speed_mode: SpeedModeEnum,
 })
 
 export type Extrinsic = z.infer<typeof ExtrinsicSchema>
