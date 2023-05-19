35a4e4fa77960f39852d4783a25ceeebc90a9aea
MaciejBaj <maciej@t3rn.io>
2023-05-19 08:57:26 +0100

    feat: add repatriation from slash treasury to Rewards

--- primitives/src/rewards.rs
+++ primitives/src/rewards.rs
@@ -1,8 +1,11 @@
 use frame_support::pallet_prelude::*;
 use sp_core::H256;
 use sp_std::prelude::*;
+use t3rn_types::fsx::FullSideEffect;
 
-pub trait RewardsWriteApi<Account, Error> {
-    fn repatriate_executors_from_slash_treasury(sfx_id: &H256) -> bool;
-    fn repatriate_requesters_from_slash_treasury(sfx_id: &H256) -> bool;
+pub trait RewardsWriteApi<Account, Balance, BlockNumber> {
+    fn repatriate_executor_from_slash_treasury(
+        sfx_id: &H256,
+        fsx: &FullSideEffect<Account, BlockNumber, Balance>,
+    ) -> bool;
 }
