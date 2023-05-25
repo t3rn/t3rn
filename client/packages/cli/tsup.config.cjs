82b505f3c241b630dfd1ce3fafd68a8aa0d38227
Victor Aremu <victor.olorunbunmi@gmail.com>
2023-05-26 00:46:03 +0100

    feat: update CLI to use ESM as build output

---
+++ client/packages/cli/tsup.config.cjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+  entry: ["src/cli.ts"],
+  splitting: false,
+  sourcemap: false,
+  clean: true,
+  format: ["esm"],
+  external: ["esbuild"],
+})
