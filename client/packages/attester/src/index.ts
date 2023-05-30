128012736b6252e02a2a204d7600fa314a9cabea
marcin <6998565+3h4x@users.noreply.github.com>
2023-05-30 12:38:55 +0200

    feat: added attester nodejs app

---
+++ client/packages/attester/src/index.ts
@@ -0,0 +1,156 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config()
+import { Connection } from './connection'
+import { cryptoWaitReady } from "@t3rn/sdk"
+import { Prometheus } from "./prometheus"
+import fs from 'fs'
+import pino from "pino"
+import ethUtil from 'ethereumjs-util'
+import { hexToU8a } from '@polkadot/util'
+
+// Determine if pretty printing is enabled based on the PROFILE environment variable
+const isPrettyPrintEnabled = process.env.PROFILE  === 'local' || process.env.LOG_PRETTY === 'true'
+
+const { stderr } = process
+// Create a writable stream that discards the output
+const NullWritable = fs.createWriteStream('/dev/null')
+
+// Redirect stdout to the NullWritable stream
+// stdout.write = NullWritable.write.bind(NullWritable)
+stderr.write = NullWritable.write.bind(NullWritable)
+
+const loggerConfig = {
+	level: process.env.LOG_LEVEL || 'info',
+	formatters: {
+	  level: (label) => {
+		return { level: label }
+	  },
+	},
+	base: undefined,
+	stream: process.stdout,
+	transport: isPrettyPrintEnabled ? {
+		target: 'pino-pretty'
+	  } : undefined,
+
+  }
+
+  const logger = pino(loggerConfig)
+
+// Apply the pino-pretty formatter if pretty printing is enabled
+
+class Attester {
+	circuit: Connection
+	target: Connection
+	config: any
+	prometheus: Prometheus
+
+	constructor(config: any, keys: any) {
+		this.config = config
+		this.prometheus = new Prometheus(this.config.targetGatewayId)
+		// this.prometheus.rangeInterval.inc({target: this.target}, this.config.rangeInterval)
+		// this.prometheus.nextSubmission.set({target: this.target}, Date.now() + this.config.rangeInterval * 1000)
+	}
+
+	async start() {
+		await this.connectClients()
+
+		await new Promise<void>((resolve) => {
+			const checkClient = () => {
+			  if (this.circuit?.sdk?.client) {
+				resolve()
+			  } else {
+				setTimeout(checkClient, 2000)
+			  }
+			}
+			checkClient()
+		  })
+		
+		this.listenEvents()
+	}
+
+	async connectClients() {
+		await cryptoWaitReady()
+		this.circuit = new Connection(this.config.circuit.rpc1, this.config.circuit.rpc2, true, this.prometheus, this.config.targetGatewayId, this.config.circuitSigner)
+		this.circuit.connect()
+	}
+
+	async listenEvents() {
+        // Subscribe to the NewAttestationMessageHash event
+        this.circuit.sdk?.client.query.system.events(async (events) => {
+            logger.info(`\nReceived ${events.length} events`)
+            // Loop through the Vec<EventRecord>
+            await Promise.all(
+                events.map(async (record) => {
+                    // Extract the phase, event and the event types
+                    const { event } = record
+					logger.debug(event.toHuman())
+
+                    if (event.section == 'attesters') {
+                        logger.info(`\t${event.section}:${event.method}:: (phase=${record.phase})`)
+
+                        switch (event.method) {
+                            case 'NewAttestationMessageHash': {
+                                const [targetId, messageHash, executionVendor] = event.data
+                                logger.info(`Received the attestation message hash request to sign`,{targetId: targetId.toString(), messageHash: messageHash.toHex(), executionVendor: executionVendor.toString()} )
+                                // Submit the attestation for the given target ID for the given message hash for each attester's key in the keys.json file
+								if (executionVendor.toString() == 'Substrate') {
+									console.warn('Substrate unhandled yet')
+								} else if (executionVendor.toString() == 'Ed25519') {
+									console.warn('Ed25519 unhandled yet')
+								} else if (executionVendor.toString() == 'EVM') {
+									// Generate the signature for the message hash
+									const privateKey = Buffer.from(hexToU8a(this.config.keys.ethereum.privateKey))
+
+									const sigObj = ethUtil.ecsign(Buffer.from(hexToU8a(messageHash.toHex())), privateKey)
+									const signature = ethUtil.toRpcSig(sigObj.v, sigObj.r, sigObj.s)
+
+									const tx = this.circuit.client.tx.attesters.submitAttestation(messageHash, signature, targetId)
+
+									logger.info('Executed', { executionVendor: executionVendor.toString(), targetId: targetId.toString(), messageHash: messageHash.toHex(), hash: tx.hash.toHex()})
+								}
+
+                                break
+                            }
+                            case 'NewTargetProposed': {
+                                logger.info(`Received the new target proposed event`)
+                                break
+                            }
+                            default: {
+                                break
+                            }
+                        }
+                    }
+                }),
+            )
+        })
+	}
+}
+
+(async () => {
+	let config: any
+	if(process.env.PROFILE === 'prod') {
+		// eslint-disable-next-line @typescript-eslint/no-var-requires
+		config = require('../config/prod.ts').default
+	} else if (process.env.PROFILE === 'roco') {
+		// eslint-disable-next-line @typescript-eslint/no-var-requires
+		config = require('../config/roco.ts').default
+	} else {
+		// eslint-disable-next-line @typescript-eslint/no-var-requires
+		config = require('../config/local.ts').default
+	}
+	const keys = {
+		ethereum: {
+			privateKey: process.env.PRIVATE_KEY_ETH,
+		},
+		substrate: {
+			privateKey: process.env.PRIVATE_KEY_SUBSTRATE,
+		},
+		btc: {
+			privateKey:process.env.PRIVATE_KEY_BTC,
+		} ,
+	}
+	const attester = new Attester(config, keys)
+	logger.info(`Starting attester`)
+	await attester.start()
+
+})()
---
+++ client/packages/attester/src/prometheus.ts
@@ -0,0 +1,90 @@
+import client from 'prom-client'
+import http from 'http'
+export class Prometheus {
+	circuitActive: boolean
+	targetActive: boolean
+	targetDisconnectsTotal: any
+	circuitDisconnectsTotal: any
+	register: any
+	circuitHeight: any
+	targetHeight: any
+	circuitDisconnected: any
+	target: string
+
+	constructor(target: string) {
+		this.target = target
+		const Registry = client.Registry
+		this.register = new Registry()
+		this.createMetrics()
+	}
+
+	createMetrics() {
+		const collectDefaultMetrics = client.collectDefaultMetrics
+		collectDefaultMetrics({ register: this.register })
+		this.circuitHeight = new client.Gauge(
+			{
+				name: 'circuit_height',
+				help: 'The header height stored on circuit',
+				registers: [this.register],
+				labelNames: ['target']
+			}
+		)
+
+		this.targetHeight = new client.Gauge(
+			{
+				name: 'target_height',
+				help: 'The current header height on the target',
+				registers: [this.register],
+				labelNames: ['target']
+			}
+		)
+
+		this.circuitDisconnected = new client.Counter(
+			{
+				name: 'circuit_disconnect',
+				help: 'Information on circuit disconnections',
+				registers: [this.register],
+				labelNames: ['endpoint', 'target']
+			}
+		)
+
+		this.circuitDisconnectsTotal = new client.Counter(
+			{
+				name: 'circuit_disconnects_total',
+				help: 'Number of times circuit rpc server has disconnected',
+				registers: [this.register],
+				labelNames: ['target']
+			}
+		)
+
+		this.startServer()
+	}
+
+	startServer() {
+		const server = http.createServer(async (req, res) => {
+		  try {
+			if (req.url === '/metrics') {
+			  res.setHeader('Content-Type', this.register.contentType)
+			  const metrics = await this.register.metrics()
+			  res.end(metrics)
+			} else if (req.url === '/status') {
+			  res.setHeader('Content-Type', 'text/plain')
+			  res.statusCode = this.circuitActive && this.targetActive ? 200 : 500
+			  res.end(JSON.stringify({circuitActive: this.circuitActive, targetActive: this.targetActive}))
+			} else {
+			  res.statusCode = 404
+			  res.end('Not found.')
+			}
+		  } catch (error) {
+			res.statusCode = 500
+			res.end(error.toString())
+		  }
+		})
+
+		const port = 8080
+		server.listen(port, () => {
+		  console.log(`Metrics server listening on port ${port}`)
+		})
+	}
+
+}
