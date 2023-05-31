ab989875b48418269d7efeec6beef7409642c87b
marcin <6998565+3h4x@users.noreply.github.com>
2023-05-31 13:53:54 +0200

    chore: unify spaces, quotes, commas, parens with prettier

--- client/packages/attester/src/connection.ts
+++ client/packages/attester/src/connection.ts
@@ -2,108 +2,118 @@ import { Sdk, ApiPromise, WsProvider, Keyring } from "@t3rn/sdk"
 import { Prometheus } from "./prometheus"
 
 export class Connection {
-  client: ApiPromise
-  provider: WsProvider
-  rpc1: any
-  usingPrimaryRpc = true
-  rpc2: any
-  isCircuit: boolean
-  isActive = false
-  sdk: Sdk | undefined
-  signer: any
-  prometheus: Prometheus
-  target: string
+    client: ApiPromise
+    provider: WsProvider
+    rpc1: any
+    usingPrimaryRpc = true
+    rpc2: any
+    isCircuit: boolean
+    isActive = false
+    sdk: Sdk | undefined
+    signer: any
+    prometheus: Prometheus
+    target: string
 
-  constructor(
-    rpc1: any,
-    rpc2: any,
-    isCircuit: boolean,
-    prometheus: Prometheus,
-    target: string,
-    substratePrivateKey: string
-  ) {
-    this.rpc1 = rpc1
-    this.rpc2 = rpc2
-    this.usingPrimaryRpc = true
-    this.isCircuit = isCircuit
-    this.prometheus = prometheus
-    this.target = target
-    const keyring = new Keyring({ type: "sr25519" })
-    this.signer = keyring.addFromMnemonic(substratePrivateKey)
-  }
-
-  async connect() {
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      try {
-        this.provider = this.createProvider()
-        await this.setListeners()
-        break
-      } catch (e) {
-        this.prometheus.circuitDisconnectsTotal.inc({ target: this.target })
-        this.usingPrimaryRpc = !this.usingPrimaryRpc // toggle connection
-        console.log(`Retrying in 2 second with ${this.currentProvider().ws}`)
-        await new Promise((resolve, _reject) => setTimeout(resolve, 2000))
-      }
+    constructor(
+        rpc1: any,
+        rpc2: any,
+        isCircuit: boolean,
+        prometheus: Prometheus,
+        target: string,
+        substratePrivateKey: string
+    ) {
+        this.rpc1 = rpc1
+        this.rpc2 = rpc2
+        this.usingPrimaryRpc = true
+        this.isCircuit = isCircuit
+        this.prometheus = prometheus
+        this.target = target
+        const keyring = new Keyring({ type: "sr25519" })
+        this.signer = keyring.addFromMnemonic(substratePrivateKey)
     }
-  }
 
-  async setListeners() {
-    return new Promise((resolve, reject) => {
-      this.provider.on("connected", async () => {
-        this.isActive = true
-        console.log(`Connected to ${this.currentProvider().ws}`)
-        if (this.isCircuit) {
-          this.prometheus.circuitActive = true
-          const sdk = new Sdk(this.provider, this.signer)
-          this.sdk = sdk
-          this.client = await sdk.init()
-        } else {
-          this.prometheus.targetActive = true
-          this.client = await ApiPromise.create({
-            provider: this.provider,
-          })
-
-          // update prometheus metrics with incoming blocks
-          this.client.derive.chain.subscribeNewHeads(header => {
-            this.prometheus.targetHeight.set(header.number.toNumber())
-          })
+    async connect() {
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+            try {
+                this.provider = this.createProvider()
+                await this.setListeners()
+                break
+            } catch (e) {
+                this.prometheus.circuitDisconnectsTotal.inc({
+                    target: this.target,
+                })
+                this.usingPrimaryRpc = !this.usingPrimaryRpc // toggle connection
+                console.log(
+                    `Retrying in 2 second with ${this.currentProvider().ws}`
+                )
+                await new Promise((resolve, _reject) =>
+                    setTimeout(resolve, 2000)
+                )
+            }
         }
-      })
+    }
 
-      this.provider.on("disconnected", () => {
-        this.isActive = false
-        this.isCircuit
-          ? (this.prometheus.circuitActive = false)
-          : (this.prometheus.targetActive = false)
-        console.log(`Disconnected from ${this.currentProvider().ws}`)
-        this.provider.disconnect()
-        if (this.client) {
-          this.client.disconnect()
-        }
-        reject()
-      })
+    async setListeners() {
+        return new Promise((resolve, reject) => {
+            this.provider.on("connected", async () => {
+                this.isActive = true
+                console.log(`Connected to ${this.currentProvider().ws}`)
+                if (this.isCircuit) {
+                    this.prometheus.circuitActive = true
+                    const sdk = new Sdk(this.provider, this.signer)
+                    this.sdk = sdk
+                    this.client = await sdk.init()
+                } else {
+                    this.prometheus.targetActive = true
+                    this.client = await ApiPromise.create({
+                        provider: this.provider,
+                    })
 
-      this.provider.on("error", () => {
-        this.isActive = false
-        this.isCircuit
-          ? (this.prometheus.circuitActive = false)
-          : (this.prometheus.targetActive = false)
-        console.log(`Error from ${this.currentProvider().ws}`)
-        this.provider.disconnect()
-        if (this.client) {
-          this.client.disconnect()
-        }
-        reject()
-      })
-    })
-  }
+                    // update prometheus metrics with incoming blocks
+                    this.client.derive.chain.subscribeNewHeads((header) => {
+                        this.prometheus.targetHeight.set(
+                            header.number.toNumber()
+                        )
+                    })
+                }
+            })
 
-  currentProvider(): any {
-    return this.usingPrimaryRpc ? this.rpc1 : this.rpc2
-  }
+            this.provider.on("disconnected", () => {
+                this.isActive = false
+                this.isCircuit
+                    ? (this.prometheus.circuitActive = false)
+                    : (this.prometheus.targetActive = false)
+                console.log(`Disconnected from ${this.currentProvider().ws}`)
+                this.provider.disconnect()
+                if (this.client) {
+                    this.client.disconnect()
+                }
+                reject()
+            })
 
-  createProvider() {
-    return new WsProvider(this.usingPrimaryRpc ? this.rpc1.ws : this.rpc2.ws)
-  }
+            this.provider.on("error", () => {
+                this.isActive = false
+                this.isCircuit
+                    ? (this.prometheus.circuitActive = false)
+                    : (this.prometheus.targetActive = false)
+                console.log(`Error from ${this.currentProvider().ws}`)
+                this.provider.disconnect()
+                if (this.client) {
+                    this.client.disconnect()
+                }
+                reject()
+            })
+        })
+    }
+
+    currentProvider(): any {
+        return this.usingPrimaryRpc ? this.rpc1 : this.rpc2
+    }
+
+    createProvider() {
+        return new WsProvider(
+            this.usingPrimaryRpc ? this.rpc1.ws : this.rpc2.ws
+        )
+    }
 }
--- client/packages/attester/src/index.ts
+++ client/packages/attester/src/index.ts
@@ -10,7 +10,7 @@ import { hexToU8a } from "@polkadot/util"
 
 // Determine if pretty printing is enabled based on the PROFILE environment variable
 const isPrettyPrintEnabled =
-  process.env.PROFILE === "local" || process.env.LOG_PRETTY === "true"
+    process.env.PROFILE === "local" || process.env.LOG_PRETTY === "true"
 
 const { stderr } = process
 // Create a writable stream that discards the output
@@ -21,19 +21,19 @@ const NullWritable = fs.createWriteStream("/dev/null")
 stderr.write = NullWritable.write.bind(NullWritable)
 
 const loggerConfig = {
-  level: process.env.LOG_LEVEL || "info",
-  formatters: {
-    level: label => {
-      return { level: label }
+    level: process.env.LOG_LEVEL || "info",
+    formatters: {
+        level: (label) => {
+            return { level: label }
+        },
     },
-  },
-  base: undefined,
-  stream: process.stdout,
-  transport: isPrettyPrintEnabled
-    ? {
-        target: "pino-pretty",
-      }
-    : undefined,
+    base: undefined,
+    stream: process.stdout,
+    transport: isPrettyPrintEnabled
+        ? {
+              target: "pino-pretty",
+          }
+        : undefined,
 }
 
 const logger = pino(loggerConfig)
@@ -41,151 +41,165 @@ const logger = pino(loggerConfig)
 // Apply the pino-pretty formatter if pretty printing is enabled
 
 class Attester {
-  circuit: Connection
-  target: Connection
-  config: any
-  prometheus: Prometheus
+    circuit: Connection
+    target: Connection
+    config: any
+    prometheus: Prometheus
 
-  constructor(config: any, keys: any) {
-    this.config = config
-    this.prometheus = new Prometheus(this.config.targetGatewayId)
-    // this.prometheus.rangeInterval.inc({target: this.target}, this.config.rangeInterval)
-    // this.prometheus.nextSubmission.set({target: this.target}, Date.now() + this.config.rangeInterval * 1000)
-  }
+    constructor(config: any, keys: any) {
+        this.config = config
+        this.prometheus = new Prometheus(this.config.targetGatewayId)
+        // this.prometheus.rangeInterval.inc({target: this.target}, this.config.rangeInterval)
+        // this.prometheus.nextSubmission.set({target: this.target}, Date.now() + this.config.rangeInterval * 1000)
+    }
 
-  async start() {
-    await this.connectClients()
+    async start() {
+        await this.connectClients()
 
-    await new Promise<void>(resolve => {
-      const checkClient = () => {
-        if (this.circuit?.sdk?.client) {
-          resolve()
-        } else {
-          setTimeout(checkClient, 2000)
-        }
-      }
-      checkClient()
-    })
-
-    this.listenEvents()
-  }
-
-  async connectClients() {
-    await cryptoWaitReady()
-    this.circuit = new Connection(
-      this.config.circuit.rpc1,
-      this.config.circuit.rpc2,
-      true,
-      this.prometheus,
-      this.config.targetGatewayId,
-      this.config.circuitSigner
-    )
-    this.circuit.connect()
-  }
-
-  async listenEvents() {
-    // Subscribe to the NewAttestationMessageHash event
-    this.circuit.sdk?.client.query.system.events(async events => {
-      logger.info(`\nReceived ${events.length} events`)
-      // Loop through the Vec<EventRecord>
-      await Promise.all(
-        events.map(async record => {
-          // Extract the phase, event and the event types
-          const { event } = record
-          logger.debug(event.toHuman())
-
-          if (event.section == "attesters") {
-            logger.info(
-              `\t${event.section}:${event.method}:: (phase=${record.phase})`
-            )
-
-            switch (event.method) {
-              case "NewAttestationMessageHash": {
-                const [targetId, messageHash, executionVendor] = event.data
-                logger.info(
-                  `Received the attestation message hash request to sign`,
-                  {
-                    targetId: targetId.toString(),
-                    messageHash: messageHash.toHex(),
-                    executionVendor: executionVendor.toString(),
-                  }
-                )
-                // Submit the attestation for the given target ID for the given message hash for each attester's key in the keys.json file
-                if (executionVendor.toString() == "Substrate") {
-                  console.warn("Substrate unhandled yet")
-                } else if (executionVendor.toString() == "Ed25519") {
-                  console.warn("Ed25519 unhandled yet")
-                } else if (executionVendor.toString() == "EVM") {
-                  // Generate the signature for the message hash
-                  const privateKey = Buffer.from(
-                    hexToU8a(this.config.keys.ethereum.privateKey)
-                  )
-
-                  const sigObj = ethUtil.ecsign(
-                    Buffer.from(hexToU8a(messageHash.toHex())),
-                    privateKey
-                  )
-                  const signature = ethUtil.toRpcSig(
-                    sigObj.v,
-                    sigObj.r,
-                    sigObj.s
-                  )
-
-                  const tx = this.circuit.client.tx.attesters.submitAttestation(
-                    messageHash,
-                    signature,
-                    targetId
-                  )
-
-                  logger.info("Executed", {
-                    executionVendor: executionVendor.toString(),
-                    targetId: targetId.toString(),
-                    messageHash: messageHash.toHex(),
-                    hash: tx.hash.toHex(),
-                  })
+        await new Promise<void>((resolve) => {
+            const checkClient = () => {
+                if (this.circuit?.sdk?.client) {
+                    resolve()
+                } else {
+                    setTimeout(checkClient, 2000)
                 }
-
-                break
-              }
-              case "NewTargetProposed": {
-                logger.info(`Received the new target proposed event`)
-                break
-              }
-              default: {
-                break
-              }
             }
-          }
+            checkClient()
         })
-      )
-    })
-  }
+
+        this.listenEvents()
+    }
+
+    async connectClients() {
+        await cryptoWaitReady()
+        this.circuit = new Connection(
+            this.config.circuit.rpc1,
+            this.config.circuit.rpc2,
+            true,
+            this.prometheus,
+            this.config.targetGatewayId,
+            this.config.circuitSigner
+        )
+        this.circuit.connect()
+    }
+
+    async listenEvents() {
+        // Subscribe to the NewAttestationMessageHash event
+        this.circuit.sdk?.client.query.system.events(async (events) => {
+            logger.info(`\nReceived ${events.length} events`)
+            // Loop through the Vec<EventRecord>
+            await Promise.all(
+                events.map(async (record) => {
+                    // Extract the phase, event and the event types
+                    const { event } = record
+                    logger.debug(event.toHuman())
+
+                    if (event.section == "attesters") {
+                        logger.info(
+                            `\t${event.section}:${event.method}:: (phase=${record.phase})`
+                        )
+
+                        switch (event.method) {
+                            case "NewAttestationMessageHash": {
+                                const [targetId, messageHash, executionVendor] =
+                                    event.data
+                                logger.info(
+                                    `Received the attestation message hash request to sign`,
+                                    {
+                                        targetId: targetId.toString(),
+                                        messageHash: messageHash.toHex(),
+                                        executionVendor:
+                                            executionVendor.toString(),
+                                    }
+                                )
+                                // Submit the attestation for the given target ID for the given message hash for each attester's key in the keys.json file
+                                if (executionVendor.toString() == "Substrate") {
+                                    console.warn("Substrate unhandled yet")
+                                } else if (
+                                    executionVendor.toString() == "Ed25519"
+                                ) {
+                                    console.warn("Ed25519 unhandled yet")
+                                } else if (
+                                    executionVendor.toString() == "EVM"
+                                ) {
+                                    // Generate the signature for the message hash
+                                    const privateKey = Buffer.from(
+                                        hexToU8a(
+                                            this.config.keys.ethereum.privateKey
+                                        )
+                                    )
+
+                                    const sigObj = ethUtil.ecsign(
+                                        Buffer.from(
+                                            hexToU8a(messageHash.toHex())
+                                        ),
+                                        privateKey
+                                    )
+                                    const signature = ethUtil.toRpcSig(
+                                        sigObj.v,
+                                        sigObj.r,
+                                        sigObj.s
+                                    )
+
+                                    const tx =
+                                        this.circuit.client.tx.attesters.submitAttestation(
+                                            messageHash,
+                                            signature,
+                                            targetId
+                                        )
+
+                                    logger.info("Executed", {
+                                        executionVendor:
+                                            executionVendor.toString(),
+                                        targetId: targetId.toString(),
+                                        messageHash: messageHash.toHex(),
+                                        hash: tx.hash.toHex(),
+                                    })
+                                }
+
+                                break
+                            }
+                            case "NewTargetProposed": {
+                                logger.info(
+                                    `Received the new target proposed event`
+                                )
+                                break
+                            }
+                            default: {
+                                break
+                            }
+                        }
+                    }
+                })
+            )
+        })
+    }
 }
 
 ;(async () => {
-  let config: any
-  if (process.env.PROFILE === "prod") {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    config = require("../config/prod.ts").default
-  } else if (process.env.PROFILE === "roco") {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    config = require("../config/roco.ts").default
-  } else {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    config = require("../config/local.ts").default
-  }
-  const keys = {
-    ethereum: {
-      privateKey: process.env.PRIVATE_KEY_ETH,
-    },
-    substrate: {
-      privateKey: process.env.PRIVATE_KEY_SUBSTRATE,
-    },
-    btc: {
-      privateKey: process.env.PRIVATE_KEY_BTC,
-    },
-  }
-  const attester = new Attester(config, keys)
-  logger.info(`Starting attester`)
-  await attester.start()
+    let config: any
+    if (process.env.PROFILE === "prod") {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        config = require("../config/prod.ts").default
+    } else if (process.env.PROFILE === "roco") {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        config = require("../config/roco.ts").default
+    } else {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        config = require("../config/local.ts").default
+    }
+    const keys = {
+        ethereum: {
+            privateKey: process.env.PRIVATE_KEY_ETH,
+        },
+        substrate: {
+            privateKey: process.env.PRIVATE_KEY_SUBSTRATE,
+        },
+        btc: {
+            privateKey: process.env.PRIVATE_KEY_BTC,
+        },
+    }
+    const attester = new Attester(config, keys)
+    logger.info(`Starting attester`)
+    await attester.start()
 })()
