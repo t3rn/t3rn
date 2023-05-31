45384ff14c626257dd1b7dafced1fd8fdf63849c
marcin <6998565+3h4x@users.noreply.github.com>
2023-05-31 16:59:18 +0200

    feat: add logging lib

---
+++ client/packages/attester/src/logging.ts
@@ -0,0 +1,32 @@
+import fs from 'fs'
+import pino from 'pino'
+
+// Determine if pretty printing is enabled based on the PROFILE environment variable
+const isPrettyPrintEnabled =
+    process.env.PROFILE === 'local' || process.env.LOG_PRETTY === 'true'
+
+const { stderr } = process
+// Create a writable stream that discards the output
+const NullWritable = fs.createWriteStream('/dev/null')
+
+// Redirect stdout to the NullWritable stream
+// stdout.write = NullWritable.write.bind(NullWritable)
+stderr.write = NullWritable.write.bind(NullWritable)
+
+const loggerConfig = {
+    level: process.env.LOG_LEVEL || 'info',
+    formatters: {
+        level: (label) => {
+            return { level: label }
+        },
+    },
+    base: undefined,
+    stream: process.stdout,
+    transport: isPrettyPrintEnabled
+        ? {
+              target: 'pino-pretty',
+          }
+        : undefined,
+}
+
+export const logger = pino(loggerConfig)
