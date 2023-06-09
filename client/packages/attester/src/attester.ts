530532fbbbbf203e7c04643b318c07be92430f91
marcin <6998565+3h4x@users.noreply.github.com>
2023-06-09 15:20:44 +0200

    fix: working attesting + start refactor

--- client/packages/attester/src/attester.ts
+++ client/packages/attester/src/attester.ts
@@ -2,7 +2,8 @@ import { Connection } from './connection'
 import { cryptoWaitReady } from '@t3rn/sdk'
 import { logger } from './logging'
 import { Prometheus } from './prometheus'
-import ethUtil from 'ethereumjs-util'
+import * as ethUtil from "ethereumjs-util"
+
 import { hexToU8a } from '@polkadot/util'
 
 export class Attester {
@@ -18,7 +19,7 @@ export class Attester {
     }
 
     async start() {
-        await this.connectClients()
+		await this.connectClients()
 
         await new Promise<void>((resolve) => {
             const checkClient = () => {
@@ -47,21 +48,24 @@ export class Attester {
 
     async listenEvents() {
         logger.info('Listening to events')
+
         // Subscribe to the NewAttestationMessageHash event
         this.circuit.sdk?.client.query.system.events(async (events) => {
             logger.debug({ events_count: events.length }, `Received events`)
             this.prometheus.eventsTotal.inc(events.length)
+
             // Loop through the Vec<EventRecord>
             await Promise.all(
                 events.map(async (record) => {
                     // Extract the phase, event and the event types
                     const { event } = record
-                    logger.debug(event.toHuman())
+                    logger.debug({'event': event}, "Event data")
 
                     if (event.section == 'attesters') {
-                        this.prometheus.eventsAttestationsTotal.inc().set({
+                        this.prometheus.eventsAttestationsTotal.inc({
                             method: event.method,
                         })
+
                         logger.info(
                             {
                                 section: event.section,
@@ -73,63 +77,27 @@ export class Attester {
 
                         switch (event.method) {
                             case 'NewAttestationMessageHash': {
-                                const [targetId, messageHash, executionVendor] =
-                                    event.data
+                                const [targetId, messageHash, executionVendor] = event.data
                                 logger.info(
                                     {
                                         targetId: targetId.toString(),
                                         messageHash: messageHash.toHex(),
-                                        executionVendor:
-                                            executionVendor.toString(),
+                                        executionVendor: executionVendor.toString(),
                                     },
                                     `Received the attestation message hash request to sign`
                                 )
                                 // Submit the attestation for the given target ID for the given message hash for each attester's key in the keys.json file
                                 if (executionVendor.toString() == 'Substrate') {
-                                    logger.warn('Substrate unhandled yet')
+                                    logger.warn('Substrate not implemented')
                                 } else if (
                                     executionVendor.toString() == 'Ed25519'
                                 ) {
-                                    logger.warn('Ed25519 unhandled yet')
+                                    logger.warn('Ed25519 not implemented')
                                 } else if (
                                     executionVendor.toString() == 'EVM'
                                 ) {
                                     // Generate the signature for the message hash
-                                    const privateKey = Buffer.from(
-                                        hexToU8a(
-                                            this.config.keys.ethereum.privateKey
-                                        )
-                                    )
-
-                                    const sigObj = ethUtil.ecsign(
-                                        Buffer.from(
-                                            hexToU8a(messageHash.toHex())
-                                        ),
-                                        privateKey
-                                    )
-                                    const signature = ethUtil.toRpcSig(
-                                        sigObj.v,
-                                        sigObj.r,
-                                        sigObj.s
-                                    )
-
-                                    const tx =
-                                        this.circuit.client.tx.attesters.submitAttestation(
-                                            messageHash,
-                                            signature,
-                                            targetId
-                                        )
-
-                                    logger.info(
-                                        {
-                                            executionVendor:
-                                                executionVendor.toString(),
-                                            targetId: targetId.toString(),
-                                            messageHash: messageHash.toHex(),
-                                            hash: tx.hash.toHex(),
-                                        },
-                                        'Executed'
-                                    )
+                                    await this.submitAttestationEVM(messageHash, targetId, executionVendor)
                                 }
 
                                 break
@@ -148,5 +116,64 @@ export class Attester {
                 })
             )
         })
+    }
+
+    private async submitAttestationEVM(messageHash: any, targetId: any, executionVendor: any) {
+        const privateKey = Buffer.from(
+            hexToU8a(
+                this.keys.ethereum.privateKey
+            )
+        )
+
+        const sigObj = ethUtil.ecsign(
+            Buffer.from(
+                hexToU8a(messageHash)
+            ),
+            privateKey
+        )
+
+        const signature = ethUtil.toRpcSig(
+            sigObj.v,
+            sigObj.r,
+            sigObj.s
+        )
+
+        logger.info(
+            {
+                executionVendor: executionVendor.toString(),
+                targetId: targetId.toString(),
+                messageHash: messageHash,
+                signature: signature.toString(),
+            },
+            'Submitting attestation'
+        )
+        const tx = this.circuit.client.tx.attesters.submitAttestation(
+            messageHash,
+            signature,
+            targetId
+        )
+
+        const result = await this.submitTx(tx, this.circuit)
+        logger.info(
+            {
+                executionVendor: executionVendor.toString(),
+                targetId: targetId.toString(),
+                messageHash: messageHash,
+                // hash: result?.hash.toHex(),
+            },
+            'Attestation submitted'
+        )
+    }
+
+    async submitTx(tx: any, circuit: any) {
+        if (circuit.sdk && circuit.isActive) {
+            try {
+                return await circuit.sdk.circuit.tx.signAndSendSafe(tx)
+            }
+            catch (error) {
+                logger.error(error)
+            }
+        }
+
     }
 }
