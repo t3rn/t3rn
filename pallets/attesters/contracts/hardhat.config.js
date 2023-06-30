89643550e9afe5d68844f1053cc21a84b3692e32
marcin <6998565+3h4x@users.noreply.github.com>
2023-06-30 10:21:05 +0200

    chore(hardhat): update hardhat config file

--- pallets/attesters/contracts/hardhat.config.js
+++ pallets/attesters/contracts/hardhat.config.js
@@ -8,12 +8,12 @@ module.exports = {
     ganache: {
       url: "http://127.0.0.1:7545",
       chainId: 1337, 
-      accounts: ["0x6bfa80bcf02789b719e4b47c26063b1caec900aebc0d09034d3cf2e4f6bfad98"], 
+      accounts: ["0x6bfa80bcf02789b719e4b47c26063b1caec900aebc0d09034d3cf2e4f6bfad98"], // default ganache-cli mnemonic
     },
     sepolia: {
       url: "https://eth-sepolia.public.blastapi.io", 
       chainId: 11155111, 
-      accounts: ["0x026725ded690042c65163d263432d79b293a8a10108c7e7a32ff185f1926fb61"], 
+      accounts: [process.env.ETHEREUM_PRIVATE_KEY], 
     },
   },
 
