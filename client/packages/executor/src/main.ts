e7db096d84329f3f3a0e479eb4fb4fcda1299aae
marcin <6998565+3h4x@users.noreply.github.com>
2023-07-16 10:42:31 +0200

    fix: add ethereum secret to helm

--- client/packages/executor/src/main.ts
+++ client/packages/executor/src/main.ts
@@ -1,3 +1,4 @@
+import { config } from "../config/config";
 import { AttestationManager } from "./attestationManager";
 import { Instance } from "./index";
 import { logger } from "./logging";
@@ -7,9 +8,15 @@ async function main() {
   const instance = new Instance(process.env.EXECUTOR);
   await instance.setup();
 
-  const attestationManager = new AttestationManager(instance.circuitClient);
-  logger.info("Processing Attestation Batches");
-  await attestationManager.processAttestationBatches();
+  if (config.attestations.ethereum.privateKey === undefined) {
+    logger.error("Ethereum private key is not defined.");
+  } else {
+    const attestationManager = new AttestationManager(instance.circuitClient);
+    if (config.attestations.processBatches) {
+      logger.info("Processing Attestation Batches");
+      await attestationManager.processAttestationBatches();
+    }
+  }
 }
 
 main();
