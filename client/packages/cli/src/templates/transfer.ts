e03c92cf5d0b1c6432189b94bf856f61b081156e
marcin <6998565+3h4x@users.noreply.github.com>
2023-07-21 11:09:59 +0200

    feat(cli): parametrize CLI with env variables (#1196)
    * feat: parametrize tran template
    
    * fix: use ws connection from config
    
    * fix: parametrize cli

--- client/packages/cli/src/templates/transfer.ts
+++ client/packages/cli/src/templates/transfer.ts
@@ -1,20 +1,28 @@
+const SFX_TRAN_TO = process.env.SFX_TRAN_TO || "5Hmf2ARKQWr2RXLYUuZRN2HzEoDLVUGquhwLN8J7nsRMYcGQ"
+const SFX_TRAN_TARGET = process.env.SFX_TRAN_TARGET || "roco"
+const SFX_TRAN_AMOUNT = Number(process.env.SFX_TRAN_AMOUNT) || 1
+const SFX_TRAN_INSURANCE = process.env.SFX_TRAN_INSURANCE || "0.1"
+const SFX_TRAN_MAX_REWARD = process.env.SFX_TRAN_MAX_REWARD || "40"
+const SFX_TRAN_SIGNATURE = process.env.SFX_TRAN_SIGNATURE || "0x"
+const SFX_TRAN_SPEED_MODE = process.env.SFX_TRAN_SPEED_MODE || "Fast"
+
 export default {
   sideEffects: [
     {
-      target: "roco",
-      maxReward: "40",
-      insurance: "0.1",
+      target: SFX_TRAN_TARGET,
+      maxReward: SFX_TRAN_MAX_REWARD,
+      insurance: SFX_TRAN_INSURANCE,
       action: "tran",
       encodedArgs: [
         {
-          to: "5Hmf2ARKQWr2RXLYUuZRN2HzEoDLVUGquhwLN8J7nsRMYcGQ",
-          amount: 1,
+          to: SFX_TRAN_TO,
+          amount: SFX_TRAN_AMOUNT,
         },
       ],
-      signature: "0x",
+      signature: SFX_TRAN_SIGNATURE,
       enforceExecutor: null,
       rewardAssetId: null, // defaults to TRN
     },
   ],
-  speed_mode: "Fast",
+  speed_mode: SFX_TRAN_SPEED_MODE,
 }
