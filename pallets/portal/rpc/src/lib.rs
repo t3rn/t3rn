93925cd4c847b9c2fbbe4a006b82bd48b04bf771
MaciejBaj <maciej@t3rn.io>
2023-08-02 09:08:28 +0100

    fix: upgrade Portal and Contracts Registry RPCs to v0.9.39

--- pallets/portal/rpc/src/lib.rs
+++ pallets/portal/rpc/src/lib.rs
@@ -23,7 +23,7 @@ const RUNTIME_ERROR: i64 = 1;
 pub trait PortalApi<AccountId> {
     /// Returns latest finalized header of a gateway if available
     #[method(name = "portal_fetchHeadHeight")]
-    fn fetch_head_height(&self, chain_id: ChainId) -> RpcResult<Vec<u8>>;
+    fn fetch_head_height(&self, chain_id: ChainId) -> RpcResult<u128>;
 }
 
 /// A struct that implements the [`PortalApi`].
@@ -50,12 +50,12 @@ where
     C::Api: PortalRuntimeApi<Block, AccountId>,
 {
     // ToDo ChainId decoding is not working, like in XDNS
-    fn fetch_head_height(&self, gateway_id: ChainId) -> RpcResult<u128> {
+    fn fetch_head_height(&self, chain_id: ChainId) -> RpcResult<u128> {
         let api = self.client.runtime_api();
         let at = self.client.info().best_hash;
 
         let result: Option<u128> = api
-            .fetch_head_height(&at, chain_id)
+            .fetch_head_height(at, chain_id)
             .map_err(runtime_error_into_rpc_err)?;
 
         match result {
