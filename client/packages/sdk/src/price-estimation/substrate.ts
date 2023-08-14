46d58e4bb3504bdcefb776a0b1da5bc5dcb3446f
Victor Aremu <Victor.olorunbunmi@gmail.com>
2023-08-14 09:48:43 +0100

    feat: implement substrate price estimation (#1234)
    * feat: implement substrate price estimation
    
    * fix: prevent typedocs from including test files
    
    * feat: implement NativeAssetMap

---
+++ client/packages/sdk/src/price-estimation/substrate.ts
@@ -0,0 +1,12 @@
+import { SubmittableExtrinsic } from "@polkadot/api/promise/types";
+import { AddressOrPair } from "@polkadot/api/types";
+
+export interface EstimateSubmittableExtrinsicParams {
+  tx: SubmittableExtrinsic
+  account: AddressOrPair
+}
+
+export const estimateSfxGasFee = async ({ tx, account }: EstimateSubmittableExtrinsicParams) => {
+  const paymentInfo = await tx.paymentInfo(account);
+  return paymentInfo.partialFee.toNumber();
+}
