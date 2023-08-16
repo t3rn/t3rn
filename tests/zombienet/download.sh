350ddb44224ac290c4210b429dc2f3dbae48a9d3
marcin <6998565+3h4x@users.noreply.github.com>
2023-08-16 17:03:17 +0200

    fix: prepare correct wasm

--- tests/zombienet/download.sh
+++ tests/zombienet/download.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-version=$(($2+1))
 bin_dir=../../bin
 
 echo "=========== ${1} ==========="
 version=$(git tag --list --sort=-version:refname "v[0-9]*.[0-9]*.[0-9]*-rc.[0-9]*" | head -n 1)
 echo Previous version: "$version"
 
-./download_previous_collator.sh "${1}"
-./prepare_wasm.sh "$version"
+# Prepare collator binary
+./download_collator.sh "${1}"
+# Prepare WASM binary
+cp ../../target/release/wbuild/${1}-parachain-runtime/${1}_parachain_runtime.compact.compressed.wasm ${bin_dir}/parachain_runtime.compact.compressed.wasm
