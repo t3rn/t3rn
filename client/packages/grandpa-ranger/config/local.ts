a820382b9e152f248d43006781dd94a3bc52ad15
marcin <6998565+3h4x@users.noreply.github.com>
2023-08-28 13:28:49 +0200

    fix: bring back yarn lock

--- client/packages/grandpa-ranger/config/local.ts
+++ client/packages/grandpa-ranger/config/local.ts
@@ -19,5 +19,4 @@ export default {
 	},
 	rangeInterval: 10, // time between range submissions in seconds
 	targetGatewayId: "roco",
-	circuitSigner: "0x0177d124e501887c2470e260c8f0da60db9ed3dba808a682f09afb39eff0c561"
 }
