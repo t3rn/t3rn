4876db12bf4d8190a7cd286a40a38c6e4f126bb3
Maciej Baj <6909553+MaciejBaj@users.noreply.github.com>
2023-09-06 10:32:28 +0200

    chore: reinstantiate standalone RPC for Portal + XDNS (#1282)
    * chore: reinstantiate standalone RPC for Portal + XDNS
    
    * chore: add missing Portal + XDNS runtime api to standalone

--- runtime/standalone/src/impl_versioned_runtime_with_api.rs
+++ runtime/standalone/src/impl_versioned_runtime_with_api.rs
@@ -14,11 +14,11 @@ pub use frame_support::{
     },
     StorageValue,
 };
-
 pub use pallet_balances::Call as BalancesCall;
 use pallet_grandpa::AuthorityId as GrandpaId;
 pub use pallet_timestamp::Call as TimestampCall;
 
+use pallet_circuit::ChainId;
 use sp_api::impl_runtime_apis;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::{crypto::KeyTypeId, OpaqueMetadata};
@@ -30,8 +30,14 @@ use sp_runtime::{
     ApplyExtrinsicResult,
 };
 pub use sp_runtime::{Perbill, Permill};
+use t3rn_primitives::{
+    portal::HeightResult,
+    xdns::{FullGatewayRecord, GatewayRecord},
+    TreasuryAccountProvider,
+};
 
 pub use crate::consensus_aura_config::*;
+use pallet_xdns_rpc_runtime_api::GatewayABIConfig;
 
 use sp_runtime::create_runtime_str;
 
@@ -290,6 +296,36 @@ impl_runtime_apis! {
             add_benchmarks!(params, batches);
 
             Ok(batches)
+        }
+    }
+
+     impl pallet_xdns_rpc_runtime_api::XdnsRuntimeApi<Block, AccountId> for Runtime {
+        fn fetch_records() -> Vec<GatewayRecord<AccountId>> {
+             <XDNS as t3rn_primitives::xdns::Xdns<Runtime, Balance>>::fetch_gateways()
+        }
+
+        fn fetch_full_gateway_records() -> Vec<FullGatewayRecord<AccountId>> {
+             <XDNS as t3rn_primitives::xdns::Xdns<Runtime, Balance>>::fetch_full_gateway_records()
+        }
+
+        fn fetch_abi(_chain_id: ChainId) -> Option<GatewayABIConfig> {
+            // deprecated
+            None
+        }
+
+        fn retreive_treasury_address(treasury_account: t3rn_primitives::TreasuryAccount) -> AccountId {
+            Runtime::get_treasury_account(treasury_account)
+        }
+    }
+
+     impl pallet_portal_rpc_runtime_api::PortalRuntimeApi<Block, AccountId> for Runtime {
+        fn fetch_head_height(chain_id: ChainId) -> Option<u128> {
+            let res = <Portal as t3rn_primitives::portal::Portal<Runtime>>::get_fast_height(chain_id);
+
+            match res {
+                Ok(HeightResult::Height(height)) => Some(height.into()),
+                _ => None,
+            }
         }
     }
 
