8f586545929355aca19413156388cf041e8e4673
marcin <6998565+3h4x@users.noreply.github.com>
2023-09-07 09:11:50 +0200

    fix: remove config generation

--- client/packages/cli/src/commands/init.ts
+++ client/packages/cli/src/commands/init.ts
@@ -16,27 +16,8 @@ export const handleInitCmd = async (args: Args<"config" | "transfer">) => {
 
   console.log(greet())
 
-  if (args.config) {
-    initConfigFile(args.config)
-  }
-
   if (args.transfer) {
     initTransferFile(args.transfer)
-  }
-}
-
-export const initConfigFile = (filePath: string | true) => {
-  const template = JSON.stringify(setupTemplate, null, 2)
-
-  try {
-    const file = filePath === true ? path.join("./", CONFIG_FILE) : filePath
-    writeFileSync(file, template)
-    log("SUCCESS", `@t3rn/cli config initialized at ${file}`)
-  } catch (e) {
-    log(
-      "ERROR",
-      `Unable to initialize t3rn CLI config. Reason: ${cleanStack(e.stack)}`,
-    )
   }
 }
 
