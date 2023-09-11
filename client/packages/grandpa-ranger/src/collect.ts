2a864f5be64aaf8321865fcfae63a0a181863b34
marcin <6998565+3h4x@users.noreply.github.com>
2023-09-11 18:55:55 +0200

    chore: tabs to spaces

--- client/packages/grandpa-ranger/src/collect.ts
+++ client/packages/grandpa-ranger/src/collect.ts
@@ -1,104 +1,146 @@
-import {Connection} from "./connection";
-import {ApiPromise, Encodings} from "@t3rn/sdk";
-const axios = require('axios').default;
-import {Prometheus} from "./prometheus";
-import { logger } from "./logging";
+import { Connection } from "./connection"
+import { ApiPromise, Encodings } from "@t3rn/sdk"
+const axios = require("axios").default
+import { Prometheus } from "./prometheus"
+import { logger } from "./logging"
 
-export const generateRange = async (config: any, circuitConnection: Connection, targetConnection: Connection, prometheus: Prometheus, target: string): Promise<any[]> => {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const circuitHeight = await currentGatewayHeight(circuitConnection, config.targetGatewayId)
-			let targetHeight = await currentTargetHeight(targetConnection)
+export const generateRange = async (
+  config: any,
+  circuitConnection: Connection,
+  targetConnection: Connection,
+  prometheus: Prometheus,
+  target: string
+): Promise<any[]> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const circuitHeight = await currentGatewayHeight(
+        circuitConnection,
+        config.targetGatewayId
+      )
+      let targetHeight = await currentTargetHeight(targetConnection)
 
-			logger.debug(
-				{
-					circuitHeight,
-					targetHeight
-				},
-				"Current heights"
-			)
+      logger.debug(
+        {
+          circuitHeight,
+          targetHeight,
+        },
+        "Current heights"
+      )
 
-			prometheus.circuitHeight.set({target}, circuitHeight)
+      prometheus.circuitHeight.set({ target }, circuitHeight)
 
-			if(targetHeight > circuitHeight) {
-				let batches = await generateBatchProof(circuitConnection.client, targetConnection.client, config.targetGatewayId, circuitHeight + 1, targetHeight)
-				return resolve(batches)
-			} else {
-				throw new Error("No new blocks to submit")
-
-			}
-
-		} catch(error) {
-			return reject(error)
-		}
-	})
+      if (targetHeight > circuitHeight) {
+        let batches = await generateBatchProof(
+          circuitConnection.client,
+          targetConnection.client,
+          config.targetGatewayId,
+          circuitHeight + 1,
+          targetHeight
+        )
+        return resolve(batches)
+      } else {
+        throw new Error("No new blocks to submit")
+      }
+    } catch (error) {
+      return reject(error)
+    }
+  })
 }
 
-const generateBatchProof = async (circuitClient: ApiPromise, targetClient: ApiPromise, targetGatewayId: string, from: number, to: number): Promise<any[]> => {
-	let transactionArguments: any[] = [];
+const generateBatchProof = async (
+  circuitClient: ApiPromise,
+  targetClient: ApiPromise,
+  targetGatewayId: string,
+  from: number,
+  to: number
+): Promise<any[]> => {
+  let transactionArguments: any[] = []
 
-	while(from < to) {
-		// get finalityProof element of epoch that contains block #from
-		const finalityProof = await targetClient.rpc.grandpa.proveFinality(from)
-		// decode finality proof
-		let { justification, headers } = Encodings.Substrate.Decoders.finalityProofDecode(finalityProof)
+  while (from < to) {
+    // get finalityProof element of epoch that contains block #from
+    const finalityProof = await targetClient.rpc.grandpa.proveFinality(from)
+    // decode finality proof
+    let { justification, headers } =
+      Encodings.Substrate.Decoders.finalityProofDecode(finalityProof)
 
-		const justificationSize = Math.floor(Buffer.from(JSON.stringify(justification)).length / 1024)
-		const headersSize = Math.floor(Buffer.from(JSON.stringify(headers)).length / 1024)
-		logger.debug("Fetched finality proof from target chain")
-		logger.debug(`Justification size: ${justificationSize}kb`);
-		logger.debug(`Headers size: ${headersSize}kb`)
+    const justificationSize = Math.floor(
+      Buffer.from(JSON.stringify(justification)).length / 1024
+    )
+    const headersSize = Math.floor(
+      Buffer.from(JSON.stringify(headers)).length / 1024
+    )
+    logger.debug("Fetched finality proof from target chain")
+    logger.debug(`Justification size: ${justificationSize}kb`)
+    logger.debug(`Headers size: ${headersSize}kb`)
 
-		let signed_header;
-		if(headers.length == 0) { // Only one block in epoch missing
-			signed_header = await getHeader(targetClient, from)
-			from = parseInt(signed_header.number) + 1
-		} else {
-			signed_header = headers.pop()
-			headers = [await getHeader(targetClient, from), ...headers]
-			from = parseInt(signed_header.number.toJSON()) + 1
-		}
+    let signed_header
+    if (headers.length == 0) {
+      // Only one block in epoch missing
+      signed_header = await getHeader(targetClient, from)
+      from = parseInt(signed_header.number) + 1
+    } else {
+      signed_header = headers.pop()
+      headers = [await getHeader(targetClient, from), ...headers]
+      from = parseInt(signed_header.number.toJSON()) + 1
+    }
 
-		let range = circuitClient.createType("Vec<Header>", headers)
-		justification = Encodings.Substrate.Decoders.justificationDecode(justification);
+    let range = circuitClient.createType("Vec<Header>", headers)
+    justification =
+      Encodings.Substrate.Decoders.justificationDecode(justification)
 
-		//push to transaction queue
-		transactionArguments.push({gatewayId: circuitClient.createType("ChainId", targetGatewayId), signed_header, range, justification})
-	}
-	return transactionArguments;
-
+    //push to transaction queue
+    transactionArguments.push({
+      gatewayId: circuitClient.createType("ChainId", targetGatewayId),
+      signed_header,
+      range,
+      justification,
+    })
+  }
+  return transactionArguments
 }
 
 const currentTargetHeight = async (connection: Connection): Promise<number> => {
-	const header = await connection.client.rpc.chain.getHeader(
-		await connection.client.rpc.chain.getFinalizedHead()
-	);
-	return header.number.toNumber();
+  const header = await connection.client.rpc.chain.getHeader(
+    await connection.client.rpc.chain.getFinalizedHead()
+  )
+  return header.number.toNumber()
 }
 
-const currentGatewayHeight = async (client: Connection, targetGatewayId: string)=> {
-	// client.rpc.portal.
-	return axios.post(client.currentProvider().http, {
-		jsonrpc: '2.0',
-		method: 'portal_fetchHeadHeight',
-		params: [Array.from(new TextEncoder().encode(targetGatewayId))],
-		id: 1
-	}, {
-		headers: {
-		'Content-Type': 'application/json'
-		}
-	})
-	.then(response => {
-		if (response.data.error) throw new Error(response.data.error.message)
-	  	return response.data.result;
-	})
-	.catch(error => {
-		throw new Error(`Gateway height couldnt be fetched! Err: ${error.toString()}`)
-	})
+const currentGatewayHeight = async (
+  client: Connection,
+  targetGatewayId: string
+) => {
+  // client.rpc.portal.
+  return axios
+    .post(
+      client.currentProvider().http,
+      {
+        jsonrpc: "2.0",
+        method: "portal_fetchHeadHeight",
+        params: [Array.from(new TextEncoder().encode(targetGatewayId))],
+        id: 1,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .then(response => {
+      if (response.data.error) throw new Error(response.data.error.message)
+      return response.data.result
+    })
+    .catch(error => {
+      throw new Error(
+        `Gateway height couldnt be fetched! Err: ${error.toString()}`
+      )
+    })
 }
 
 const getHeader = async (client: ApiPromise, height: number) => {
-    return (await client.rpc.chain.getHeader(
-        await client.rpc.chain.getBlockHash(height)
-    )).toJSON()
+  return (
+    await client.rpc.chain.getHeader(
+      await client.rpc.chain.getBlockHash(height)
+    )
+  ).toJSON()
 }
--- client/packages/grandpa-ranger/src/connection.ts
+++ client/packages/grandpa-ranger/src/connection.ts
@@ -1,113 +1,134 @@
-import { Sdk, ApiPromise, WsProvider, Keyring } from '@t3rn/sdk';
-import { Prometheus } from "./prometheus";
-import { logger } from './logging';
+import { Sdk, ApiPromise, WsProvider, Keyring } from "@t3rn/sdk"
+import { Prometheus } from "./prometheus"
+import { logger } from "./logging"
 
 export class Connection {
-	client: ApiPromise;
-	provider: WsProvider;
-	rpc1: any;
-	usingPrimaryRpc: boolean = true;
-	rpc2: any;
-	isCircuit: boolean;
-	isActive: boolean = false;
-	sdk: Sdk | undefined;
-	signer: any;
-	prometheus: Prometheus;
-	target: string;
+  client: ApiPromise
+  provider: WsProvider
+  rpc1: any
+  usingPrimaryRpc: boolean = true
+  rpc2: any
+  isCircuit: boolean
+  isActive: boolean = false
+  sdk: Sdk | undefined
+  signer: any
+  prometheus: Prometheus
+  target: string
 
-	constructor(rpc1: any, rpc2: any, isCircuit: boolean, prometheus: Prometheus, target: string) {
-		this.rpc1 = rpc1;
-		this.rpc2 = rpc2;
-		this.usingPrimaryRpc = true;
-		this.isCircuit = isCircuit;
-		this.prometheus = prometheus;
-		this.target = target;
-		const keyring = new Keyring({ type: 'sr25519' });
-		if (isCircuit) {
-			this.signer =
-				process.env.CIRCUIT_SIGNER_KEY === undefined
-				? keyring.addFromUri("//Alice")
-				: keyring.addFromMnemonic(process.env.CIRCUIT_SIGNER_KEY)
-			logger.info(`Using signer ${this.signer.address} for ${this.rpc1.ws}`)
-		}
-	}
+  constructor(
+    rpc1: any,
+    rpc2: any,
+    isCircuit: boolean,
+    prometheus: Prometheus,
+    target: string
+  ) {
+    this.rpc1 = rpc1
+    this.rpc2 = rpc2
+    this.usingPrimaryRpc = true
+    this.isCircuit = isCircuit
+    this.prometheus = prometheus
+    this.target = target
+    const keyring = new Keyring({ type: "sr25519" })
+    if (isCircuit) {
+      this.signer =
+        process.env.CIRCUIT_SIGNER_KEY === undefined
+          ? keyring.addFromUri("//Alice")
+          : keyring.addFromMnemonic(process.env.CIRCUIT_SIGNER_KEY)
+      logger.info(`Using signer ${this.signer.address} for ${this.rpc1.ws}`)
+    }
+  }
 
-	async connect() {
-		while(true) {
-			try {
-				this.provider = this.createProvider();
-				await this.setListeners();
-				break;
-			} catch(e) {
-				const endpoint = this.usingPrimaryRpc ? this.rpc1.ws : this.rpc2.ws
-				this.isCircuit ?
-					this.prometheus.circuitDisconnected.inc({endpoint, target: this.target}) :
-					this.prometheus.targetDisconnected.inc({endpoint, target: this.target});
+  async connect() {
+    while (true) {
+      try {
+        this.provider = this.createProvider()
+        await this.setListeners()
+        break
+      } catch (e) {
+        const endpoint = this.usingPrimaryRpc ? this.rpc1.ws : this.rpc2.ws
+        this.isCircuit
+          ? this.prometheus.circuitDisconnected.inc({
+              endpoint,
+              target: this.target,
+            })
+          : this.prometheus.targetDisconnected.inc({
+              endpoint,
+              target: this.target,
+            })
 
-				this.isCircuit ?
-					this.prometheus.circuitDisconnectsTotal.inc({target: this.target}, 1) :
-					this.prometheus.targetDisconnectsTotal.inc({target: this.target}, 1);
+        this.isCircuit
+          ? this.prometheus.circuitDisconnectsTotal.inc(
+              { target: this.target },
+              1
+            )
+          : this.prometheus.targetDisconnectsTotal.inc(
+              { target: this.target },
+              1
+            )
 
-				this.usingPrimaryRpc = !this.usingPrimaryRpc; // toggle connection
-				logger.warn(`Retrying in 2 second with ${this.currentProvider().ws}`)
-				await new Promise((resolve, _reject) => setTimeout(resolve, 2000));
-			}
-		}
-	}
+        this.usingPrimaryRpc = !this.usingPrimaryRpc // toggle connection
+        logger.warn(`Retrying in 2 second with ${this.currentProvider().ws}`)
+        await new Promise((resolve, _reject) => setTimeout(resolve, 2000))
+      }
+    }
+  }
 
-	async setListeners() {
-		return new Promise((resolve, reject) => {
-			this.provider.on('connected', async () => {
-				this.isActive = true;
-				logger.info(`Connected to ${this.currentProvider().ws}`)
-				if(this.isCircuit) {
-					this.prometheus.circuitActive = true;
-					const sdk = new Sdk(this.provider, this.signer, true);
-					this.sdk = sdk;
-					this.client = await sdk.init();
-				} else {
-					this.prometheus.targetActive = true;
-					this.client = await ApiPromise.create({
-						provider: this.provider
-					})
+  async setListeners() {
+    return new Promise((resolve, reject) => {
+      this.provider.on("connected", async () => {
+        this.isActive = true
+        logger.info(`Connected to ${this.currentProvider().ws}`)
+        if (this.isCircuit) {
+          this.prometheus.circuitActive = true
+          const sdk = new Sdk(this.provider, this.signer, true)
+          this.sdk = sdk
+          this.client = await sdk.init()
+        } else {
+          this.prometheus.targetActive = true
+          this.client = await ApiPromise.create({
+            provider: this.provider,
+          })
 
-					// update prometheus metrics with incoming blocks
-					this.client.derive.chain.subscribeNewHeads(header => {
-						this.prometheus.targetHeight.set(header.number.toNumber());
-					})
-				}
-			})
+          // update prometheus metrics with incoming blocks
+          this.client.derive.chain.subscribeNewHeads(header => {
+            this.prometheus.targetHeight.set(header.number.toNumber())
+          })
+        }
+      })
 
-			this.provider.on('disconnected', () => {
-				this.isActive = false;
-				this.isCircuit ? this.prometheus.circuitActive = false : this.prometheus.targetActive = false;
-				logger.warn(`Disconnected from ${this.currentProvider().ws}`)
-				this.provider.disconnect()
-				if(this.client) {
-					this.client.disconnect()
-				}
-				reject()
-			})
+      this.provider.on("disconnected", () => {
+        this.isActive = false
+        this.isCircuit
+          ? (this.prometheus.circuitActive = false)
+          : (this.prometheus.targetActive = false)
+        logger.warn(`Disconnected from ${this.currentProvider().ws}`)
+        this.provider.disconnect()
+        if (this.client) {
+          this.client.disconnect()
+        }
+        reject()
+      })
 
-			this.provider.on('error',  () => {
-				this.isActive = false;
-				this.isCircuit ? this.prometheus.circuitActive = false : this.prometheus.targetActive = false;
-				logger.warn(`Error from ${this.currentProvider().ws}`)
-				this.provider.disconnect()
-				if(this.client) {
-					this.client.disconnect()
-				}
-				reject()
-			})
-		})
+      this.provider.on("error", () => {
+        this.isActive = false
+        this.isCircuit
+          ? (this.prometheus.circuitActive = false)
+          : (this.prometheus.targetActive = false)
+        logger.warn(`Error from ${this.currentProvider().ws}`)
+        this.provider.disconnect()
+        if (this.client) {
+          this.client.disconnect()
+        }
+        reject()
+      })
+    })
+  }
 
-	}
+  currentProvider(): any {
+    return this.usingPrimaryRpc ? this.rpc1 : this.rpc2
+  }
 
-	currentProvider(): any {
-		return this.usingPrimaryRpc ? this.rpc1 : this.rpc2
-	}
-
-	createProvider() {
-		return new WsProvider(this.usingPrimaryRpc ? this.rpc1.ws : this.rpc2.ws)
-	}
+  createProvider() {
+    return new WsProvider(this.usingPrimaryRpc ? this.rpc1.ws : this.rpc2.ws)
+  }
 }
