11bb5619a57bde4de404c6faedbe2b1eab231f94
t3rn-ci <89840377+t3rn-ci@users.noreply.github.com>
2023-09-26 15:36:10 +0000

    build(release): release 1.56.1-rc.0

--- runtime/t0rn-parachain/src/lib.rs
+++ runtime/t0rn-parachain/src/lib.rs
@@ -78,11 +78,11 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // https://docs.rs/sp-version/latest/sp_version/struct.RuntimeVersion.html
     spec_name: create_runtime_str!("t0rn"),
     impl_name: create_runtime_str!("Circuit Collator"),
-    authoring_version: 150,
-    spec_version: 151,
-    impl_version: 149,
+    authoring_version: 151,
+    spec_version: 152,
+    impl_version: 150,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 150,
+    transaction_version: 151,
     // https://github.com/paritytech/cumulus/issues/998
     // https://github.com/paritytech/substrate/pull/9732
     // https://github.com/paritytech/substrate/pull/10073
