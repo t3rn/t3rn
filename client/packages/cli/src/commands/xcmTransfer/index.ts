0f8f5f2687d5434b33d9df5a91f236682fba6dc6
chexware <aleksandar.brayanov@protonmail.com>
2023-10-10 09:28:18 +0100

    feat: enabled sending XCM transactions using the CLI

--- client/packages/cli/src/commands/xcmTransfer/index.ts
+++ client/packages/cli/src/commands/xcmTransfer/index.ts
@@ -3,8 +3,8 @@ import {Args} from "@/types.js"
 import {validate} from "@/utils/fns.js"
 import {XcmTransferSchema} from "@/schemas/xcm.ts"
 import {colorLogMsg} from "@/utils/log.js"
-import {ApiPromise, WsProvider } from "@t3rn/sdk"
-import { generateXcmTransferParameters } from "@t3rn/sdk/utils"
+import {ApiPromise, WsProvider, Keyring } from "@t3rn/sdk"
+import { generateXcmTransferParameters, createAssets, createBeneficiary, createDestination } from "@/utils/xcm.ts"//"@t3rn/sdk/utils"
 
 export const spinner = ora()
 
@@ -45,12 +45,43 @@ export const handleXcmTransferCommand = async (
             "ROC",
             "para"
         )
-        console.log("\n")
-        console.table(JSON.stringify(xcmTransactionParams))
+        const xcmDestParam = createDestination(targetApi, args.dest, "0")
+        const xcmBeneficiaryParam = createBeneficiary(targetApi, args.recipient)
+        const xcmAssetsParam = createAssets(targetApi, "ROC", "0", args.targetAmount)
+        const xcmAssetFeeItem = targetApi.registry.createType("u32", 0)
+        console.log("Sending XCM Transfer:\n")
+        const keyring = new Keyring({ type: "sr25519" })
+        const signer = keyring.addFromUri("//Alice")
+        await targetApi.tx.xcmPallet
+            .reserveTransferAssets(xcmDestParam,xcmBeneficiaryParam, xcmAssetsParam, xcmAssetFeeItem)
+            .signAndSend(signer, ({ status, events }) => {
+                if (status.isInBlock || status.isFinalized) {
+                    events
+                        // find/filter for failed events
+                        .filter(({ event }) =>
+                            api.events.system.ExtrinsicFailed.is(event)
+                        )
+                        // we know that data for system.ExtrinsicFailed is
+                        // (DispatchError, DispatchInfo)
+                        .forEach(({ event: { data: [error, info] } }) => {
+                            if (error.isModule) {
+                                // for module errors, we have the section indexed, lookup
+                                const decoded = api.registry.findMetaError(error.asModule);
+                                const { docs, method, section } = decoded;
+
+                                console.log(`${section}.${method}: ${docs.join(' ')}`);
+                            } else {
+                                // Other, CannotLookup, BadOrigin, no extra info
+                                console.log(error.toString());
+                            }
+                        });
+                }
+            })
+
+        spinner.stop()
+        process.exit(0)
 
     } catch (e) {
         spinner.fail(colorLogMsg("ERROR", e))
     }
-
-    spinner.stop()
 }
