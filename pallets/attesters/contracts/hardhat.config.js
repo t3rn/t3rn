a7c1e97e044b0adad7b61b310381b6a3f141b3ae
MaciejBaj <maciej@t3rn.io>
2023-10-12 14:48:48 +0100

    refactor: add XOrders on top of Remote and fix sending to vault problem

--- pallets/attesters/contracts/hardhat.config.js
+++ pallets/attesters/contracts/hardhat.config.js
@@ -9,7 +9,7 @@ if (process.env.ETHEREUM_PRIVATE_KEY != null) {
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.20",
 
   networks: {
     ganache: {
--- pallets/attesters/contracts/scripts/callRemoteOrder.js
+++ pallets/attesters/contracts/scripts/callRemoteOrder.js
@@ -4,17 +4,23 @@ const { ethers } = require('hardhat');
 async function main() {
 
     const ContractGMP = await ethers.getContractFactory('EscrowGMP');
-    const contractGMP = ContractGMP.attach("0x9f345c72Ae8BeFEA11d96FbF0D8920a2A71dF370");
+    const contractGMP = ContractGMP.attach("0x3f2a7B4101b49C40dF12b92e21eeF2D0a4a5B867");
     const storePayloadCall = await contractGMP.storeRemoteOrderPayload("0x0909090909090909090909090909090909090909090909090909090909090909", "0x0909090909090909090909090909090909090909090909090909090909090909");
     const storePayloadReceipt = await storePayloadCall.wait();
     console.log('Call ContractGMP.storeRemoteOrderPayload result:', storePayloadReceipt.cumulativeGasUsed.toString());
-    const ContractXOrder = await ethers.getContractFactory('RemoteOrder');
-    const contract = ContractXOrder.attach("0x4e8540F5a94CCcDFBd1a1290F68c1BFbD3EA04E3");
-    const contractxOrderRes = await contract.remoteOrder("0x03030303050505050000000000000000000000000000000000000000000000000000000009090909090909090909090909090909090909090909090909090909090909090000000000000000000000000000000000000000000000000000000000000064000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000640000000000000000000000000000000000000000000000000000000000000064", { value: ethers.utils.parseUnits('100','wei') })
+    const ContractXOrder = await ethers.getContractFactory('XOrder');
+    const contract = ContractXOrder.attach("0x34705639686234A9EA46Aadb7ed3BAA0C5df9eF4");
+    const contractxOrderRes = await contract.remoteOrder("0x0303030300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006f39c4f2ec2bcf1cfb2c7b74f0c81c075b684dea367314850bcc5dc03cfcd71b6700000000000000000000000000000000000000000000000000000000000000640000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000006e", { value: ethers.utils.parseUnits('110','wei') })
     const contractxOrderReceipt = await contractxOrderRes.wait();
-    // const contractxOrderRes = await contract.remoteOrderDecoded("0x03030303", "0x05050505", "0x0909090909090909090909090909090909090909090909090909090909090909", 100, "0x0000000000000000000000000000000000000000", 100, 100, { value: ethers.utils.parseUnits('100','wei') });
-    // const contractxOrderRes = await contract.generateId("0x1932978B1Ee86aD07E3e324ACc1641DBf14dbF59", "1")
-    console.log('Call ContractXOrder.remoteOrder receipt:', contractxOrderReceipt);
+    console.log('Call ContractXOrder.remoteOrder receipt -- gas used :', contractxOrderReceipt.cumulativeGasUsed.toString());
+    const contractxAddSupportedBridgeAssets = await contract.addSupportedBridgeAsset("0x0000000000000000000000000000000000000000", 1000); // 1000 is the bridge asset id for ETH on t0rn
+    const contractxAddSupportedBridgeAssetsReceipt = await contractxAddSupportedBridgeAssets.wait();
+    console.log('Call ContractXOrder.addSupportedBridgeAsset receipt -- gas used :', contractxAddSupportedBridgeAssetsReceipt.cumulativeGasUsed.toString());
+
+    // bridge sepl for alice 0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d
+    const contractxBridgeAssets = await contract.remoteBridgeAsset("0x0000000000000000000000000000000000000000", "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d", 100, 100, { value: ethers.utils.parseUnits('100','wei') }); // 1000 is the bridge asset id for ETH on t0rn
+    const contractxBridgeAssetsReceipt = await contractxBridgeAssets.wait();
+    console.log('Call ContractXOrder.remoteBridgeAsset receipt -- gas used :', contractxBridgeAssetsReceipt.cumulativeGasUsed.toString());
 }
 
 main()
