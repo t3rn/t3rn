4110a3c597706e2642dd846da6aaa96ab212d48a
marcin <6998565+3h4x@users.noreply.github.com>
2023-10-16 10:16:04 +0200

    fix: t1rn with 3334 paraid

--- tests/zombienet/zombienet.sh
+++ tests/zombienet/zombienet.sh
@@ -136,7 +136,11 @@ smoke() {
     # TODO[Optimisation]: loop through directory and test all
     # TODO[Optimisation, NotImplemented]: when zombienet can run on a pre-existing network, run it
     echo "::group::Zombienet tests..."
-    time zombienet --provider="$provider" test $working_dir/smoke/0001-is_up.zndsl
+    if [ "$NETWORK" = "t0rn" ]; then
+        time zombienet --provider="$provider" test $working_dir/smoke/0001-is_up.zndsl
+    else
+        time zombienet --provider="$provider" test $working_dir/smoke/0001-is_up_t1rn.zndsl
+    fi
     echo "::endgroup::"
 }
 
