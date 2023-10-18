b7245634835394a444281e350f1ef0a2c30f03f3
marcin <6998565+3h4x@users.noreply.github.com>
2023-10-18 13:28:19 +0200

    feat: add electron files

---
+++ client/packages/executor/src/executor.ts
@@ -0,0 +1,34 @@
+import { config } from "../config/config";
+import { AttestationManager } from "./attestationManager";
+import { Executor } from "./executor.class";
+import { logger } from "./logging";
+import { Prometheus } from "./prometheus";
+import { ipcMain } from 'electron';
+
+async function main() {
+  logger.info("Starting prometheus");
+  const prometheus = new Prometheus();
+
+  logger.info("Starting executor");
+  const instance = new Executor(prometheus);
+  await instance.setup();
+
+  ipcMain.on('get-instance', (event) => {
+    event.returnValue = instance.getInstance();
+  });
+
+  if (config.attestations.ethereum.privateKey === undefined) {
+    logger.warn("Ethereum private key is not defined. Skipping Attestations.");
+  } else {
+    const attestationManager = new AttestationManager(
+      instance.circuitClient,
+      prometheus,
+    );
+    if (config.attestations.processBatches) {
+      logger.info("Processing Attestation Batches");
+      await attestationManager.processAttestationBatches();
+    }
+  }
+}
+
+main();
