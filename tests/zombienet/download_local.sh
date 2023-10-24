0f3590ba442cd433937006454a33aedee678aa54
marcin <6998565+3h4x@users.noreply.github.com>
2023-10-24 10:32:41 +0200

    fix: make scripts more simple

--- tests/zombienet/download_local.sh
+++ tests/zombienet/download_local.sh
@@ -3,26 +3,16 @@ bin_dir=../../bin
 
 case "$1" in
     t0rn*)
-        echo "=========== T0RN ==========="
         new_version=$(git tag --list --sort=-version:refname "v[0-9]*.[0-9]*.[0-9]*-rc.[0-9]*" | head -n 1)
-        echo New version: "$new_version"
-        
-        ./download_previous_collator.sh "t0rn"
-        ../../scripts/build_wasm.sh "t0rn"
-        ../../scripts/update_specs.sh "t0rn"
-        cp ../../target/release/wbuild/t0rn-parachain-runtime/${1}_parachain_runtime.compact.compressed.wasm  ${bin_dir}/parachain_runtime.compact.compressed.wasm
     ;;
     t3rn*)
-        echo "=========== T3RN ==========="
         new_version=$(git tag --list --sort=-version:refname "v[0-9]*.[0-9].[0-9]" | head -n 1)
-        echo Tags: "$tags_list"
-        echo New version: "$new_version"
-        
-        ./download_previous_collator.sh "t3rn"
-        ../../scripts/build_wasm.sh "t3rn"
-        ../../scripts/update_specs.sh "t3rn"
-        cp ../../target/release/wbuild/t0rn-parachain-runtime/${1}_parachain_runtime.compact.compressed.wasm  ${bin_dir}/parachain_runtime.compact.compressed.wasm
     ;;
     *)
-    exit 1;;
 esac
+echo New version: "$new_version"
+
+./download_previous_collator.sh $1
+../../scripts/build_wasm.sh $1
+../../scripts/update_specs.sh $1
+cp ../../target/release/wbuild/${1}-parachain-runtime/${1}_parachain_runtime.compact.compressed.wasm  ${bin_dir}/parachain_runtime.compact.compressed.wasm
