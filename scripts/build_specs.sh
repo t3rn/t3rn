53e0586ff0019d405af99d7b888eb9e5d5fb6d75
marcin <6998565+3h4x@users.noreply.github.com>
2023-10-26 11:01:56 +0200

    fix: update t1rn specs and remove obsoleted wasm binaries (#1400)
    fix: update t1rn specs

--- scripts/build_specs.sh
+++ scripts/build_specs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -e
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
 PARACHAIN_NAME=${1:-t3rn}
@@ -13,11 +13,11 @@ mkdir -p $SPECS_DIR
 
 $COLLATOR_BIN build-spec --chain $RELAYCHAIN_NAME > $SPECS_DIR/${PARACHAIN_NAME}.json
 $COLLATOR_BIN build-spec --chain $RELAYCHAIN_NAME --raw > $SPECS_DIR/${PARACHAIN_NAME}.raw.json
-$COLLATOR_BIN export-genesis-wasm --chain $SPECS_DIR/${PARACHAIN_NAME}.raw.json > $SPECS_DIR/${PARACHAIN_NAME}.wasm
+# We use WASM that is attached to a release so this step is not needed anymore
+# $COLLATOR_BIN export-genesis-wasm --chain $SPECS_DIR/${PARACHAIN_NAME}.raw.json > $SPECS_DIR/${PARACHAIN_NAME}.wasm
 
 rm -rf "${ROOT_DIR}"/target/release/specs
 
-echo "successfully generated artifacts (.json, raw.json, .wasm) for: ${PARACHAIN_NAME} to ${ROOT_DIR}/target/release/specs"
-echo "$PARACHAIN_NAME.wasm: $(head -c 64 "${ROOT_DIR}"/specs/"${PARACHAIN_NAME}".wasm)"
+echo "successfully generated artifacts (.json, raw.json) for: ${PARACHAIN_NAME} to ${ROOT_DIR}/target/release/specs"
 echo "$PARACHAIN_NAME.json: $(head -c 21 "${ROOT_DIR}"/specs/"${PARACHAIN_NAME}".json)"
 echo "$PARACHAIN_NAME.raw.json: $(head -c 21 "${ROOT_DIR}"/specs/"${PARACHAIN_NAME}".raw.json)"
