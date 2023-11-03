afcd6d183a076910f20fb08d03098a89388fbd65
marcin <6998565+3h4x@users.noreply.github.com>
2023-11-03 11:20:22 +0100

    fix: use SfxType from SDK

--- client/packages/sdk/src/types.ts
+++ client/packages/sdk/src/types.ts
@@ -1,4 +1,5 @@
-import { SfxType, SfxStatus, XtxStatus } from "./side-effects/types"
+import { SfxStatus, XtxStatus } from "./side-effects/types"
+import { SfxType } from "@t3rn/sdk"
 
 /**
  * SideEffect types namespace
