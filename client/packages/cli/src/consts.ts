b152e2f84b9c46f5c0551d54e10068f24e67f36d
Galin Kostov <galin@t3rn.io>
2023-11-07 12:17:46 -0500

    chore: change execution endpoint url from eth to sepolia

--- client/packages/cli/src/consts.ts
+++ client/packages/cli/src/consts.ts
@@ -6,5 +6,5 @@ export const ETHEREUM_EPOCHS_PER_COMMITTEE_PERIOD = 256
 export const LODESTAR_ENDPOINT = 'https://lodestar-sepolia.chainsafe.io'
 export const RELAY_ENDPOINT =
   'https://rpc.ankr.com/premium-http/eth_sepolia_beacon/9b5188fb2ebf6f1e050bf1a1b623623759a0108f7a161b3986f3f21329166288'
-export const EXECUTION_ENDPOINT = 'https://rpc.ankr.com/eth'
+export const EXECUTION_ENDPOINT = 'https://rpc.ankr.com/eth_sepolia'
 export const BEACON_ENDPOINT = 'http://0.0.0.0:5052'
