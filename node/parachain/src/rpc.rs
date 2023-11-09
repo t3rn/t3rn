926d67569bc3b7a7f29162851e7838cb3c0d9c4d
MaciejBaj <maciej@t3rn.io>
2023-11-09 14:43:12 +0000

    fix: correct t3rn node rpc feature flags

--- node/parachain/src/rpc.rs
+++ node/parachain/src/rpc.rs
@@ -40,7 +40,6 @@ pub struct FullDeps<C, P> {
 }
 
 /// Instantiate all full RPC extensions.
-#[cfg(not(feature = "t3rn"))]
 pub fn create_full<C, P>(
     deps: FullDeps<C, P>,
 ) -> Result<RpcModule<()>, Box<dyn std::error::Error + Send + Sync>>
@@ -67,38 +66,11 @@ where
 
     module.merge(System::new(client.clone(), pool, deny_unsafe).into_rpc())?;
     module.merge(TransactionPayment::new(client.clone()).into_rpc())?;
+
+    #[cfg(all(feature = "t3rn", not(feature = "default")))]
     module.merge(Xdns::new(client.clone()).into_rpc())?;
+    #[cfg(all(feature = "t3rn", not(feature = "default")))]
     module.merge(Portal::new(client).into_rpc())?;
-
-    Ok(module)
-}
-
-/// Instantiate all full RPC extensions for t3rn shell - no XDNS, no Portal
-#[cfg(all(feature = "t3rn", not(feature = "default")))]
-pub fn create_full<C, P>(
-    deps: FullDeps<C, P>,
-) -> Result<RpcModule<()>, Box<dyn std::error::Error + Send + Sync>>
-where
-    C: ProvideRuntimeApi<Block>,
-    C: HeaderBackend<Block> + HeaderMetadata<Block, Error = BlockChainError> + 'static,
-    C: Send + Sync + 'static,
-    C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Nonce>,
-    C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
-    C::Api: BlockBuilder<Block>,
-    P: TransactionPool + 'static,
-{
-    use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApiServer};
-    use substrate_frame_rpc_system::{System, SystemApiServer};
-
-    let mut module = RpcModule::new(());
-    let FullDeps {
-        client,
-        pool,
-        deny_unsafe,
-    } = deps;
-
-    module.merge(System::new(client.clone(), pool, deny_unsafe).into_rpc())?;
-    module.merge(TransactionPayment::new(client).into_rpc())?;
 
     Ok(module)
 }
