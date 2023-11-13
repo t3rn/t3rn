d535a079eba99e160e6848a7381d51e34b435ecf
chexware <aleksandar.brayanov@protonmail.com>
2023-11-13 12:34:38 +0000

    feat: added registerAsset CLI parameters validation

--- client/packages/cli/src/schemas/registerAsset.ts
+++ client/packages/cli/src/schemas/registerAsset.ts
@@ -4,11 +4,11 @@ export const AssetRegistrationSchema = z.object({
   endpoint: z.string({
     invalid_type_error: 'Enpoint must be a string',
     required_error: 'Endpoint is required',
-  }),
+  }).startsWith('ws://'),
   dest: z.string({
-    invalid_type_error: 'Destination type must be a string',
+    invalid_type_error: 'Destination type must be a one of the options: t0rn; local; para',
     required_error: 'Destination type is required',
-  }),
+  }).regex(/t0rn|local|para/),
   name: z.string({
     invalid_type_error: 'Name must be a string',
     required_error: 'Name is required',
@@ -22,7 +22,8 @@ export const AssetRegistrationSchema = z.object({
       invalid_type_error: 'Token symbol must be a string',
       required_error: 'Token symbol is required',
     })
-    .max(7),
+    .max(7)
+    .regex(/^[a-zA-Z]+$/),
   decimals: z.number({
     invalid_type_error: 'Token decimals must be a number',
     required_error: 'Token decimals number is required',
