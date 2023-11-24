6fe7cef4e1cf2d0b263865a87a664917b02a2f42
MaciejBaj <maciej@t3rn.io>
2023-11-24 20:07:35 +0000

    feat: format start all grandpa rangers script

--- client/packages/grandpa-ranger/src/startAll.ts
+++ client/packages/grandpa-ranger/src/startAll.ts
@@ -1,32 +1,35 @@
-import { GrandpaRanger } from './ranger';
-import { logger } from './logging';
+import { GrandpaRanger } from './ranger'
+import { logger } from './logging'
 
 const startRanger = async (profile: string) => {
-  let config: any;
+  let config: any
   try {
-    logger.info(`👨‍🦳 trying to load GRANDPA ranger for ${profile}...`);
-    process.env.PROFILE = profile;
-    config = require(`../config/${profile}.ts`).default;
-    logger.info(`Using ${profile}.ts profile`);
+    logger.info(`👨‍🦳 trying to load GRANDPA ranger for ${profile}...`)
+    process.env.PROFILE = profile
+    config = require(`../config/${profile}.ts`).default
+    logger.info(`Using ${profile}.ts profile`)
   } catch (error) {
-    logger.fatal(`🪦 GRANDPA ranger for ${profile} failed to load!`);
-    await new Promise(resolve => setTimeout(resolve, 600000));
-    process.exit(1);
+    logger.fatal(`🪦 GRANDPA ranger for ${profile} failed to load!`)
+    await new Promise(resolve => setTimeout(resolve, 600000))
+    process.exit(1)
   }
 
-  const ranger = new GrandpaRanger(config);
-  await ranger.start();
-};
+  const ranger = new GrandpaRanger(config)
+  await ranger.start()
+}
 
-(async () => {
+;(async () => {
   try {
     await Promise.all([
       startRanger('rococo'),
       startRanger('kusama'),
-      startRanger('polkadot')
-    ]);
+      startRanger('polkadot'),
+    ])
   } catch (error) {
-    logger.error({error}, `An error occurred while starting the rangers for ${process.env.PROFILE}`);
-    process.exit(1);
+    logger.error(
+      { error },
+      `An error occurred while starting the rangers for ${process.env.PROFILE}`,
+    )
+    process.exit(1)
   }
-})();
+})()
