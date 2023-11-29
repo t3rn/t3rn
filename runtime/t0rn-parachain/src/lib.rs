e68497eaa1d6001374ec901e053cae0739903324
Maciej Baj <6909553+MaciejBaj@users.noreply.github.com>
2023-11-29 22:37:06 +0000

    build: upload t0rn specs on tx version = 2 (#1483)
    * build: upload t0rn specs on tx version = 2
    
    * build: de-bump t0rn runtime versions to 0,0,1

--- runtime/t0rn-parachain/src/lib.rs
+++ runtime/t0rn-parachain/src/lib.rs
@@ -78,11 +78,11 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // https://docs.rs/sp-version/latest/sp_version/struct.RuntimeVersion.html
     spec_name: create_runtime_str!("t0rn"),
     impl_name: create_runtime_str!("Circuit Collator"),
-    authoring_version: 1,
-    spec_version: 1,
-    impl_version: 1,
+    authoring_version: 0,
+    spec_version: 0,
+    impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 2,
+    transaction_version: 1,
     // https://github.com/paritytech/cumulus/issues/998
     // https://github.com/paritytech/substrate/pull/9732
     // https://github.com/paritytech/substrate/pull/10073
