07c769dc859d4adf7ad5e08ad4e18cc0546ba15b
Galin Kostov <galin@t3rn.io>
2023-11-30 11:39:39 -0300

    build: add force option to rebootgateway

--- client/packages/cli/src/cli.ts
+++ client/packages/cli/src/cli.ts
@@ -53,6 +53,7 @@ withExportMode(
     .command('rebootGateway')
     .argument('vendor')
     .description('Reboot a gateway')
+    .option('-f, --force', 'Force on live chain')
     .action(wrapCryptoWaitReady(handleRebootCommand)),
 )
 
