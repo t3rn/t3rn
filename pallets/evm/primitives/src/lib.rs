d0f23f7058cf7b54c10909d2355a7d5793f4114d
chexware <aleksandar.brayanov@protonmail.com>
2023-12-05 04:01:07 +0000

    feat: added AddressMapping trait to enable H160 to AccountId32 and AccountId32 to H160 address mapping

--- pallets/evm/primitives/src/lib.rs
+++ pallets/evm/primitives/src/lib.rs
@@ -77,6 +77,26 @@ pub trait Erc20Mapping {
     fn decode_evm_address(v: EvmAddress) -> Option<AssetId>;
 }
 
+/// A mapping between `AccountId` and `EvmAddress`.
+pub trait AddressMapping<AccountId> {
+    /// Returns the AccountId used go generate the given EvmAddress.
+    fn get_account_id(evm: &EvmAddress) -> AccountId;
+    /// Returns the EvmAddress associated with a given AccountId or the
+    /// underlying EvmAddress of the AccountId.
+    /// Returns None if there is no EvmAddress associated with the AccountId
+    /// and there is no underlying EvmAddress in the AccountId.
+    fn get_evm_address(account_id: &AccountId) -> Option<EvmAddress>;
+    /// Returns the EVM address associated with an account ID and generates an
+    /// account mapping if no association exists.
+    fn get_or_create_evm_address(account_id: &AccountId) -> EvmAddress;
+    /// Returns the default EVM address associated with an account ID.
+    fn get_default_evm_address(account_id: &AccountId) -> EvmAddress;
+    /// Returns true if a given AccountId is associated with a given EvmAddress
+    /// and false if is not.
+    fn is_linked(account_id: &AccountId, evm: &EvmAddress) -> bool;
+}
+
+
 pub enum AccessedStorage {
     AccountCodes(H160),
     AccountStorages((H160, H256)),
