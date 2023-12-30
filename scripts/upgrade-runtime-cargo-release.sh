82a190e389cb79d4c5a8d27e2d0c056f0a7aee0b
marcin <6998565+3h4x@users.noreply.github.com>
2023-12-30 16:15:52 +0100

    fix: comment t7rn for now

--- scripts/upgrade-runtime-cargo-release.sh
+++ scripts/upgrade-runtime-cargo-release.sh
@@ -47,14 +47,14 @@ else
     echo "Because cargo release pre-release-hooks are run per target we need to make sure that versions are incremented only once"
 fi
 
-PARACHAIN_NAME=t7rn
-if git diff --quiet $DIR/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs; then
-    echo "Increment versions for ${PARACHAIN_NAME} runtime"
-    $SED -i.bak -r 's/([[:blank:]]+impl_version: )([0-9]+)(.*)/echo "\1$(echo \"\2 + 1\"|bc)\3"/e' $DIR/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs
-    $SED -i.bak -r 's/([[:blank:]]+transaction_version: )([0-9]+)(.*)/echo "\1$(echo \"\2 + 1\"|bc)\3"/e' $DIR/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs
-    $SED -i.bak -r 's/([[:blank:]]+authoring_version: )([0-9]+)(.*)/echo "\1$(echo \"\2 + 1\"|bc)\3"/e' $DIR/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs
-    $SED -i.bak -r 's/([[:blank:]]+spec_version: )([0-9]+)(.*)/echo "\1$(echo \"\2 + 1\"|bc)\3"/e' $DIR/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs
-    rm $DIR/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs.bak
-else
-    echo "Because cargo release pre-release-hooks are run per target we need to make sure that versions are incremented only once"
-fi
+# PARACHAIN_NAME=t7rn
+# if git diff --quiet $DIR/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs; then
+#     echo "Increment versions for ${PARACHAIN_NAME} runtime"
+#     $SED -i.bak -r 's/([[:blank:]]+impl_version: )([0-9]+)(.*)/echo "\1$(echo \"\2 + 1\"|bc)\3"/e' $DIR/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs
+#     $SED -i.bak -r 's/([[:blank:]]+transaction_version: )([0-9]+)(.*)/echo "\1$(echo \"\2 + 1\"|bc)\3"/e' $DIR/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs
+#     $SED -i.bak -r 's/([[:blank:]]+authoring_version: )([0-9]+)(.*)/echo "\1$(echo \"\2 + 1\"|bc)\3"/e' $DIR/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs
+#     $SED -i.bak -r 's/([[:blank:]]+spec_version: )([0-9]+)(.*)/echo "\1$(echo \"\2 + 1\"|bc)\3"/e' $DIR/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs
+#     rm $DIR/runtime/${PARACHAIN_NAME}-parachain/src/lib.rs.bak
+# else
+#     echo "Because cargo release pre-release-hooks are run per target we need to make sure that versions are incremented only once"
+# fi
