ffd5cbc9014c6bd8a823f4210687bdf2ca8176df
t3rn-ci <89840377+t3rn-ci@users.noreply.github.com>
2023-12-30 15:44:35 +0000

    build(release): release {{version}}

--- runtime/t1rn-parachain/src/lib.rs
+++ runtime/t1rn-parachain/src/lib.rs
@@ -78,11 +78,11 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // https://docs.rs/sp-version/latest/sp_version/struct.RuntimeVersion.html
     spec_name: create_runtime_str!("t3rn"),
     impl_name: create_runtime_str!("t3rn Circuit Collator"),
-    authoring_version: 30,
-    spec_version: 30,
-    impl_version: 30,
+    authoring_version: 31,
+    spec_version: 31,
+    impl_version: 31,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 30,
+    transaction_version: 31,
     // https://github.com/paritytech/cumulus/issues/998
     // https://github.com/paritytech/substrate/pull/9732
     // https://github.com/paritytech/substrate/pull/10073
--- runtime/t2rn-parachain/src/lib.rs
+++ runtime/t2rn-parachain/src/lib.rs
@@ -11,11 +11,11 @@ include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("t2rn"),
     impl_name: create_runtime_str!("t2rn"),
-    authoring_version: 22,
-    spec_version: 22,
-    impl_version: 22,
+    authoring_version: 23,
+    spec_version: 23,
+    impl_version: 23,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 22,
+    transaction_version: 23,
     state_version: 1,
 };
 
