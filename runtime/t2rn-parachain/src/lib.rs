f0d0e3e9b466ec7a4461858f24055fb8e72ee864
t3rn-ci <89840377+t3rn-ci@users.noreply.github.com>
2024-01-16 23:22:56 +0100

    build(release): release {{version}}

--- runtime/t2rn-parachain/src/lib.rs
+++ runtime/t2rn-parachain/src/lib.rs
@@ -11,11 +11,11 @@ include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("t2rn"),
     impl_name: create_runtime_str!("t2rn"),
-    authoring_version: 28,
-    spec_version: 28,
-    impl_version: 28,
+    authoring_version: 29,
+    spec_version: 29,
+    impl_version: 29,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 28,
+    transaction_version: 29,
     state_version: 1,
 };
 
