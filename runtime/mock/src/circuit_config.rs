72925c8839699f6ebcce8f10e7f6023e1ad6aa1f
Maciej Baj <6909553+MaciejBaj@users.noreply.github.com>
2024-01-16 14:39:58 +0000

    feat: bump sepolia & ethereum light client versions; fix benchmarks  (#1540)
    * fix: import XDNS trait to Vacuum benchmarks
    
    * build: exclude celestia benchmarks from t0rn runtime
    
    * feat: bump sepolia & ethereum light client versions
    
    * style: fix vacuum benchmarks formatting
    
    * chore: set treasuries spend period to 1 week
    
    * feat: remove GenesisValidators root out of eth2 and sepolia configs
    
    * fix: move eth2 init called inside if Externalities-provided

--- runtime/mock/src/circuit_config.rs
+++ runtime/mock/src/circuit_config.rs
@@ -346,7 +346,6 @@ impl pallet_grandpa_finality_verifier::Config<KusamaInstance> for Runtime {
 
 parameter_types! {
     pub const SyncCommitteeSize: u32 = 26;
-    pub const GenesisValidatorsRoot: [u8; 32] = [216,234,23,31,60,148,174,162,30,188,66,161,237,97,5,42,207,63,146,9,192,14,78,251,170,221,172,9,237,155,128,120];
     pub const SlotsPerEpoch: u32 = 32;
     pub const EpochsPerSyncCommitteePeriod: u32 = 256;
     pub const HeadersToStoreEth: u32 = 50400 + 1; // 1 week + 1. We want a multiple of 32 + 1.
@@ -356,7 +355,6 @@ parameter_types! {
 impl pallet_eth2_finality_verifier::Config for Runtime {
     type CommitteeMajorityThreshold = CommitteeMajorityThreshold;
     type EpochsPerSyncCommitteePeriod = EpochsPerSyncCommitteePeriod;
-    type GenesisValidatorRoot = GenesisValidatorsRoot;
     type HeadersToStore = HeadersToStoreEth;
     type LightClientAsyncAPI = XDNS;
     type RuntimeEvent = RuntimeEvent;
@@ -368,7 +366,6 @@ impl pallet_eth2_finality_verifier::Config for Runtime {
 impl pallet_sepolia_finality_verifier::Config for Runtime {
     type CommitteeMajorityThreshold = CommitteeMajorityThreshold;
     type EpochsPerSyncCommitteePeriod = EpochsPerSyncCommitteePeriod;
-    type GenesisValidatorRoot = GenesisValidatorsRoot;
     type HeadersToStore = HeadersToStoreEth;
     type LightClientAsyncAPI = XDNS;
     type RuntimeEvent = RuntimeEvent;
