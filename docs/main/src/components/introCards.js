e65177ae12d0828df0659304362da317866bc626
Jossif Elefteriadis <jossif_@hotmail.com>
2024-07-11 18:46:20 +0200

    fix: change theme color and add cards to intro

---
+++ docs/main/src/components/introCards.js
@@ -0,0 +1,28 @@
+import Link from "@docusaurus/Link";
+
+export default function IntroCard() {
+  return (
+    <section className="introCards-main">
+      <section className="introCard">
+        <section className="introCard-title">Executor</section>
+        <section className="introCard-description">
+          Executors are off-chain agents acting as market makers that fulfil
+          cross-chain orders, earning fees and rewards.
+        </section>
+        <Link className="introCard-link" to={"executor/executor-overview"}>
+          Become an Executor
+        </Link>
+      </section>
+      <section className="introCard">
+        <section className="introCard-title">Swap UI</section>
+        <section className="introCard-description">
+          Our Swap UI enables you to make fast, secure, and cost-efficient
+          cross-chain transactions.
+        </section>
+        <Link className="introCard-link" to={"https://bridge.t1rn.io/"}>
+          Start Swapping
+        </Link>
+      </section>
+    </section>
+  );
+}
